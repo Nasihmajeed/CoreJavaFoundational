ac5b2aab8fb5768237db1fac52a3d5b9cc738d59
ashiq k p <muhammed.ashiq.k.p@lxisoft.com>
2019-08-28 21:45:52 +0530

    IN-186 die_animal

--- AnimalGame/src/com/lxisoft/animalgame/Animal.java
+++ AnimalGame/src/com/lxisoft/animalgame/Animal.java
@@ -13,19 +13,19 @@ public class Animal
 	public void eat()
 	{
 		System.out.println("animal eat");
-		// tiger=new Tiger();
-		// lion=new Lion();
-		// bear=new Bear();
-		// tiger.eat();
-		// lion.eat();
-		// bear.eat();
+		tiger=new Tiger();
+		lion=new Lion();
+		bear=new Bear();
+		tiger.eat();
+		lion.eat();
+		bear.eat();
 	}
 	public void run()
 	{
 		System.out.println("animal run");
-		// tiger.run();
-		// lion.run();
-		// bear.run();
+		tiger.run();
+		lion.run();
+		bear.run();
 	}
 
 }
