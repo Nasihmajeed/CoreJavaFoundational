93e7a00bfa0e850f1aadf1c4d3ece0eb8e3d6dea
ajithkp1537 <ajith.k.p@lxisoft.com>
2019-08-29 09:38:42 +0530

    IN-181

--- AnimalGame/src/com/lxisoft/animalgame/Animal.java
+++ AnimalGame/src/com/lxisoft/animalgame/Animal.java
@@ -1,4 +1,7 @@
 package com.lxisoft.animalgame;
+
+
+import com.lxisoft.animalgame.Tiger;
 public class Animal
 {
 
@@ -8,16 +11,25 @@ public class Animal
 		Tiger tiger1=new Tiger();
 		Animal tiger2= new Tiger();
 		
-		tiger.print2();
+		tiger.print1();
+		System.out.println("1");
 		
 		tiger.print1();
-		tiger1.print2();
+		
+		System.out.println("2");
 		tiger1.print1();
-		tiger2.print2();
+
+		System.out.println("3");
+		tiger1.print1();
+
+		System.out.println("4");
+		tiger2.print1();
+
+		System.out.println("5");
 		tiger2.print1();
 		
 	}
-	public void print2()
+	public void print1()
 	{
 		System.out.println("tiger eat");
 
