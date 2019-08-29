5fdb659fec0eae9e9b2b12792b5d7cb0bd16e0be
megha-vt <megha.v.t@lxisoft.com>
2019-08-29 10:25:40 +0530

    IN-204

--- AnimalGame/src/com/lxisoft/animalgame/Forest.java
+++ AnimalGame/src/com/lxisoft/animalgame/Forest.java
@@ -10,15 +10,23 @@ public class Forest
 
 	public void print()
 	{
-	    System.out.println("pineforest");
+	    System.out.println("PINE FOREST");
+	    System.out.println("----------------");
      	animal=new Animal();
      	tiger=new Animal();
      	Tiger t=new Tiger();
+     	Lion l=new Lion();
      	lion=new Animal();
      	animal.eat();
 		tiger.eat();
 		lion.eat();
-		t.eat();	
+		t.eat();
+		l.eat();
+		animal.run();
+		tiger.run();
+		lion.run();
+		t.run();
+		l.run();	
 	}
 
 }
