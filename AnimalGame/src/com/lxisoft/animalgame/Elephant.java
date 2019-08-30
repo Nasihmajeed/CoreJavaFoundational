c665e9031e6a95628a87ee973ce586d7dbf423ad
unknown <neethu.m@lxisoft.com>
2019-08-30 09:57:32 +0530

    IN-215 creating classes for animalgame

---
+++ AnimalGame/src/com/lxisoft/animalgame/Elephant.java
@@ -0,0 +1,12 @@
+package com.lxisoft.animalgame;
+public class Elephant extends Animal
+{
+	public void eat()
+	{
+		System.out.println("Elephant eating banana");
+	}
+		public void run()
+		{
+			System.out.println("Elephant running slowly");
+		}
+	}
---
+++ AnimalGame/src/com/lxisoft/animalgame/Rabbit.java
@@ -0,0 +1,15 @@
+package com.lxisoft.animalgame;
+public class Rabbit extends Animal
+{
+	public void eat()
+	{
+		System.out.println("Rabbit eating carrot");
+		}
+		public void run()
+		{
+		System.out.println("Rabbit running speedly");	
+		
+}
+
+		
+	}
