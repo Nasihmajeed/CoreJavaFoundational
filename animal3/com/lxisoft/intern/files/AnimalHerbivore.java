164ee9bf487a452685bdf50fad94a2bf7fb3e239
josemonjoseph <josemon.joseph@lxisoft.com>
2019-09-17 16:55:02 +0530

    v3

---
+++ animal3/com/lxisoft/intern/files/AnimalHerbivore.java
@@ -0,0 +1,36 @@
+package com.lxisoft.intern.files;
+public class AnimalHerbivore extends Animal implements Herbivores
+{
+	public String fight(Animal a)
+	 {    String f = null;
+          int x = (this.getPower()-10);
+          int y = (a.getPower()-10);
+		   if(a instanceof Herbivores)
+              {    System.out.println("Both Herbivores");
+	                  if(a.getPower()>this.getPower())
+                        { System.out.println(a.getName()+" attacks "+this.getName());
+                            if((x>=0)&(y>=0))
+                               { System.out.println(this.getName()+"power decreased by 10 ,Current power is "+this.setPower(x));
+                                 System.out.println(a.getName()+" power decresed by 10 ,Current power is "+a.setPower(y));
+                               }
+                        }
+                       else
+                        {  System.out.println(this.getName()+" attacks "+a.getName());
+                           if((x>=0)&(y>=0))
+                            {  System.out.println(this.getName()+"power decreased by 10 ,Current power is "+this.setPower(x));
+                               System.out.println(a.getName()+" power decresed by 10 ,Current power is "+a.setPower(y));
+                            }
+                        }
+             }
+            else
+                { if(a.getPower()>this.getPower())
+                    { System.out.println(a.getName()+" killed "+this.getName());
+                      f=this.getName();
+                    }
+                   else System.out.println(this.getName()+" Only attacks");
+                }
+            return f; 
+	 }
+public void eat()
+{}
+}
