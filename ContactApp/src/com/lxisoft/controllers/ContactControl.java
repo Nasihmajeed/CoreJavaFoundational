69823befead8f7501c066cdc0a7aac6afab413a6
megha-vt <megha.v.t@lxisoft.com>
2019-10-25 16:32:43 +0530

    IN-1450,IN-1441

--- ContactApp/src/com/lxisoft/controllers/ContactControl.java
+++ ContactApp/src/com/lxisoft/controllers/ContactControl.java
@@ -1,22 +1,28 @@
 package com.lxisoft.controllers;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 import com.lxisoft.models.*;
 import com.lxisoft.view.*;
 public class ContactControl
 {
-	List<Contact>contactList=new ArrayList<Contact>();
-	String[] name={"meghu","meharu","ammu"};
-	String[] contactNo={"12345","23456","56789"};
+	
 	public List<Contact> getContactDetails()
 	{
+		List<Contact>contactList=new ArrayList<Contact>();
+		String[] name={"meghu","meharu","ammu"};
+		String[] contactNo={"12345","23456","56789"};
 		for(int i=0;i<3;i++)
 		{
 			Contact contact=new Contact();
 			contact.setName(name[i]);
 			contact.setContactNo(contactNo[i]);
-
+			contactList.add(contact);
 		}
 		return contactList;
+    }
+    public void addDetails(String name,String contactNo)
+    {
+    	List<Contact>contactList=new ArrayList<Contact>();
+    	Contact contact=new Contact();
+    	contactList.add(contact);
     }
 }
--- ContactApp/src/com/lxisoft/view/Tdd.java
+++ ContactApp/src/com/lxisoft/view/Tdd.java
@@ -1,21 +1,35 @@
 package com.lxisoft.view;
 import com.lxisoft.controllers.*;
 import com.lxisoft.models.*;
-import java.util.List;
-import java.util.ArrayList;
-
+import java.util.*;
 public class Tdd
 {
 	public static void main(String[] args)
 	{
+		Scanner sc=new Scanner(System.in);
 		ContactControl control=new ContactControl();
 		List<Contact>contactList=new ArrayList<Contact>();
-
+		System.out.println("CONTACT APP\n***************");
 		contactList=control.getContactDetails();
 		for(int i=0;i<contactList.size();i++)
 		{
 			System.out.println("Name= "+contactList.get(i).getName());
 			System.out.println("Number= "+contactList.get(i).getContactNo());
 		}
+		System.out.println("\nDo you want to perform any operations?\n0-Add\n1-Remove\n2-Edit\n3-Exit");
+		int x=sc.nextInt();
+		switch(x)
+		{
+			case 0:addContact(control);break;
+		}
+	}
+	public static void addContact(ContactControl control)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter name:");
+		String name=sc.next();
+		System.out.println("Enter contact number:");
+		String contactNo=sc.next();
+		control.addDetails(name,contactNo);
 	}
 }
