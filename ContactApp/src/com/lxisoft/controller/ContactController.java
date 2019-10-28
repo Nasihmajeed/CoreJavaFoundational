29e52acee1f88a5791334da6a8aaf923f3d26fa2
unknown <pushkala.m@lxisoft.com>
2019-10-28 12:45:21 +0530

    IN-1501

--- ContactApp/src/com/lxisoft/controller/ContactController.java
+++ ContactApp/src/com/lxisoft/controller/ContactController.java
@@ -4,40 +4,56 @@ import java.util.*;
 public class ContactController
 {
 	ArrayList<Contact> contacts=new ArrayList<Contact>();
-	Contact contact=new Contact();
+	Contact contact;
 	public void addContact(String name,long number)
 	{
-		
+		contact=new Contact();
 		contact.setName(name);
 		contact.setNo(number);
-		contacts.add(contact);
-		
+		contacts.add(contact);	
 	}
-	public void searchContact(String name)
+	public void searchContact(int i,String name)
 	{
+		
 		boolean search=contacts.contains(name);
-		System.out.println(search);
-		if(search)
-		{
-			System.out.println(" Contact found..!");
-			getContact();
-		}
-		else
-			System.out.println(" There is no such contacts..!");
-	}
-	public void updateContact(){}
-	public void deleteContact(String name)
-	{
-	
-	}
-	
-	public void getLength()
-	{
-	
+		//if(search)
+			if(name.equals(contacts.get(i).getName()))
+			{
+				System.out.println(" Contact found..!");
+				getContactDetails(i);
+			}
 		
-
+		// else
+		// 	System.out.println(" There is no such contacts..!");
 	}
-
+	public void updateContact(int i,String name,long number)
+	{	
+		contact=new Contact();
+		contact.setName(name);
+		contact.setNo(number);
+		contacts.set(i,contact);
+	}
+	public void deleteContact(int i)
+	{
+		contacts.remove(i);
+		System.out.println("contact Deleted!");
+	}
+	public int getLength()
+	{
+		return contacts.size();	
+	}
+	public void getContactDetails(int i)
+	{
+		System.out.println(contacts.get(i).getName()+" \t "+contacts.get(i).getNo());
+	}
+	public String getName(int i)
+	{
+		return contacts.get(i).getName();
+	}
+	public long getNo(int i)
+	{
+		return contacts.get(i).getNo();
+	}
 
 	
 
--- ContactApp/src/com/lxisoft/view/Tdd.java
+++ ContactApp/src/com/lxisoft/view/Tdd.java
@@ -7,39 +7,23 @@ public class Tdd
 	
 	public static void main(String []args)
 	{
-		//Contact contact=getContact();
 		Scanner sc=new Scanner(System.in);
 		ContactController control=new ContactController();
 		int default_option=0;
 		char continueOpt='\0';
 		do
 		{  
-			String name;long number;
+		
 			System.out.println(" select your option	");
 			System.out.println(" 1:Add  \n 2:Search  \n 3:update  \n 4:Delete \n 5:View");
 			int option=sc.nextInt();
 			switch(option)
 			{
-				case 1:		System.out.println("contact Name:	");
-							name=sc.next();
-							System.out.println("number ");
-							number=sc.nextLong();
-							control.addContact(name,number);
-							break;
-
-				case 2:		System.out.println(" Enter the name to search:");
-							name=sc.next();
-							control.searchContact(name);break;
-
-				case 3:		System.out.println("enter the contact to edit");
-
-							control.updateContact();break;
-
-				case 4:		System.out.println("enter the contact to delete");
-							name=sc.next();
-							control.deleteContact(name);break;
-
-				case 5:		getContacts(control);break;
+				case 1:		createNewContact(control);break;
+				case 2:		searchContact(control);break;
+				case 3:		updateContact(control);break;
+				case 4:		deleteContact(control);break;
+				case 5:		readContacts(control);break;
 				default:	System.out.println("Enter the correct option!");
 							default_option=1;
 							break;
@@ -47,37 +31,81 @@ public class Tdd
 			System.out.println("Do you want to continue ? Y/N");
 			continueOpt=sc.next().charAt(0);
 		}while(default_option==1|(continueOpt=='Y'|continueOpt=='y'));
-		
-		public static void getContacts(ContactController control)
-		{
-	
-			int length=control.getLength();
-			for(int i=0;i<;i++)
-			{
-
-			// 	System.out.println("Name: "+contacts.get(i).getName()+"\n"+"phno: "+contacts.get(i).getNo()); 	
-			}
-			
-
-		}
-
 	}
+	public static void readContacts(ContactController control)
+	{
+		int length=control.getLength();
+		System.out.println("Name \t ContactNo");
+		for(int i=0;i<length;i++)
+			control.getContactDetails(i);
+	}
+	public static void createNewContact(ContactController control)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("contact Name:	");
+		String name=sc.next();
+		System.out.println("number ");
+		long number=sc.nextLong();
+		control.addContact(name,number);
+	}
+	public static void searchContact(ContactController control)
+	{
+		Scanner sc=new Scanner(System.in);
+		int length=control.getLength();
+		System.out.println(" Enter the name to search:");
+		String name=sc.next();
+		for(int i=0;i<length;i++)
+		control.searchContact(i,name);
+	}
+	public static void updateContact(ContactController control)
+	{
+		Scanner sc=new Scanner(System.in);
+		int length=control.getLength();
+		System.out.println("contact List \n.................");
+		System.out.println("  Name \t ContactNo");
+		for(int i=0;i<length;i++)
+		{
+			System.out.print((i)+" ");
+			control.getContactDetails(i);
+		}
+			System.out.println("enter the location to edit");
+			int position=sc.nextInt();
+			control.getContactDetails(position);
+			System.out.println("1:Edit Name \n2:Edit ContactNo ");
+			int select=sc.nextInt();
+			String name;long num;
+			switch(select)
+			{
+				case 1:	System.out.println("Enter the new name");name=sc.next();
+						num=control.getNo(position);control.updateContact(position,name,num);System.out.println("updated!");break;
+				case 2: System.out.println("Enter the new number");num=sc.nextLong();
+						name=control.getName(position);control.updateContact(position,name,num);System.out.println("updated!");break;			
+			}	
+		
+		
+	}
+	public static void deleteContact(ContactController control)
+	{
+		Scanner sc=new Scanner(System.in);
+		int length=control.getLength();
+		System.out.println("contact List \n.................");
+		System.out.println("  Name \t ContactNo");
+		for(int i=0;i<length;i++)
+		{
+			System.out.print((i)+" ");
+			control.getContactDetails(i);
+		}
+		System.out.println("enter the contact to delete");
+		int number=sc.nextInt() ;
+		control.deleteContact(number);
+	}
+
+
+}
 	
 		
-	
-	// public static Contact getContact()
-	// {
-	// 	Scanner sc=new Scanner(System.in);
-	// 	Contact contact=new Contact();
-	// 	System.out.println("contact Name:	");
-	// 	contact.setName(sc.next());
-	// 	System.out.println("number ");
-	// 	contact.setNo(sc.nextLong());
-	
-	// 	return contact;
 
-	// }
 
 	
-}
+
 
