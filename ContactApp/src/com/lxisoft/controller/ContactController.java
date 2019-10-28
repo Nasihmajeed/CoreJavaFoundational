4c6ba45267bffd854109b7863d01cca4c891721c
ashiq k p <muhammed.ashiq.k.p@lxisoft.com>
2019-10-28 18:23:34 +0530

    IN-1538

--- ContactApp/src/com/lxisoft/controller/ContactController.java
+++ ContactApp/src/com/lxisoft/controller/ContactController.java
@@ -8,11 +8,12 @@ import com.lxisoft.view.Tdd;
 public class ContactController
 {
 	ArrayList<Contact> array=new ArrayList<Contact>();
-	
+	Tdd t;
 
-	public void setContact(int n)
+	public void setContact()
 	{
-		Tdd t=new Tdd();
+		t=new Tdd();
+		int n=t.inputContact();
 		for (int i=0;i<n;i++)
 		{
 			String[] temp=t.scan(i);
@@ -28,7 +29,58 @@ public class ContactController
 		return array;
 	}
 	
+	public void features(int option)
+	{
+		switch(option)
+		{
+			case 0:contactAdd();break;
+			case 1:contactSearch();break;
+			case 2:contactDelete();break;
+			case 3:t.displayContact();break;
+		}
+	}
 
+	public void contactAdd()
+	{
+		int n=t.inputContact();
+		for (int i=0;i<n;i++)
+		{
+			String[] temp=t.scan(i);
+			Contact contact=new Contact();
+			contact.setName(temp[0]);
+			contact.setNumber(temp[1]);
+			array.add(array.size(),contact);
+		}
+		t.displayContact();
+	}
+
+	public int contactSearch()
+	{
+		int i=0;
+		String element=t.scanElement();
+		for(Contact contact: array)
+		{
+			i++;
+			if((contact.getName()).equals(element))
+			{
+				t.elementFound(contact,i);
+				return i;
+			}
+		}
+		if(i==0)
+			System.out.println("Contact not present");
+		return i;
+	}
+
+	public void contactDelete()
+	{
+		int i=contactSearch();
+		if(i!=0)
+		{
+			array.remove(i-1);
+		}
+		t.displayContact();
+	}
 }
 
 
--- ContactApp/src/com/lxisoft/view/Tdd.java
+++ ContactApp/src/com/lxisoft/view/Tdd.java
@@ -1,25 +1,28 @@
 package com.lxisoft.view;
 import com.lxisoft.controller.ContactController;
 import com.lxisoft.model.*;
+import java.util.concurrent.TimeUnit;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Tdd
 {
 	Scanner sc=new Scanner(System.in);
-	ContactController controller=new ContactController();
+	static ContactController controller=new ContactController();
 	
 	public static void main(String[] args)
 	{
 		System.out.print(" \n\n");
 		Tdd t=new Tdd();
-		t.inputContact();
+		controller.setContact();
 		t.displayContact();
+		t.contactOptions();
 	}
 
 	public void displayContact()
 	{
 		int i=1;
+		System.out.print("\n THE CONTACT LIST IS \n");
 		ArrayList<Contact> array=controller.getContact();
 		for(Contact contact: array)
 		{
@@ -28,11 +31,11 @@ public class Tdd
 		}
 	}
 
-	public void inputContact()
+	public int inputContact()
 	{
 		System.out.print("enter the max range of contacts");
 		int n=sc.nextInt();
-		controller.setContact(n);		
+		return n;		
 	}
 
 	public String[] scan(int i)
@@ -43,7 +46,57 @@ public class Tdd
 		temp[1]=sc.next();
 		return temp;
 	}
+
+	public void contactOptions()
+	{
+		while(true)
+		{
+			System.out.println("do you wish to continue y/n");
+			char ch=sc.next().charAt(0);
+			if(ch!='y')
+				break;
+			processing();
+			System.out.println("\n press for features-->  0-Addition  1-searching  2-deletion  3-display");
+			int option=sc.nextInt();
+			controller.features(option);
+			System.out.println("\n");
+		}
+		System.out.println("\t\t\t\t THANK YOU");	
+	}
+
+	public void processing()
+	{
+		System.out.print("\t\t Processing");
+		for(int z=0;z<6;z++)
+		{
+			System.out.print("!");
+			try
+			{
+				TimeUnit.MILLISECONDS.sleep(100);
+			}
+			catch(InterruptedException e)
+			{
+				System.out.println("Exception occured:"+e);
+			}
+		}
+	}
+
+	public String scanElement()
+	{
+		System.out.println("enter the name to search/delete");
+		String element=sc.next();
+		return element;
+	}
+
+	public void elementFound(Contact contact,int i)
+	{
+		System.out.print("Contact present at S.No:-"+(i)+"\t\tName- " +contact.getName());
+		System.out.print("\t\tNumber- " + contact.getNumber()+"\n");
+	}
+
 }
+
+
 
 
 // public static void totalPerson()
@@ -68,4 +121,40 @@ public class Tdd
 // 	{
 // 		System.out.println("name "+array.get(0).getName());
 // 		System.out.println("name "+array.get(0).getNumber());
+// 	}
+// public ArrayList<Integer> contactSearch()
+// 	{
+// 		int i=0,count=0;
+// 		ArrayList<Integer> values=new ArrayList<Integer>();
+// 		String element=t.scanElement();
+// 		for(Contact contact: array)
+// 		{
+// 			if((contact.getName()).equals(element))
+// 			{
+// 				values.add(count,i);
+// 				t.elementFound(contact,i);
+// 				System.out.println("fdsjfdshfdj11"+values.get(count));
+// 				++count;
+// 			}
+// 			++i;
+// 		}
+// 		for (int k=0;k<values.size();k++)
+// 		{
+// 			System.out.println("fdsjfdshfdj22"+values.get(k));
+// 		}
+// 		if(count==0)
+// 			System.out.println("Contact not present");
+// 		return values;
+// 	}
+
+// 	public void contactDelete()
+// 	{
+// 		ArrayList<Integer> values=contactSearch();
+// 		for (int k=0;k<values.size();k++)
+// 		{
+// 			System.out.println("fdsjfdshfdj333"+values.get(k));
+// 			int v=values.get(k+1);
+// 			array.remove(v);
+// 		}
+// 		t.displayContact();
 // 	}
