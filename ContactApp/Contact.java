30ea6a735c29fac4d8eadebdd6da8caf975f1a1a
sanfar <muhammed.sanfar.m@lxisoft.com>
2019-10-30 16:44:53 +0530

    IN-1604 complete selecting a number in contact app

--- ContactApp/Contact.java
+++ ContactApp/Contact.java
@@ -15,8 +15,8 @@ public class Contact
 		members[0]=member1;
 		System.out.println("num    : "+members[0].num);
 		System.out.println("Name   : "+members[0].name);
-		System.out.println("Number : "+members[0].number);
-		System.out.println("Email  : "+members[0].email);
+		/*System.out.println("Number : "+members[0].number);
+		System.out.println("Email  : "+members[0].email);*/
 		System.out.println("\n");
 
 		Members member2=new Members();
@@ -27,9 +27,9 @@ public class Contact
 		members[1]=member2;
 		System.out.println("num    : "+members[1].num);
 		System.out.println("Name   : "+members[1].name);
-		System.out.println("Number : "+members[1].number);
+		/*System.out.println("Number : "+members[1].number);
 		System.out.println("Email  : "+members[1].email);
-		System.out.println("\n");
+*/		System.out.println("\n");
 
 		Members member3=new Members();
 		member3.num=2;
@@ -39,11 +39,15 @@ public class Contact
 		members[2]=member3;
 		System.out.println("num    : "+members[2].num);
 		System.out.println("Name   : "+members[2].name);
-		System.out.println("Number : "+members[2].number);
-		System.out.println("Email  : "+members[2].email);
+		/*System.out.println("Number : "+members[2].number);
+		System.out.println("Email  : "+members[2].email);*/
 		System.out.println("\n");
 
 		m.details(members);
-		
+		m.printdetail();
+		/*for (i=3;i<members.length;i++) 
+		{
+			members[i]=m.printdetail();	
+		}*/
 	}
 }
--- ContactApp/Contactdetail.java
+++ ContactApp/Contactdetail.java
@@ -2,19 +2,22 @@ import java.util.Scanner;
 public class Contactdetail
 {
 	String name,email,number;
-	
+	int no;
 	void details()
 	{
 		Scanner scr=new Scanner(System.in);
+		System.out.println("Enter no :");
+		no=scr.nextInt();
 		System.out.println("Enter name :");
 		name=scr.next();
 		System.out.println("Enter number :");
 		number=scr.next();
 		System.out.println("Email :");
-		email=scr.nextLine();
+		email=scr.next();
 	}
 	void printdetail()
 	{
+		System.out.println("no :"+no);
 		System.out.println("Name :"+name);
 		System.out.println("Number :"+number);
 		System.out.println("Email :"+email);
