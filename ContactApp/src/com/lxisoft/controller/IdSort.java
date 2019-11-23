40ce23bc9ebc983ca1a262a7d0b0c1fbc94dd447
ashiq k p <muhammed.ashiq.k.p@lxisoft.com>
2019-11-23 05:34:54 +0530

    IN-2457

--- ContactApp/src/com/lxisoft/controller/IdSort.java
+++ ContactApp/src/com/lxisoft/controller/IdSort.java
@@ -6,9 +6,9 @@ public class IdSort implements Comparator<Contact>
 {
     public int compare(Contact c1,Contact c2) 
     { 
-
-    	// for comparison 
-    	return c1.getId().compareTo(c1.getId()); 
+    	int x=Integer.parseInt(c1.getId());
+    	int y=Integer.parseInt(c2.getId());
+    	return x-y; 
  	 
     } 
 }
