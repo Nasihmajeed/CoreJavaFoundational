a5fe4098e4b3f80b8bf122d7fa834d131c952975
unknown <AdarshLxi>
2019-11-27 19:51:42 +0530

    IN-2673,Refactoring Bill Method

--- Hotel/com/lxisoft/adminuser/User.java
+++ Hotel/com/lxisoft/adminuser/User.java
@@ -4,14 +4,15 @@ import com.lxisoft.hotel.FoodOrdering;
 import java.util.Scanner;
 public class User
 {
+	Scanner scanner;
 	public void bill(Hotel hotel)
 	{
-		Scanner scanner = new Scanner(System.in);
-		FoodOrdering foodOrdering = new FoodOrdering();
+		scanner = new Scanner(System.in);
+		/*FoodOrdering foodOrdering = new FoodOrdering();*/
 		boolean numberExist = false;
 		int[] array = new int[10];
 		int[] quantity = new int[10];
-		int i=0,n=1;
+		int i=0;
 		System.out.println("********Menu********");
 		hotel.getProduct(hotel.foodProduct);
 		Boolean isTrue = false;
@@ -22,53 +23,50 @@ public class User
 			int num = scanner.nextInt();
 			switch(num)
 			{
-				case 1:
-					System.out.println("==> Select Food");
-					int number = scanner.nextInt();
-					for(int j=0;j<10;j++)
-					{
-						if(number==array[j])
-						{
-							n++;
-						    quantity[j]=n;
-							numberExist = true;
-							break;
-					    }
-					}
-					if(numberExist)
-					{
-					numberExist = false;
-					}
-					else
-					{
-						n=1;
-						quantity[i]=n;
-						array[i] = number;
-						i++;
-					}
-					isTrue = true;
-					break;
-				case 2:
-					print(hotel,array,quantity);
-					break;
-				default :
-					System.out.println("==> Select the Above Option");
-					isTrue = true;
-					break;
+			case 1:
+				selectProduct(array);
+				isTrue = true;
+				break;
+			case 2:
+				print(hotel,selectProduct(array),quantity);
+				isTrue = false;
+				break;
+			default :
+				System.out.println("==> Select the Above Option");
+				isTrue = true;
+				break;
 			}
 
 		}while(isTrue);
 		
 	}
+	public int[] selectProduct(int[] array)
+	{
+		scanner = new Scanner(System.in);
+		int n=1;
+		System.out.println("==> Select Food");
+		int productNumber = scanner.nextInt();
+		for(int i=0;i<10;i++)
+		{
+			if(array[i]==0)
+			{
+				array[i]=productNumber;
+				break;
+				/*quantity[i]=n;*/
+			}
+		}
+		return array;
+	}
 	public void print(Hotel hotel,int[] array,int[] quantity)
 	{
+		scanner = new Scanner(System.in);
 		int amount = 0;
 		System.out.println("******BILL******");
 		for(int i=0;i<10;i++)
 		{
 			if(array[i]!=0)
 			{
-				amount = amount + (hotel.foodProduct[array[i]-1].getAmount()*quantity[i]);
+				amount = amount + (hotel.foodProduct[array[i]-1].getAmount());
 			}
 		}
 		if(hotel.getName()!=null)
