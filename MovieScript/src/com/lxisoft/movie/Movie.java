0d2fe8f6564c03c97e6f3b7d8d2f08acef8300a5
gokullxi <gokul.t.s@lxisoft.com>
2019-12-30 16:43:01 +0530

    IN-4172,IN-4128 implementing file creation,file writing,file reding

--- MovieScript/src/com/lxisoft/movie/Movie.java
+++ MovieScript/src/com/lxisoft/movie/Movie.java
@@ -1,16 +1,40 @@
 package com.lxisoft.movie;
+import java.util.ArrayList;
+import java.io.File;
 public class Movie 
 {
 	String name="dark knight";
 	String director = "christopher nolan";
 	String producer = "warner bros";
+	Script script=new Script();
 
 	public void display()
 	{
 		System.out.println("movie name : "+name+"\ndirector : "+director+"\nproducer : "+producer);
 		System.out.println("**************************************");
 		System.out.println("scene no 1\n-----------");
+		fileCreation();
 		
+	}
+	public void fileCreation()
+	{
+		File villan=new File("D:\\lxi\\java\\my Git\\CoreJavaFoundational\\MovieScript\\src\\com\\lxisoft\\file\\villanDialogue.txt");
+		File comedian=new File("D:\\lxi\\java\\my Git\\CoreJavaFoundational\\MovieScript\\src\\com\\lxisoft\\file\\comedianDialogue.txt");
+		script.comedianDialogue();
+		script.villanDialogue();
+		script.writeToFile(villan,script.villan);
+		script.writeToFile(comedian,script.comedian);
+		ArrayList<Dialogue> readArray1= script.readFromFile(villan);
+		ArrayList<Dialogue> readArray2= script.readFromFile(comedian);
+		for(Dialogue d1: readArray1)
+		{
+			System.out.println(d1.dialogue);
+		}
+		for(Dialogue d2: readArray2)
+		{
+			System.out.println(d2.dialogue);
+		}
+
 	}
 
 }
--- MovieScript/src/com/lxisoft/movie/Script.java
+++ MovieScript/src/com/lxisoft/movie/Script.java
@@ -1,5 +1,6 @@
 package com.lxisoft.movie;
 import java.util.ArrayList;
+import java.io.*;
 public class Script 
 {
 	String scriptWriter = "christopher nolan";
@@ -7,14 +8,63 @@ public class Script
 	public ArrayList<Dialogue>villan= new ArrayList<Dialogue>();
 	public void comedianDialogue()
 	{
+		for(int i=0;i<3;i++)
+		{
+			comedian.add(new Dialogue());
+		}
 		comedian.get(0).dialogue="hello";
 		comedian.get(1).dialogue="whats your role in this movie ?";
 		comedian.get(2).dialogue="i am the comedian ";
 	}
 	public void villanDialogue()
 	{
+		for(int i=0;i<3;i++)
+		{
+			villan.add(new Dialogue());
+		}
 		villan.get(0).dialogue="hai..";
 		villan.get(1).dialogue="i am the villan ";
 		villan.get(2).dialogue="whats your role in the movie ?";
+	}
+	public void writeToFile(File file,ArrayList<Dialogue>dialogueArray)
+	{
+		try{
+		FileWriter fw =new FileWriter(file,false);
+		BufferedWriter bw =new BufferedWriter(fw);
+		for(Dialogue d : dialogueArray)
+		{	
+			bw.write(d.dialogue);
+			bw.newLine();
+		}
+		bw.flush();
+		bw.close();
+	}
+	catch(Exception e)
+	{
+		e.printStackTrace();
+	}
+	}
+	public ArrayList<Dialogue> readFromFile(File file)
+	{
+		ArrayList<Dialogue> readDialogue=new ArrayList<Dialogue>();
+		try{
+			FileReader fr =new FileReader(file);
+			BufferedReader br =new BufferedReader(fr);
+			String s;
+			while((s=br.readLine())!=null)
+			{
+					readDialogue.add(new Dialogue());
+					for(int i=0;i<readDialogue.size();i++)
+					{
+						if(readDialogue.get(i).dialogue==null)
+						readDialogue.get(i).dialogue=s;
+					}
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return readDialogue;
 	}
 }
