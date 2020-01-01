6bcbea026cdee17a2e52e010b44ea43ebdbb7ec0
unknown <AdarshLxi>
2020-01-01 13:42:08 +0530

    IN-4250,Code Testing and Updation

--- MovieScript/src/com/lxisoft/movie/Villan.java
+++ MovieScript/src/com/lxisoft/movie/Villan.java
@@ -1,5 +1,15 @@
 package com.lxisoft.movie;
+import com.lxisoft.Interface.Villanic;
+import java.util.ArrayList;
 public class Villan extends Actors implements Villanic
 {
-	
+	public ArrayList<Dialouge> villanDialouge = new ArrayList<Dialouge>();
+
+	public ArrayList<Dialouge> setArray(ArrayList<Dialouge> dialouge)
+	{
+		for(int i=0;i<5;i++)
+		{
+			//dialouge.add();
+		}
+	}
 }
