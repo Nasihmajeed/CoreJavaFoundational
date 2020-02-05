abd6e62cf8929902d6b7b5755414e4718f1e2a24
Ashikafs <ashik.hussain@lxisoft.com>
2020-02-05 12:57:11 +0530

    http://jira.bytatech.com:8080/browse/IN-5965

--- AnimalGame/com/lxisoft/forest/Forest.java
+++ AnimalGame/com/lxisoft/forest/Forest.java
@@ -8,39 +8,26 @@ private String forestname ="Black Forest";
 ArrayList<Animal> animal = new ArrayList<Animal>();
 static Scanner input =new Scanner(System.in);
 Game game;
-public void setForestName(String arg)
-{
-	forestname =forestname;
+// public void setForestName(String arg)
+// {
+// 	forestname =forestname;
 
-}
-public String getForestName()
-{
-	return forestname;
-}
+// }
+// public String getForestName()
+// {
+// 	return forestname;
+// }
 
 
 
 public void createForest()
 	{
+		System.out.println("\n\t"+forestname);
 		
-		animal.add(new Lion()); 
-		animal.get(0).setAnimalName("Lion"); 
-		animal.get(0).setAnimalStrength(8); 
+		creatAnimal();
 
-		animal.add(new Tiger()); 
-		animal.get(1).setAnimalName("Tiger"); 
-		animal.get(1).setAnimalStrength(7); 
-
-		animal.add(new Rabbit()); 
-		animal.get(2).setAnimalName("Rabbit"); 
-		animal.get(2).setAnimalStrength(4); 
-
-		animal.add(new Elephant()); 
-		animal.get(3).setAnimalName("Elephant"); 
-		animal.get(3).setAnimalStrength(9); 
-
-		displayForest();
-
+		setAnimalDetails();
+		
 		int temp=playGame();
 		//System.out.println(temp);
 		if(temp==0)
@@ -55,22 +42,47 @@ public void createForest()
 
 			
 	}
+public void creatAnimal()
+{
+animal.add(new Lion()); 
+animal.add(new Tiger()); 
+animal.add(new Rabbit());
+animal.add(new Elephant());
 
-public void displayForest()
-	{
-		System.out.println("\n\t"+getForestName());
+}
+
+public void setAnimalDetails()
+{
+	animal.get(0).setAnimalName("Lion"); 
+		animal.get(0).setAnimalStrength(8); 
+
+
+		animal.get(1).setAnimalName("Tiger"); 
+		animal.get(1).setAnimalStrength(7); 
+
+		 
+		animal.get(2).setAnimalName("Rabbit"); 
+		animal.get(2).setAnimalStrength(4); 
+
+		 
+		animal.get(3).setAnimalName("Elephant"); 
+		animal.get(3).setAnimalStrength(9); 
+}
+// public void displayForest()
+// 	{
+// 		System.out.println("\n\t"+getForestName());
 
 		
-		// System.out.println(animal.get(0).getAnimalName());
-		// System.out.println(animal.get(0).getAnimalStrength());
-		// // ;
-		// animal.get(1)getAnimalName();
-		// animal.get(1)getAnimalStrength();
-	}
+// 		// System.out.println(animal.get(0).getAnimalName());
+// 		// System.out.println(animal.get(0).getAnimalStrength());
+// 		// // ;
+// 		// animal.get(1)getAnimalName();
+// 		// animal.get(1)getAnimalStrength();
+// 	}
 
 public int playGame()
 {
-	System.out.print("\n1.Start The Game\nPress 0 to Exit\n Enter Your Choise :");
+	System.out.print("\n1.Start The Game\nPress 0 to Exit\n\n Enter Your Choise :");
 	int choise=input.nextInt();
 	return choise;
 }
--- AnimalGame/com/lxisoft/game/Game.java
+++ AnimalGame/com/lxisoft/game/Game.java
@@ -1,16 +1,48 @@
 package com.lxisoft.game;
 import java.util.*;
-
+import com.lxisoft.forest.*;
 public class Game
 {
+	Random objGenerator = new Random();
 	public void startGame(ArrayList<Animal> animal)
 	{
-		// for (int i=0;i<animal.size;i++)
-		// {
-		// for (int j=i+1;j<animal.size;j++)
-		// {
 
-		 System.out.println("You Entered in to Game");
+		 System.out.println("\n\nYou Entered in to Game\n\n");
+
+		for (int i=0;i<animal.size();i++)
+		{
+		// for (int j=i+1;j<animal.size();j++)
+		// {
+			int randomNumber = objGenerator.nextInt(3);
+			if(animal.get(i).getAnimalStrength()<animal.get(randomNumber).getAnimalStrength())
+			{
+				System.out.println("\n"+animal.get(i).getAnimalName()+" Meet With "+animal.get(randomNumber).getAnimalName()+"\n......\n");
+				System.out.println(animal.get(randomNumber).getAnimalName()+" Winned");
+				System.out.println(randomNumber);
+				animal.remove(i);
+				//i=i+1;
+			}
+			else if(animal.get(i).getAnimalStrength()>animal.get(randomNumber).getAnimalStrength())
+			{
+				System.out.println("\n"+animal.get(i).getAnimalName()+" Meet With "+animal.get(randomNumber).getAnimalName()+"\n......\n");
+				System.out.println(animal.get(i).getAnimalName()+" Winned");
+					System.out.println(randomNumber);
+				animal.remove(randomNumber);
+
+			}
+			else
+			{
+				System.out.println("\n"+animal.get(i).getAnimalName()+" Meet With "+animal.get(randomNumber).getAnimalName()+"\n......\n");
+				System.out.println("No Atack");
+									System.out.println(randomNumber);
+
+				continue;
+			}
+
+		// }
+		}
+
+		
  		
 	}
 }
