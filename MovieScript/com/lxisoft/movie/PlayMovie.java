9d237f49a565a626fbc156eb3e060f4911f87fca
Ashikafs <ashik.hussain@lxisoft.com>
2020-03-05 16:41:37 +0530

    http://jira.bytatech.com:8080/browse/IN-7121

--- MovieScript/com/lxisoft/movie/PlayMovie.java
+++ MovieScript/com/lxisoft/movie/PlayMovie.java
@@ -6,49 +6,60 @@ class PlayMovie
 	Random random = new Random();
 public void Play(ArrayList<Actor> actors,FileReppo script)
 {
-	int count =2;
+	int count =5;
 	System.out.println("\n\nWelcome To All");
 	System.out.println("\n________________________\n\n");
 
 	do{
 		int number1 = random.nextInt(2); 
-		int dilogeNumber = random.nextInt(2);
+		int dilogeNumber = random.nextInt(4);
 
 		if(actors.get(number1) instanceof Villanisum)
 		{
 			int number2=number1+1;
-		 PlayDialouge(number1,dilogeNumber,number2);
-		 count--;
+			PlayDialouge(actors.get(number1),dilogeNumber,actors.get(number2),script);
+		 	count--;
 		}
 
 		else if(actors.get(number1) instanceof Comic)
 		{
 			int number2=number1-1;
-		PlayDialouge(number1,dilogeNumber,number2);
-		count--;
+			PlayDialouge(actors.get(number1),dilogeNumber,actors.get(number2),script);
+			count--;
 		}
 
 	}while(count > 0);
 
-	MovieDetails();
+	MovieDetails(actors);
 }
 
-public void MovieDetails()
+public void MovieDetails(ArrayList<Actor> actors)
 {
-	System.out.print("Thanks To All");
-	System.out.print(actors.get(0).getName()+" : "+actors.get(0).getmovieRoll());
-	System.out.print(actors.get(1).getName()+" : "+actors.get(1).getmovieRoll());
+	System.out.println("\n\n\nThanks To All");
+	System.out.println(actors.get(0).getName()+" : "+actors.get(0).getmovieRoll());
+	System.out.println(actors.get(1).getName()+" : "+actors.get(1).getmovieRoll());
 }
 
-public int  PlayDialouge(int number1,int dilogeNumber,int number2)
+public void  PlayDialouge(Actor number1,int dilogeNumber,Actor number2,FileReppo script)
 {
-	
-	System.out.print(actors.get(number1).getName()+" : ");
+	if((dilogeNumber+1)%2==0)	
+	{
+	System.out.print(number1.getName()+" : ");
 	script.readDialogue(dilogeNumber,"ComicScript.csv");
 	
-	System.out.print(actors.get(number2).getName()+" : ");
+	System.out.print(number2.getName()+" : ");
 	script.readDialogue(dilogeNumber,"VillanScript.csv");
-	
+	System.out.println("\n");
+	}
+	else
+	{
+	System.out.print(number2.getName()+" : ");
+	script.readDialogue(dilogeNumber,"VillanScript.csv");
+
+	System.out.print(number1.getName()+" : ");
+	script.readDialogue(dilogeNumber,"ComicScript.csv");
+	System.out.println("\n");
+	}	
 }
 
 }
