0ddee383da17c11f2c73a74a994c6b439123b63e
Ashikafs <ashik.hussain@lxisoft.com>
2020-03-09 16:49:25 +0530

    http://jira.bytatech.com:8080/browse/IN-7247

--- MovieScript/com/lxisoft/movie/PlayMovie.java
+++ MovieScript/com/lxisoft/movie/PlayMovie.java
@@ -40,6 +40,7 @@ public void Play(ArrayList<Actor> actors,FileReppo script)
 	}while(count > 0);
 
 	MovieDetails(actors);
+
 }
 
 public void PlayDialoge(Actor a1,Actor a2,ArrayList<Dialogue> d1,ArrayList<Dialogue> d2,int dilogeNumber)
@@ -59,17 +60,49 @@ public void MovieDetails(ArrayList<Actor> actors)
 	System.out.println(actors.get(1).getName()+" : "+actors.get(1).getmovieRoll());
 }
 
-public void EditScript()
+public void EditScript(Actor a1,Actor a2,ArrayList<Dialogue> d1,ArrayList<Dialogue> d2)
 {
-	System.out.print("\nPress 1 To add Dialogue\nPress 2 to Delete a Dialogue\nPress 0 to Exit\n\n Enter Your Choise :");
+	System.out.print("\nPress 1 To Add Dialogue\nPress 2 to Delete a Dialogue\nPress 0 to Exit\n\n Enter Your Choise :");
 	int option=input.nextInt();
 	if(option==1)
-	{		addDialouge();	}	
+	{
+		System.out.println("Whose Dialogue you Want to Add \n1.jhon\n2.Arya");
+		int choise=input.nextInt();
+		if((choise-1)==0)
+		{
+			AddDialouge(a1,a2,d1,d2);
+		}
+		else
+		{
+			AddDialouge(a2,a1,d2,d1);
+		}
+	}	
 	if(option==2)
-	{		deleteDialouge();	}	
+	{	DeleteDialouge();	}	
 	else if(ch==0)
 	{		System.out.println("EXited");	}
 	else
-	{	System.out.println("You Entered A Rong Choise");}
+	{	System.out.println("You Entered A wrong Choise");}
 }
+
+public void AddDialouge(Actor a1,Actor a2,ArrayList<Dialogue> d1,ArrayList<Dialogue> d2,int n)
+{
+	
+		System.out.printl(a1.getName()+" : ");
+		d1.get(n).setdialogue(String dialogue)
+		System.out.println(a2.getName()+" : ");
+		d1.add.setdialogue(String dialogue)	
+	
+}
+public void DeleteDialouge(Actor a1,Actor a2,ArrayList<Dialogue> d1,ArrayList<Dialogue> d2,int n)
+{
+	
+		System.out.printl(a1.getName()+" : ");
+		d1.get(n).remove();
+		System.out.println(a2.getName()+" : ");
+		d1.remove();
+	
+}
+
+
 }
