4c70cdc40c92c5d03fffb0c3149c3ee004960afe
aswathy30 <aswathy.m@lxisoft.com>
2020-04-11 05:29:21 +0530

    http://jira.bytatech.com:8080/browse/IN-8728

--- SchoolProject/public class Students.java
+++ SchoolProject/public class Students.java
@@ -4,6 +4,7 @@ public class Students
 	int class;
 	int regno;
 	int percentage;
+	int m1,m2,m3,m4,m5;
 	public String printDetail()
 		{
 			return "\n \t \t  Name : "+s_name+"\n \t \t Class :"+class+"\n Register number :"+regno+"\n Percentage :"+percentage;
@@ -11,6 +12,12 @@ public class Students
 			
 
 
+		}
+
+		public percentage()
+		{
+				percentage=(m1+m2+m3+m4+m5)/5*100;
+				System.out.println("percentage");
 		}
 	
 }
