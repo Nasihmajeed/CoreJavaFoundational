c99ed31435b82fa714dbe22306cb99d53e332216
arun-k-paul <arun.k.paul@lxisoft.com>
2020-04-24 17:35:39 +0530

    http://jira.bytatech.com:8080/browse/IN-9502

--- SchoolProject/ClassRoom.java
+++ SchoolProject/ClassRoom.java
@@ -1,61 +1,17 @@
 public class ClassRoom
+import java.util.Scanner 
+
 {
 	
 	Student student1=new Student();
 	public void set1Student()
 	{ 
-	student1.name="arun";
-	student1.rNo=01;
-	System.out.println(student1.name);
-	System.out.println(student1.rNo);
+	Scanner student1=new Scanner(System.in);
+	System.out.println("enter name of the student");
+	String name=student1.nextline();
+	System.out.println("nameof the student is: "+name);
+}
+}
+
 
 	
-}
-
-	Student student2=new Student();
-	public void set2Student()
-	{ 
-	student2.name="babu";
-	student2.rNo=02;
-	System.out.println(student2.name);
-	System.out.println(student2.rNo);
-
-	
-}
-
-	Student student3=new Student();
-	public void set3Student()
-	{ 
-	student3.name="chandran";
-	student3.rNo=03;
-	System.out.println(student3.name);
-	System.out.println(student3.rNo);
-
-	
-}
-
-	Student student4=new Student();
-	public void set4Student()
-	{ 
-	student4.name="dundu";
-	student4.rNo=04;
-	System.out.println(student4.name);
-	System.out.println(student4.rNo);
-
-	
-}
-
-	Student student5=new Student();
-	public void set5Student()
-	{ 
-	student5.name="eapan";
-	student5.rNo=05;
-	System.out.println(student5.name);
-	System.out.println(student5.rNo);
-
-	
-}
-
-
-
-}
--- SchoolProject/Tdd.java
+++ SchoolProject/Tdd.java
@@ -3,8 +3,7 @@ public class Tdd
 public static void main (String array[])
 {
 	School school=new School();
-	System.out.println("name and roll no.s are:");
-	school.printStudent();
+	System.out.println("nameof the student is: "+name);
 	
 }	
 }
