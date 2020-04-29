c51d50aabc0af7d1ebc23c39961ebe9a8b457c80
arun-k-paul <arun.k.paul@lxisoft.com>
2020-04-29 15:01:08 +0530

    http://jira.bytatech.com:8080/browse/IN-9609

--- SchoolProject/ClassRoom.java
+++ SchoolProject/ClassRoom.java
@@ -10,14 +10,18 @@ public class ClassRoom
 	String name=s.nextLine();
 	System.out.println("enter roll no: ");
 	int rNo=s.nextInt();	
+	s.nextLine();
 	System.out.println("enter  semester one  mark: ");
 	int t1=s.nextInt();
+	s.nextLine();
 	System.out.println("enter  semester two  mark: ");
 	int t2=s.nextInt();
+	s.nextLine();
 	System.out.println("enter parent's contact number: ");
 	int parentNo=s.nextInt();
+	s.nextLine();
 	System.out.println("\n student detail: ");
-	System.out.println("\n name : "+name);
+	System.out.println(" name : "+name);
 	System.out.println("Roll no: "+rNo);
 	System.out.println("total marks= "+(t1+t2));
 	System.out.println("Parent's contact number= "+parentNo);
@@ -31,17 +35,22 @@ public class ClassRoom
 	String name=s.nextLine();
 	System.out.println("enter roll no: ");
 	int rNo=s.nextInt();	
+	s.nextLine();
 	System.out.println("enter  semester one  mark: ");
 	int t1=s.nextInt();
+	s.nextLine();
 	System.out.println("enter  semester two  mark: ");
 	int t2=s.nextInt();
+	s.nextLine();
 	System.out.println("enter parent's contact number: ");
 	int parentNo=s.nextInt();
+	s.nextLine();
 	System.out.println("\n student detail: ");
-	System.out.println("\n name : "+name);
+	System.out.println(" name : "+name);
 	System.out.println("Roll no: "+rNo);
 	System.out.println("total marks= "+(t1+t2));
 	System.out.println("Parent's contact number= "+parentNo);
+
      }
 
 	Student std3=new Student();
@@ -51,14 +60,18 @@ public class ClassRoom
 	String name=s.nextLine();
 	System.out.println("enter roll no: ");
 	int rNo=s.nextInt();	
+	s.nextLine();
 	System.out.println("enter  semester one  mark: ");
 	int t1=s.nextInt();
+	s.nextLine();
 	System.out.println("enter  semester two  mark: ");
 	int t2=s.nextInt();
+	s.nextLine();
 	System.out.println("enter parent's contact number: ");
 	int parentNo=s.nextInt();
+	s.nextLine();
 	System.out.println("\n student detail: ");
-	System.out.println("\n name : "+name);
+	System.out.println(" name : "+name);
 	System.out.println("Roll no: "+rNo);
 	System.out.println("total marks= "+(t1+t2));
 	System.out.println("Parent's contact number= "+parentNo);
@@ -68,18 +81,23 @@ public class ClassRoom
 	Student std4=new Student();
 	public void set4Student()
 	{ 
+
 	System.out.println("\n enter name of the student: ");
 	String name=s.nextLine();
 	System.out.println("enter roll no: ");
 	int rNo=s.nextInt();	
+	s.nextLine();
 	System.out.println("enter  semester one  mark: ");
 	int t1=s.nextInt();
+	s.nextLine();
 	System.out.println("enter  semester two  mark: ");
 	int t2=s.nextInt();
+	s.nextLine();
 	System.out.println("enter parent's contact number: ");
 	int parentNo=s.nextInt();
+	s.nextLine();
 	System.out.println("\n student detail: ");
-	System.out.println("\n name : "+name);
+	System.out.println(" name : "+name);
 	System.out.println("Roll no: "+rNo);
 	System.out.println("total marks= "+(t1+t2));
 	System.out.println("Parent's contact number= "+parentNo);
@@ -88,22 +106,28 @@ public class ClassRoom
 
 	Student std5=new Student();
 	public void set5Student()
-	{ 
-	System.out.println("\n enter name of the student: ");
+	{
+
+		System.out.println("\n enter name of the student: ");
 	String name=s.nextLine();
 	System.out.println("enter roll no: ");
 	int rNo=s.nextInt();	
+	s.nextLine();
 	System.out.println("enter  semester one  mark: ");
 	int t1=s.nextInt();
+	s.nextLine();
 	System.out.println("enter  semester two  mark: ");
 	int t2=s.nextInt();
+	s.nextLine();
 	System.out.println("enter parent's contact number: ");
 	int parentNo=s.nextInt();
+	s.nextLine();
 	System.out.println("\n student detail: ");
-	System.out.println("\n name : "+name);
+	System.out.println(" name : "+name);
 	System.out.println("Roll no: "+rNo);
 	System.out.println("total marks= "+(t1+t2));
 	System.out.println("Parent's contact number= "+parentNo);
+
      }
  }
 
