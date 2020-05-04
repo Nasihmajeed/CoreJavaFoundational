afbb8a8c149eb076d085aa8e01aa16424dded256
arun-k-paul <arun.k.paul@lxisoft.com>
2020-05-04 16:37:43 +0530

    http://jira.bytatech.com:8080/browse/IN-9609

--- SchoolProject/ClassRoom.java
+++ SchoolProject/ClassRoom.java
@@ -2,34 +2,37 @@ import java.util.Scanner;
 public class ClassRoom
 {
 	Scanner s=new Scanner(System.in);
-	int n,total=0,Std;
+	int n,i,total=0,Std;
+	public void setStudent()
+	{
 	System.out.println (" Enter number of students ");
 	n=s.nextInt();
-    Student std=new Student();
-    Student std=new Student[50];
-    std(i)=new Student()
-    
-     for (int i=1; i<=n; i++)
+    s.nextLine();
+    Student[] std=new Student[10];
+    for (int i=1; i<=n; i++)
      {
-        System.out.println("enter 1st student details: ");
+     	std[i]=new Student();
+        System.out.println("enter student details");
         System.out.println("enter name of the student: ");
-		String name=s.nextLine();
+		std[i].name=s.nextLine();
 		System.out.println("enter roll no: ");
-		int rNo=s.nextInt();	
+		std[i].rNo=s.nextInt();	
 		s.nextLine();
 		System.out.println("enter  semester one  mark: ");
-		int t1=s.nextInt();
+		std[i].t1=s.nextInt();
 		s.nextLine();
 		System.out.println("enter  semester two  mark: ");
-		int t2=s.nextInt();
+		std[i].t2=s.nextInt();
 		s.nextLine();
+		std[i].total=std[i].t1+std[i].t2;
 		System.out.println("enter parent's contact number: ");
-		int parentNo=s.nextInt();
+		std[i].parentNo=s.nextInt();
 		s.nextLine();
 		System.out.println("\n student detail: ");
-		System.out.println(" name : "+name);
-		System.out.println("Roll no: "+rNo);
-		System.out.println("total marks= "+(t1+t2));
-		System.out.println("Parent's contact number= "+parentNo);
+		System.out.println(" \n name : "+std[i].name);
+		System.out.println("Roll no: "+std[i].rNo);
+		System.out.println("total marks= "+std[i].total);
+		System.out.println("Parent's contact number= "+std[i].parentNo);
 	  }
+}
 }
--- SchoolProject/Student.java
+++ SchoolProject/Student.java
@@ -4,6 +4,7 @@ public class Student
     int rNo;
     int t1;
     int t2;
+    int total;
     int parentNo;
 }
 
