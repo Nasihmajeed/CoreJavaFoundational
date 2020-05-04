52e41b6e2620d3ef542fc5bb12358e2766e68212
faris-backer <faris.backer@lxisoft.com>
2020-05-04 19:42:11 +0530

    git log

--- SchoolProject/Classroom.java
+++ SchoolProject/Classroom.java
@@ -1,13 +1,38 @@
 import java.util.Scanner;
-import java.util.Array;
-public class Classroom
+public class ClassRoom
 {
-	Scanner s = new Scanner(System.in);
-	student[] stds ;
-	stds = new student[5] ;
-
-	
-
-
-
+	Scanner s=new Scanner(System.in);
+	int n,i,total=0,Std;
+	public void setStudent()
+	{
+	System.out.println (" Enter number of students ");
+	n=s.nextInt();
+    s.nextLine();
+    Student[] std=new Student[10];
+    for (int i=1; i<=n; i++)
+     {
+     	std[i]=new Student();
+        System.out.println("enter student details");
+        System.out.println("enter name of the student: ");
+		std[i].name=s.nextLine();
+		System.out.println("enter roll no: ");
+		std[i].rNo=s.nextInt();	
+		s.nextLine();
+		System.out.println("enter  semester one  mark: ");
+		std[i].t1=s.nextInt();
+		s.nextLine();
+		System.out.println("enter  semester two  mark: ");
+		std[i].t2=s.nextInt();
+		s.nextLine();
+		std[i].total=std[i].t1+std[i].t2;
+		System.out.println("enter parent's contact number: ");
+		std[i].parentNo=s.nextInt();
+		s.nextLine();
+		System.out.println("\n student detail: ");
+		System.out.println(" \n name : "+std[i].name);
+		System.out.println("Roll no: "+std[i].rNo);
+		System.out.println("total marks= "+std[i].total);
+		System.out.println("Parent's contact number= "+std[i].parentNo);
+	  }
+}
 }
