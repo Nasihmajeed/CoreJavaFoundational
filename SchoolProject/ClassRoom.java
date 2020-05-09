ae7b9b329beb12a3a690c05a270ff1a823e54b3f
arun-k-paul <arun.k.paul@lxisoft.com>
2020-05-09 18:54:06 +0530

    http://jira.bytatech.com:8080/browse/IN-9609

--- SchoolProject/ClassRoom.java
+++ SchoolProject/ClassRoom.java
@@ -59,7 +59,63 @@ public class ClassRoom
 		System.out.println("total marks= "+std[i].total);
 		System.out.println("Percentage= "+std[i].total/5);
 		System.out.println("Parent's contact number= "+std[i].parentNo);
+	}
 }
-}
-}
+		 public void set1Teacher()
+        {
+        	System.out.println("Enter faculty name: ");
+        	String tname=s.nextLine();
+        	System.out.println("faculty is : "+tname);
+        }
 
+	Student[] std1=new Student[10];
+
+	public void set1Student()
+	{
+	System.out.println (" \n Enter number of students ");
+	n=s.nextInt();
+    s.nextLine();
+    
+    for (int i=1; i<=n; i++)
+     {
+     	std1[i]=new Student();
+        System.out.println("\n enter student " +i+  " details");
+        System.out.println("\n enter name of the student: ");
+		std1[i].name=s.nextLine();
+		System.out.println("enter roll no: ");
+		std1[i].rNo=s.nextInt();	
+		s.nextLine();
+		System.out.println("enter  mark for subject 1: ");
+		std1[i].t1=s.nextInt();
+		s.nextLine();
+		System.out.println("enter  mark for subject 2: ");
+		std1[i].t2=s.nextInt();
+		s.nextLine();
+		System.out.println("enter  mark for subject 3: ");
+		std1[i].t3=s.nextInt();
+		s.nextLine();
+		System.out.println("enter  mark for subject 4: ");
+		std1[i].t4=s.nextInt();
+		s.nextLine();
+		System.out.println("enter mark for subject 5: ");
+		std1[i].t5=s.nextInt();
+		s.nextLine();
+		std1[i].total=std1[i].t1+std1[i].t2+std1[i].t3+std1[i].t4+std1[i].t5;
+		System.out.println("enter parent's contact number: ");
+		std1[i].parentNo=s.nextInt();
+		s.nextLine();
+		}
+	}
+	    public void print1Students()
+	    { 
+		for (int i=1; i<=n; i++)
+		{
+		System.out.println("\n student " +i+ " details are: ");
+		System.out.println(" \n name : "+std1[i].name);
+		System.out.println("Roll no: "+std1[i].rNo);
+		System.out.println("total marks= "+std1[i].total);
+		System.out.println("Percentage= "+std1[i].total/5);
+		System.out.println("Parent's contact number= "+std1[i].parentNo);
+}
+}
+}
