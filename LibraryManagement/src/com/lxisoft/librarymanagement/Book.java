96652c09ac5feace58e31f8e9079b6285d10aeda
megha-vt <megha.v.t@lxisoft.com>
2020-05-10 06:15:15 +0530

    folders and class created

---
+++ LibraryManagement/src/com/lxisoft/librarymanagement/Book.java
@@ -0,0 +1,25 @@
+package com.lxisoft.librarymanagement;
+public class Book
+{
+	private String name;
+	private String auther
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public String getName()
+	{
+		return name;
+	}	
+
+
+	public void setAuther(String auther)
+	{
+		this.auther = auther;
+	}
+	public String getAuther()
+	{
+		return auther;
+	}	
+}
---
+++ LibraryManagement/src/com/lxisoft/librarymanagement/Tdd.java
@@ -0,0 +1,7 @@
+package com.lxisoft.test;
+public class Tdd
+{
+	public static void main(String[] args) {
+		System.out.println("LIBRARY MANAGEMENT");
+	}
+}
