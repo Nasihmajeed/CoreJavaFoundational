cc4026cb8d607a0dcc277d37444aa00d358bc57c
Akhilesh-Narayanan <akhileshnarayanan.m.p@lxisoft.com>
2020-05-12 17:55:22 +0530

    Git

--- AnimalGame/src/com/lxisoft/Game/Forest.java
+++ AnimalGame/src/com/lxisoft/Game/Forest.java
@@ -18,7 +18,7 @@ public class Forest
 			Clear.cls();
 			System.out.println("\nGame Begin");
 			setAnimals();
-			printAnimalList();
+			//printAnimalList();
 		}
 	}
 
@@ -26,7 +26,8 @@ public class Forest
 	public void printAnimalList()
 	{
 		System.out.println("-----------Animal List-------------");
-		for(int i=0;i<6;i++)
+		int n=animals.size();
+		for(int i=0;i<n;i++)
 			{
 				System.out.println((animals.get(i)).getAnimalName()+" \t "+(animals.get(i)).animalEnergy + "\n" );
 			}
@@ -49,5 +50,68 @@ public class Forest
 		animals.get(3).setAnimalDetail("Wolf",5);
 		animals.get(4).setAnimalDetail("Rabbit",2);
 		animals.get(5).setAnimalDetail("Deer",3);
+		animalMeet();
 	}
+
+	public int randomNumber(int x)
+	{
+		Random rand = new Random();
+		int n = rand.nextInt(x);
+		return n;
+	}
+
+	public void animalMeet()
+	{
+		int r1,r2,n;
+		for(n=animals.size();n>1;)
+		{
+			r1=randomNumber(n);
+			r2=randomNumber(n);
+			if((r1==r2))
+			{
+				r2=randomNumber(n);
+			}
+			else
+			{
+				String animal1=animals.get(r1).getAnimalName();
+				String animal2=animals.get(r2).getAnimalName();
+				System.out.println(animal1+" Meets "+animal2);
+				int won = checkEnergy(r1,r2);
+				printResult(won,r1,r2);
+			}
+			n=animals.size();
+		}
+		System.out.println("Winner is " + animals.get(0).getAnimalName());
+	}
+
+
+	public int checkEnergy(int r1, int r2)
+	{
+		int energy1 = animals.get(r1).animalEnergy;
+		int energy2 = animals.get(r2).animalEnergy;
+
+		if(energy1>energy2)
+			return 1;
+		else if(energy2>energy1)	
+			return 2;
+		else
+			return 0;
+	}
+
+
+	public void printResult(int won,int r1,int r2)
+	 {
+	 	if(won==1)
+	 	{
+	 		System.out.println(animals.get(r1).getAnimalName() +" killed "+animals.get(r2).getAnimalName());
+			System.out.println("---------------------------------------------");
+			animals.remove(r2);
+	 	}
+	 	else if(won==2)
+	 	{
+	 		System.out.println(animals.get(r2).getAnimalName() +" killed "+animals.get(r1).getAnimalName());
+			System.out.println("---------------------------------------------");
+			animals.remove(r1);
+	 	}
+	 }
 }
