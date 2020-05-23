2d1f152224fcc51bab05f7adda6e65f084daa3e2
megha-vt <megha.v.t@lxisoft.com>
2020-05-23 18:44:35 +0530

    refactoring

--- LibraryManagement/src/com/lxisoft/test/Tdd.java
+++ LibraryManagement/src/com/lxisoft/test/Tdd.java
@@ -2,10 +2,10 @@ package com.lxisoft.test;
 import com.lxisoft.librarymanagement.*;
 public class Tdd
 {
-	static int ch;
+	
 	public static void main(String[] args) {
 		System.out.println("            !!LIBRARY MANAGEMENT!!\n           ************************");
-		Library lib=new Library();
-		lib.display(ch);
+		Librarian lib=new Librarian();
+		lib.display();
 	}
 }
