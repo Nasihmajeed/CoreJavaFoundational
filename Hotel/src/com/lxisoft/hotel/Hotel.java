e9bb97ca340d38a52ebbba370ee79ca4ff839b51
Aswathy <aswathy.m@lxisoft.com>
2020-06-06 11:15:56 +0530

    http://jira.bytatech.com:8080/browse/IN-6251

--- Hotel/src/com/lxisoft/hotel/Hotel.java
+++ Hotel/src/com/lxisoft/hotel/Hotel.java
@@ -1,51 +1,44 @@
 package com.lxisoft.hotel;
 import java.util.Scanner;
-public class Hotel{
+public class Hotel
+{
+	Menu menu=new Menu();
+    Order order=new Order();
+    String[] food =new String[10];
+Scanner input = new Scanner(System.in);
+public void printHotel()
+     {
+      
+    System.out.println("       ************************************************************************");
+    System.out.println("       *                                                                      *");
+    System.out.println("       *                          NOOR JAHAN                              *");   
+    System.out.println("       *                                            *");
+    System.out.println("       *                          PALAKKAD          *");
+    System.out.println("       *                          VEG AND NON VEG                                            *");
+    System.out.println("       ************************************************************************");
+    System.out.println("");
+    System.out.println("");
 
-	
-	
-	
-	public void printHotelDetails(){
-		Scanner scanner=new Scanner(System.in);
-		System.out.println("**************************************************************************************************");
-		System.out.println("                                                                                                 ");
-		System.out.println("                                                                                                 ");
-		System.out.println("                                                                                                 ");
-		System.out.println("                                                                                                 ");
-		System.out.println("                                                                                                 ");
-		System.out.println("                                                                                                 ");
-		System.out.println("                                                                                                 ");
-		System.out.println("                                                                                                 ");
-		System.out.println("                                                                                                 ");
-		System.out.println("                                                                                                 ");
-		System.out.println("**************************************************************************************************");
-
-		System.out.println(" **************************HOTEL NOORJAHAN**************************************");
-		System.out.println("                                                                                                 ");
-		System.out.println("                                                                                                 ");
-		System.out.println("                                                                                                 ");
-		System.out.println("                                                                                                 ");
-		System.out.println("                                                                                                 ");
-
-		System.out.println("*******************VEG  AND NONVEG**************************************");
-		System.out.println("                                                                                                 ");
-		System.out.println("                                                                                                 ");
-		System.out.println("                                                                                                 ");
-		System.out.println("                                                                                                 ");
-		System.out.println("                                                                                                 ");
-
-
-			System.out.println("*********************PALAKKAD****************************************");
-		Menu menu=new Menu();
-		menu.printMenuDetails();
-
-		Order order=new Order();
-		order.getOrderDetails();
-
-		Admin admin=new Admin();
-
-		
-	}
-
-	
 }
+public void printDetail()
+{   int choi=0;
+    do{
+      System.out.println("\n________________________");
+        System.out.println("\n\n1. User Menu \n\n2. Manager \n\n Press 0 for Exit \n");
+        choi=input.nextInt();
+        if(choi==1)
+        {
+	       menu.printMenu(food);
+        }
+        if(choi==2)
+        {
+           order.viewOrder(food);
+        }
+        else if(choi==0)
+        {
+            System.out.println(" ");
+        }
+    }while(choi>0);
+}
+}
+   
--- Hotel/src/com/lxisoft/hotel/Menu.java
+++ Hotel/src/com/lxisoft/hotel/Menu.java
@@ -1,27 +1,37 @@
 package com.lxisoft.hotel;
 import java.util.Scanner;
-public class Menu{
+public class Menu
+{	
+    Order order=new Order();
+    
+     Scanner scnr = new Scanner(System.in);
+        boolean quit= false;
+        int sum=0,i=0;        
+        int chickenmandhi=550,chickenbiriyani=120,alfahm=400,shavayi=430,softdrinks=50;
 
-	public void printMenuDetails(){
-		System.out.println("**** MENU ****");
-		 
-        System.out.println(" 1- Chicken Biriyani   " + "100" );
-
-        System.out.println(" 2- Veg biriyani    " + "80" );
-
-        System.out.println(" 3- Regular meals   " + "70" );
-
-         System.out.println(" 4- Quit" );
-
-
-         System.out.println("WHAT WOULD YOU LIKE TO HAVE");
-
-
-
+        public void printMenu(String[] food)
+        {
+        String order="";
+            do
+            {
+            System.out.println("ITEM"+"\t\t\t"+"Price");
+            System.out.println("1.Chicken Mandhi"+"\t\t\t\t"+"550");
+            System.out.println("2.Chicken Biriyani"+"\t\t\t"+"120");
+            System.out.println("3.Alfahm"+"\t\t\t\t"+"400");
+            System.out.println("4.Shavayi"+"\t\t\t\t"+"430");
+            System.out.println("5.Soft Drinks"+"\t\t\t\t"+"50");
+            System.out.println("6.Quit");
+            System.out.print("Enter the Choice \t:");
+            int choice=scnr.nextInt();
+            order.createOrder(food,choice,i);
+            }
+            while(i==0);
+            
+            
+        }
 
         
-		}
-
-	}
-	
-
+        
+    
+    }
+    
