7466bb04b6713f0cf2928b4d6afe7a2ccf071043
arun-k-paul <arun.k.paul@lxisoft.com>
2020-06-10 12:53:00 +0530

    http://jira.bytatech.com:8080/browse/IN-6355

--- Hotel/src/com/LxiSoft/Hotel/Menu.java
+++ Hotel/src/com/LxiSoft/Hotel/Menu.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import com.LxiSoft.Admin.Admin;
 public class Menu
 {	
+	int n;
      Scanner scnr = new Scanner(System.in);
 ArrayList<Item> theMenu = new ArrayList<Item>();
      public void myMenu()
@@ -20,16 +21,41 @@ ArrayList<Item> theMenu = new ArrayList<Item>();
      	theMenu.add(new Item());
      	theMenu.get(3).setName("Juice");
      	theMenu.get(3).setPrize(30);
+     	System.out.println("MENU    \t prize");
      	for (int i=0; i<theMenu.size(); i++)
-     	{
-     		System.out.println(theMenu.get(i).getName()+""+theMenu.get(i).getPrize());
+     	{	
+     		System.out.println("\n" +theMenu.get(i).getName()+"\t \t"+theMenu.get(i).getPrize());
      	}
+     }
        	
-  
-  }
+
       public void addItem()
       {
-     
-     
-   }
+
+      	System.out.println (" \n Enter number of items to be added ");
+	      n=scnr.nextInt();
+         scnr.nextLine();
+    
+    for (int i=0; i<=n; i++)
+
+    {
+    	    	   	theMenu.add(new Item());
+
+    	if(theMenu.get(i).getName()==null)
+
+    	{
+        System.out.println("\n enter item name");
+       String name=scnr.nextLine();
+       theMenu.get(i).setName(name);
+       scnr.nextLine();
+      
+       System.out.println("\n enter item prize");
+       int prize=scnr.nextInt();
+       theMenu.get(i).setPrize(prize);
+         }
+         
+        }
+     }
+
+		
 }
