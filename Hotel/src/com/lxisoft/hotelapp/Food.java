615bda190878a5105a8d51624ee04af396a682d3
faris-backer <faris.backer@lxisoft.com>
2020-06-10 19:26:40 +0530

    http://jira.bytatech.com:8080/browse/IN-6405

---
+++ Hotel/src/com/lxisoft/hotelapp/Food.java
@@ -0,0 +1,40 @@
+package com.Lxisoft.Hotelapp;
+import java.util.java;
+public class Food
+{
+	private String name;
+	private int price;
+	private String code;
+
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+	public String getName()
+	{
+		return name;
+	}
+
+
+	public void setPrice(int price)
+	{
+		this.price=price
+	}
+	public int getPrice()
+	{
+		return price 
+	}
+
+
+
+	public void setCode(String code)
+	{
+		this.code=code;
+	}
+	public String getCode()
+	{
+		return code;
+	}
+
+	
+}
--- Hotel/src/com/lxisoft/hotelapp/Hotel.java
+++ Hotel/src/com/lxisoft/hotelapp/Hotel.java
@@ -1,45 +1,46 @@
-package com.lxisoft.hotelapp;
+package com.Lxisoft.Hotelapp;
 import java.util.Scanner;
-public class Hotel
+public class hotel
 {
 	Menu menu=new Menu();
-    Order order=new Order();
-    String[] food =new String[10];
-Scanner input = new Scanner(System.in);
-public void printHotel()
-     {
-      
-    System.out.println("       ************************************************************************");
-    System.out.println("       *                                                                      *");
-    System.out.println("       *                          Top Chicken                                 *");   
-    System.out.println("       *                    Multi Cuisine Restuarent                          *");
-    System.out.println("       *         Mele Pattambi, Palakkad Road, Near juma Masjid               *");
-    System.out.println("       *                                                                      *");
-    System.out.println("       ************************************************************************");
-    System.out.println("");
-    System.out.println("");
-    
+	Scanner s=new Scanner(System.in);
+	public void printHotel()
+	{
+		System.out.println("       ************************************************************************");
+    	System.out.println("       *                                                                      *");
+    	System.out.println("       *                          Top Chicken                                 *");   
+    	System.out.println("       *                    Multi Cuisine Restuarent                          *");
+    	System.out.println("       *         Mele Pattambi, Palakkad Road, Near juma Masjid               *");
+    	System.out.println("       *                                                                      *");
+    	System.out.println("       ************************************************************************");
+		System.out.println("");
+	}
+	public void printDetails()
+	{
+		do
+		{
+			 System.out.println("\n________________________");
+			 System.out.println("\n\n1. Customer Menu \n\n2. Admin \n\n3. Employees \n\n Press 0 for Exit \n");
+			 choice=input.nextInt();
+			 if(choice==1)
+			 {
+			 	menu.printMenu();
+			 }
+			 if(choice==2)
+			 {
+			 	admin.printAdmin();
+			 }
+			 if(choice==3)
+			 {
+			 	employee.printDetails();
+			 }
+			 else if(choice==0)
+			 {
+			 	System.out.println(" ");
+			 }
 
+		while(choice>0);
+
+		}
+	}
 }
-public void printDetail()
-{   int choi=0;
-    do{
-      System.out.println("\n________________________");
-        System.out.println("\n\n1. User Menu \n\n2. Manager \n\n Press 0 for Exit \n");
-        choi=input.nextInt();
-        if(choi==1)
-        {
-	       menu.printMenu(food);
-        }
-        if(choi==2)
-        {
-           order.viewOrder(food);
-        }
-        else if(choi==0)
-        {
-            System.out.println(" ");
-        }
-    }while(choi>0);
-}
-}
-   
