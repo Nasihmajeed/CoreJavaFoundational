abd2f4f8a12355dafc68c03dc4c8fa40e9f92c3b
arun-k-paul <arun.k.paul@lxisoft.com>
2020-06-13 18:38:06 +0530

    http://jira.bytatech.com:8080/browse/IN-6507

--- Hotel/src/com/LxiSoft/Hotel/Menu.java
+++ Hotel/src/com/LxiSoft/Hotel/Menu.java
@@ -23,7 +23,7 @@ ArrayList<Item> theMenu = new ArrayList<Item>();
      	theMenu.get(3).setPrize(30);
      	
      }
-     
+
      public void printMenu()
      {
      	System.out.println("MENU    \t prize");
@@ -36,7 +36,7 @@ ArrayList<Item> theMenu = new ArrayList<Item>();
 
       public void addItem()
       {
-	       	theMenu.add(new Item());
+	     /*  	theMenu.add(new Item());
 
       	System.out.println (" \n Enter number of items to be added ");
 	      n=scnr.nextInt();
@@ -59,15 +59,15 @@ ArrayList<Item> theMenu = new ArrayList<Item>();
        theMenu.get(i).setPrize(prize);
          }
          
-        }
+        }*/
 
      }
     public void deleteItem()
    {
-  /* 	theMenu.remove(new Item());
+  	theMenu.remove(new Item());
   	System.out.println("Enter index no. of item to be deleted");
 
-   	theMenu.get(i).getName(i)=scnr.nextInt()  ;             */
+   	theMenu.get(i).getName()=scnr.nextInt()  ;             
     } 
 
 		
