e6c56650c2271177e1aa80fb761d6f7d56772c36
unknown <aswathy.m@lxisoft.com>
2020-07-02 09:29:31 +0530

    http://jira.bytatech.com:8080/browse/IN-7239

--- Hotel/src/com/lxisoft/hotel/Menu.java
+++ Hotel/src/com/lxisoft/hotel/Menu.java
@@ -15,15 +15,32 @@ public class Menu{
 
         menuList.add(new Food());
         menuList.get(0).setFoodName("VEG MEALS");
-        menuList.get(0).setFoodPrice(10);
+        menuList.get(0).setFoodPrice(60);
 
         menuList.add(new Food());
         menuList.get(1).setFoodName("NON VEG MEALS");
-        menuList.get(1).setFoodPrice(15);
+        menuList.get(1).setFoodPrice(80);
+
+        menuList.add(new Food());
+        menuList.get(2).setFoodName("VEG BIRIYANI");
+        menuList.get(2).setFoodPrice(100);
+
+        menuList.add(new Food());
+        menuList.get(3).setFoodName("NON BIRIYANI");
+        menuList.get(3).setFoodPrice(150);
+
+
+
 
         for(int i=0;i<menuList.size();i++){
             System.out.println("\n"+menuList.get(i).getFoodName()+"\t \t"+menuList.get(i).getFoodPrice());  
             }
+        }
+
+        public void addDynamically(){
+
+            System.out.println("Enter the food items to be added");
+            String element=menuList.get(4).setFoodName();
         }
     }
       
