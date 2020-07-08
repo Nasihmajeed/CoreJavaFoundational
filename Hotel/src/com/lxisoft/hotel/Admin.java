839a50df1006f2b7aefd8a085f24adbbb65c5ff8
Aswathy <aswathy.m@lxisoft.com>
2020-07-08 06:10:10 +0530

    http://jira.bytatech.com:8080/browse/IN-7501

--- Hotel/src/com/lxisoft/hotel/Admin.java
+++ Hotel/src/com/lxisoft/hotel/Admin.java
@@ -12,8 +12,46 @@ public class Admin{
 	
 
 	public void adminFunctions(){
-		
+		System.out.println("**********************************");
+		System.out.println("\n \n  ***THE CURRENT MENU IS***");
 		menu.printMenuDetails();
+		boolean quit=false;
+
+		do{
+
+			System.out.println("\n \n ***ENTER YOUR DESIRED OPERATION TO PERFORM***");
+			System.out.println("1.	ADD");
+			System.out.println("2.	DELETE");
+			System.out.println("3.	UPDATE");
+			System.out.println("4.	TO EXIT");
+
+			System.out.println("Enter your choice");
+			int choice=scanner.nextInt();
+			
+		
+			switch(choice){
+				case 1:
+				menu.addDynamically();
+				break;
+				case 2:
+				menu.removeDynamically();
+				break;
+				case 3:
+				menu.modifyNameDynamically();
+				break;
+				case 4:
+				System.out.println("Wrong input");
+				default:
+			
+
+		
+			}
+		}
+		 System.out.println("Exited from admin option");
+
+
+		
+		
 	}
 }
 	
--- Hotel/src/com/lxisoft/hotel/Hotel.java
+++ Hotel/src/com/lxisoft/hotel/Hotel.java
@@ -4,8 +4,7 @@ public class Hotel{
 
 
         Admin admin=new Admin();
-        Menu menu=new Menu();
-
+        
         
 
     
@@ -32,13 +31,31 @@ public class Hotel{
         System.out.println("                                                                                                 ");
         System.out.println("*******************************************************************");
 
-        menu.printMenuDetails();
+     //   menu.printMenuDetails();
+        System.out.println("    1.  Admin");
+        System.out.println("    2.  Menu");
+        System.out.println("    3.  Quit");
+
+      
+        System.out.println("Enter your choice");
+          int choice=scanner.nextInt();
+          switch(choice){
+            case 1:
+            admin.adminFunctions();
+          }
+
+
+
+
+
+
+       /* menu.printMenuDetails();
         menu.addDynamically();
         
         menu.getOrderDetails();
         menu.removeDynamically();
         menu.modifyNameDynamically();
-        menu.modifyPriceDynamically();
+        menu.modifyPriceDynamically();*/
 
       
 
