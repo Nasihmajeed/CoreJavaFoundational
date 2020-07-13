37c1616e0d12573b2d86a220ee943d36317c4c8b
vishnu-01 <vishnunath.n@lxisoft.com>
2020-07-13 18:09:26 +0530

    http://jira.bytatech.com:8080/browse/IN-7811

--- CarProject/TDD.java
+++ CarProject/TDD.java
@@ -1,22 +1,34 @@
+import java.util.Scanner;
 public class TDD
 {
 	public static void main(String[] args)
 	{
+		Scanner in = new Scanner(System.in);
 		Car car1 = new Car();
 		car1.driver1 = new Driver();
-		car1.name = "Maruthi";
-		car1.regNo = "KL 09 AM 35";
-		car1.model = 2016;
-		car1.driver1.name = "Suresh";
-		car1.driver1.id = 1234;
+		System.out.println("enter the name of car: ");
+		car1.name = in.next();
+		System.out.println("enter the register number of car: ");
+		car1.regNo = in.next();
+		System.out.println("enter the model/year of car: ");
+		car1.model = in.nextInt();
+		System.out.println("enter the name of car driver: ");
+		car1.driver1.name = in.next();
+		System.out.println("enter the id of car driver:");
+		car1.driver1.id = in.nextInt();
 		
 		Car car2 = new Car();
 		car2.driver2 = new Driver();
-		car2.name = "Ford";
-		car2.regNo = "KL 01 BZ 4710";
-		car2.model = 2020;
-		car2.driver2.name = "Rajesh";
-		car2.driver2.id = 1783;
+		System.out.println("enter the name of car: ");
+		car2.name = in.next();
+		System.out.println("enter the register number of car: ");
+		car2.regNo = in.next();
+		System.out.println("enter the model/year of car: ");
+		car2.model = in.nextInt();
+		System.out.println("enter the name of the driver: ");
+		car2.driver2.name = in.next();
+		System.out.println("enter the id of car driver: ");
+		car2.driver2.id = in.nextInt();
 
 		System.out.println("Car Name: "+car1.name);
 		System.out.println("Car Register No: "+car1.regNo);
