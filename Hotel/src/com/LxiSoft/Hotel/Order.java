10ca188fe2d9b34762da63c65df6f5745a1b3e2c
arun-k-paul <arun.k.paul@lxisoft.com>
2020-07-16 20:03:10 +0530

    http://jira.bytatech.com:8080/browse/IN-8061

--- Hotel/src/com/LxiSoft/Hotel/Order.java
+++ Hotel/src/com/LxiSoft/Hotel/Order.java
@@ -5,6 +5,22 @@ public class Order
 {
          Scanner scnr = new Scanner(System.in);
         Menu menu=new Menu();
-      
+         public void getOrder()
+         {
+    
+ 
+    System.out.println("GIVE YOUR indexNO. of ORDER PLEASE");
+    n=scnr.nextInt();
+
+  //  if (n>0)
+   // {
+    ArrayList<Item>billPrint=new ArrayList<Item>();
+    billPrint.add(new Item());
+     billPrint.get(n).getName();
+   billPrint.get(n).getPrize();
+      System.out.println(billPrint);  
+
+ // }
+}
         }
     
