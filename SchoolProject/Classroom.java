a95e888f84f6f6d29382ae68a3b9471dd4d2417e
vishnu-01 <vishnunath.n@lxisoft.com>
2020-07-22 18:11:42 +0530

    http://jira.bytatech.com:8080/browse/IN-8397

--- SchoolProject/Classroom.java
+++ SchoolProject/Classroom.java
@@ -5,6 +5,9 @@ public class Classroom
 	int division;
 	String dept;
 	Student[] student = new Student[3];
+	Student[] sub1 = new Sub();
+	Student[] sub2 = new Sub();
+	Student[] sub3 = new sub();
 	Scanner scanner;
 	int i;
 
@@ -18,7 +21,15 @@ public class Classroom
       System.out.println("enter the register number: ");
       student[i].regNo = scanner.nextInt();
       System.out.println("enter the name of the student: ");
-      student[i].name = scanner.next(); 
+      student[i].name = scanner.next();
+      System.out.println("enter the mark of physics: ");
+      student[i].sub1 = scanner.nextInt();
+      System.out.println("enter the mark of chemistry: ");
+      student[i].sub2 = scanner.nextInt();
+      System.out.println("enter the mark of maths: ");
+      student[i].sub3 = scanner.nextInt(); 
+      student[i].total = student[i].sub1+student[i].sub2+student[i].sub3;
+      student[i].percentage = (student[i].total/300)*100;
       System.out.println(" \n ");
         }
     }
@@ -29,6 +40,11 @@ public class Classroom
      	{
         System.out.println("register number: "+student[i].regNo);
         System.out.println("name: "+student[i].name);
+        System.out.println("physics: "+student[i].sub1);
+        System.out.println("chemistry: "+student[i].sub2);
+        System.out.println("maths: "+student[i].sub3);
+        System.out.println("total mark: "+student[i].total);
+        System.out.println("percentage: "+student[i].percentage);
         System.out.println(" \n ");
         }
      }
--- SchoolProject/Student.java
+++ SchoolProject/Student.java
@@ -2,4 +2,9 @@ public class Student
 {
 	int regNo;
 	String name;
+	int sub1;
+	int sub2;
+	int sub3;
+	int total;
+	float percentage;
 }
