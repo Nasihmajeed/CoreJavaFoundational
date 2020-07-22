106f794dbbfc55da28a9b8cd4e27b254a44eb4db
Aswathy <aswathy.m@lxisoft.com>
2020-07-22 07:38:21 +0530

    http://jira.bytatech.com:8080/browse/IN-8405

--- AnimalGame/src/com/lxisoft/forest/Forest.java
+++ AnimalGame/src/com/lxisoft/forest/Forest.java
@@ -1,10 +1,29 @@
 package com.lxisoft.forest;
+import java.util.Scanner;
 public class Forest{
 
-	Animal animals=new Animals();
+	//Animal animals=new Animal();
+	 ArrayList<Animal> animalList = new ArrayList<Animal>();
 
 	public void forestDetails(){
-		
+
+		animalList.add(new Animal());
+        animalList.get(0).setAnimalName("Tiger");
+
+        animalList.add(new Animal());
+        animalList.get(1).setAnimalName("Lion");
+
+        animalList.add(new Animal());
+        animalList.get(2).setAnimalName("Rabbit");
+
+        animalList.add(new Animal());
+        animalList.get(3).setAnimalName("Deer");
+
+
+         for(int i=0;i<animalList.size();i++){
+            System.out.println("\n"+animalList.get(i).getAnimalName());  
+            }
+        
 	}
 	
 }
--- AnimalGame/src/com/lxisoft/test/TDD.java
+++ AnimalGame/src/com/lxisoft/test/TDD.java
@@ -2,6 +2,8 @@ package com.lxisoft.test;
 public class TDD{
 
 	public static void main(String[] args){
+
+		Forest forest =new Forest();
 		forest.forestDetails();
 	}
 	
