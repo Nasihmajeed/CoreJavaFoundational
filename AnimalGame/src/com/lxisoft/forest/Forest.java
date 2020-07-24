1bb8930c816f440bfee69e4f4af083bbc90c91ee
Aswathy <aswathy.m@lxisoft.com>
2020-07-25 01:17:27 +0530

    http://jira.bytatech.com:8080/browse/IN-8645

--- AnimalGame/src/com/lxisoft/forest/Forest.java
+++ AnimalGame/src/com/lxisoft/forest/Forest.java
@@ -7,7 +7,9 @@ public class Forest{
 
 	//Animal animals=new Animal();
 	public ArrayList<Animal> animalList = new ArrayList<Animal>();
+    Scanner scanner=new Scanner(System.in);
     Random random=new Random();
+    int choice=1;
 
 
     public void printDetails(){
@@ -40,7 +42,9 @@ public class Forest{
         System.out.println("                                                                                                 ");
 
         System.out.println("                                WELCOME TO THE GAME                                              ");
-        System.out.println("
+        System.out.println("                                                                                                 ");
+
+
                     
     }
 
@@ -49,6 +53,12 @@ public class Forest{
 
 	public void forestDetails(){
 
+        System.out.println("************************************************************************************************");
+        System.out.println("************************************************************************************************");
+        System.out.println("************************************************************************************************");
+       
+        System.out.println("                                 PARTICIPANTS OF GAME ARE                                       ");  
+              
 		animalList.add(new Animal());
         animalList.get(0).setAnimalName("Tiger");
 
@@ -65,38 +75,60 @@ public class Forest{
         for(int i=0;i<animalList.size();i++){
 
 
-        System.out.println(animalList.get(i).getAnimalName());
+        System.out.println("                                                                                                 ");
+        System.out.println("                                                                                                 ");
+        System.out.println(" \n                                              "+animalList.get(i).getAnimalName()+"           ");
     }
+
+         System.out.println("************************************************************************************************");
+        System.out.println("************************************************************************************************");
+        System.out.println("************************************************************************************************");
 
        
           
 	}
 
 
+
         public void randomSelection(){
-             int randomNumber = random.nextInt(4); 
+
+
+            System.out.println("PRESS 1 TO START THE GAME");
+            int choice=scanner.nextInt();
+            if(choice==1){
+
+            //nt randomNumber = random.nextInt(4);   
+             int randomItem= random.nextInt(4);
+              int randomNumber = random.nextInt(4);   
              
         
             for(int i=0;i<1;i++){
 
-             System.out.println(" "+randomNumber);
-             
-             System.out.println(animalList.get(randomNumber).getAnimalName());
-            
-            }        
+                 System.out.println(" "+randomNumber);
+               System.out.println(" "+randomItem);
+
+                
+                if(randomNumber!=randomItem){
+                 System.out.println(animalList.get(randomNumber).getAnimalName());
+                 System.out.println(animalList.get(randomItem).getAnimalName());
+                }
+                else{
+                    for(int j=0;j<animalList.size();j++){
+                        randomNumber=random.nextInt(4);
+                         System.out.println(animalList.get(randomNumber).getAnimalName());
+                         System.out.println(animalList.get(randomItem).getAnimalName());
+
+
+                    }
+                }
+                }        
+            }
+
+            else{
+                System.out.println("EXITED FROM ANIMAL GAME");
+            }
         }
 
-        public void randomSelection1(){
-            int randomItem= random.nextInt(4);
-             for(int i=0;i<1;i++){
-
-            
-             System.out.println(" "+randomItem);
-             
-             System.out.println(animalList.get(randomItem).getAnimalName());
-            }  
-
-        }
-
+       
    
 }
