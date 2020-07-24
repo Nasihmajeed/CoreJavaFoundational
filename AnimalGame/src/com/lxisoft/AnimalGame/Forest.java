d919bff6e4e7791de620919407cce2f119bd8396
faris-backer <faris.backer@lxisoft.com>
2020-07-24 19:53:19 +0530

    http://jira.bytatech.com:8080/browse/IN-8601

--- AnimalGame/src/com/lxisoft/AnimalGame/Forest.java
+++ AnimalGame/src/com/lxisoft/AnimalGame/Forest.java
@@ -6,5 +6,13 @@ public class Forest
     Animal animal =new Animal();
     Scanner scnr = new Scanner(System.in);
 
+    public void welcomeScree()
+    {
+    	System.out.println("---------------------------------");
+    	System.out.println("---------- Animal Game ----------");
+    	System.out.println("---------------------------------");
+
+    }
+
 	
 }
