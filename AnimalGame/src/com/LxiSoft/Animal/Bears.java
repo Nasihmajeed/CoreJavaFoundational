400524e0c7a55ded883f5c87e83a897508a33343
arun-k-paul <arun.k.paul@lxisoft.com>
2020-08-06 18:09:33 +0530

    http://jira.bytatech.com:8080/browse/IN-9147

--- AnimalGame/src/com/LxiSoft/Animal/Bears.java
+++ AnimalGame/src/com/LxiSoft/Animal/Bears.java
@@ -2,25 +2,4 @@ package com.LxiSoft.Animal;
 import com.LxiSoft.Forest.*;
 public class Bears extends Animal implements Omnivorus
 {
-	/*private String name;
-	private int animalStrength;
-
-	public void setName(String name)
-
-{
-	this.name=name;
-}
-public String getName()
-{
-	return name;
-}
-
-public void setAnimalStrength(int a)
-{
-	this.animalStrength=a;
-}
-public int getAnimalStrength()
-{
-	return animalStrength;
-}*/
 }
