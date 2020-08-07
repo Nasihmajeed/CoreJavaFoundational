13f972bae36a5cc0ae67d001fea745cf8b1637d0
arun-k-paul <arun.k.paul@lxisoft.com>
2020-08-07 18:01:09 +0530

    http://jira.bytatech.com:8080/browse/IN-9207

--- AnimalGame/src/com/LxiSoft/Forest/Forest.java
+++ AnimalGame/src/com/LxiSoft/Forest/Forest.java
@@ -41,44 +41,44 @@ public class Forest
         animal.add(new Bears());
         animal.add(new Lion());
 
-		animal.get(0).setName("Tiger");
+		animal.get(0).setName("Tiger1");
         animal.get(0).setAnimalStrength(85);
         animal.get(0).setAnimalLife(true);
 
-		animal.get(1).setName("Deer");
+		animal.get(1).setName("Deer1");
         animal.get(1).setAnimalStrength(40);
         animal.get(1).setAnimalLife(true);
 
-		animal.get(2).setName("Rabbit");
+		animal.get(2).setName("Rabbit1");
        animal.get(2).setAnimalStrength(35);
            animal.get(2).setAnimalLife(true);
 
-		animal.get(3).setName("Bear");
+		animal.get(3).setName("Bear1");
         animal.get(3).setAnimalStrength(65);
     animal.get(3).setAnimalLife(true);
 
-		animal.get(4).setName("Lion");
+		animal.get(4).setName("Lion1");
         animal.get(4).setAnimalStrength(95);
     animal.get(4).setAnimalLife(true);
 
-            animal.get(5).setName("Tiger");
+  animal.get(5).setName("Tiger2");
         animal.get(5).setAnimalStrength(85);
     animal.get(5).setAnimalLife(true);
 
 
-        animal.get(6).setName("Deer");
+        animal.get(6).setName("Deer2");
         animal.get(6).setAnimalStrength(40);
     animal.get(6).setAnimalLife(true);
 
-        animal.get(7).setName("Rabbit");
+        animal.get(7).setName("Rabbit2");
        animal.get(7).setAnimalStrength(35);
     animal.get(7).setAnimalLife(true);
 
-        animal.get(8).setName("Bear");
+        animal.get(8).setName("Bear2");
         animal.get(8).setAnimalStrength(65);
     animal.get(8).setAnimalLife(true);
 
-        animal.get(9).setName("Lion");
+        animal.get(9).setName("Lion2");
         animal.get(9).setAnimalStrength(95);
     animal.get(9).setAnimalLife(true);
 
