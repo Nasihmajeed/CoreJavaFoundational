11fbc3bc1cfcda1cdafcedc0fc788df32f7a9456
faris-backer <faris.backer@lxisoft.com>
2020-08-07 20:19:07 +0530

    http://jira.bytatech.com:8080/browse/IN-9215

--- AnimalGame/src/com/lxisoft/Animals/Bear.java
+++ AnimalGame/src/com/lxisoft/Animals/Bear.java
@@ -1,15 +1,8 @@
 package com.lxisoft.AnimalGame;
 public class Deer extends Animal implements Omnivorous
 {
-	public void eat(int energy)
-	{
+	
 
-	}
-
-	public void kill()
-	{
-
-	}
 
 	public void fight()
 	{
--- AnimalGame/src/com/lxisoft/Animals/Tiger.java
+++ AnimalGame/src/com/lxisoft/Animals/Tiger.java
@@ -3,7 +3,12 @@ public class Tiger extends Animal implements Carnivorous
 {
 	public void eat(int energy)
 	{
-	
+		this.animalEnergy=this.animalEnergy+(int)(energy*.15);
+		if(this.animalEnergy>100)
+			animalEnergy=100;
+		else if(this.animalEnergy<0)
+			animalEnergy=0;
+
 	}
 
 
