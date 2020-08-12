9a12d76bd6a7ec4ec3d49b2ca55d7b65b9f02513
Aswathy <aswathy.m@lxisoft.com>
2020-08-12 07:26:48 +0530

    http://jira.bytatech.com:8080/browse/IN-9404

--- AnimalGame/src/com/lxisoft/forest/Forest.java
+++ AnimalGame/src/com/lxisoft/forest/Forest.java
@@ -335,6 +335,7 @@ public class Forest{
                     }
       
         System.out.println("\n **************************************");
+	System.out.println("\n **************************************");
     }
 
     
