a60ece484589c0c17bafb2f6e0fe920801a2a7ec
faris-backer <faris.backer@lxisoft.com>
2020-08-24 17:47:58 +0530

    http://jira.bytatech.com:8080/browse/IN-9670

--- AnimalGame/src/com/lxisoft/Forest/Forest.java
+++ AnimalGame/src/com/lxisoft/Forest/Forest.java
@@ -82,6 +82,26 @@ public class Forest
         animal.get(9).setAnimalStrength(95);
     animal.get(9).setAnimalLife(true);
 
+    animal.get(0).setName("Tiger3");
+        animal.get(0).setAnimalStrength(85);
+        animal.get(0).setAnimalLife(true);
+
+    animal.get(1).setName("Deer3");
+        animal.get(1).setAnimalStrength(40);
+        animal.get(1).setAnimalLife(true);
+
+    animal.get(2).setName("Rabbit3");
+       animal.get(2).setAnimalStrength(35);
+           animal.get(2).setAnimalLife(true);
+
+    animal.get(3).setName("Bear3");
+        animal.get(3).setAnimalStrength(65);
+    animal.get(3).setAnimalLife(true);
+
+    animal.get(4).setName("Lion3");
+        animal.get(4).setAnimalStrength(95);
+    animal.get(4).setAnimalLife(true);
+
 		System.out.println("\t Animals in the forest are ");
      	for (int i=0; i<animal.size(); i++)
      	{	
