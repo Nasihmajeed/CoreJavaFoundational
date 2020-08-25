575d2182745c8b8dceba1f5ba3099fc57735b8d6
arun-k-paul <arun.k.paul@lxisoft.com>
2020-08-25 18:10:34 +0530

    http://jira.bytatech.com:8080/browse/IN-9722

--- AnimalGame/src/com/LxiSoft/Forest/GamePlay.java
+++ AnimalGame/src/com/LxiSoft/Forest/GamePlay.java
@@ -52,137 +52,45 @@ public class GamePlay
                 System.out.println("  Animal 2   "+animal.get(randomFile).getName()+"         ");
                 System.out.println("\n STRENGTH        "+animal.get(randomFile).getAnimalStrength()+"      ");
     
-             try{        
+      /*       try{        
 
-        this.printRandom(animal);
-        this.finalWinner(animal);   
+      //  this.printRandom(animal);
+        //this.finalWinner(animal);   
 }
 
 catch(IndexOutOfBoundsException m)
 {
     System.out.println("Game ends due to some factors");
-}
+}*/
 }
 }
 //}
 
-public void printRandom(ArrayList<Animal> animal)
+public void gameConditions(ArrayList<Animal> animal)
 {
                
-                 if(animal.get(randomNo) instanceof Carnivorous|| animal.get(randomFile) instanceof Carnivorous )
+                 if(animal.get(randomNo) instanceof Carnivorous&& animal.get(randomFile) instanceof Carnivorous )
                  {
+                 	System.out.println("DONOT FIGHT");
 
-                if(animal.get(randomNo).getAnimalStrength()>animal.get(randomFile).getAnimalStrength())
-                {
-
-
-                d.pyramidPattern1();
-                 System.out.println("       Winner         ");
-                System.out.println("       "+animal.get(randomNo).getName()+"     ");
-                int newStrength=(animal.get(randomNo).getAnimalStrength()-10);
-                   
-                System.out.println("Decreased Strength     " +newStrength);
-                d.pyramidPattern2();
-                 System.out.println("    Looser         ");
-                System.out .println("        "+animal.get(randomFile).getName()+"         ");
-                int newStrength1=(animal.get(randomFile).getAnimalStrength()-35);
-              System.out.println("Decreased Strength     " +newStrength1);
-              animal.get(randomFile).setAnimalLife(false);
-                }
-                else
-                  {
-                   
-                d.pyramidPattern1();
-                 System.out.println("    Winner          ");
-
-                     System.out.println("     "+animal.get(randomFile).getName()+"         ");
-
-                      int newStrength=(animal.get(randomFile).getAnimalStrength()-10);
-                         
-                System.out.println("Decreased Strength of Winner "+newStrength);
-                d.pyramidPattern2();
-                 System.out.println("    Looser          ");
-                 System.out .println("        "+animal.get(randomNo).getName()+"         ");
-                int newStrength1=(animal.get(randomNo).getAnimalStrength()-35);
-              System.out.println("Decreased Strength     " +newStrength1);
-                 animal.get(randomNo).setAnimalLife(false);
-                  }
-              }
-
-
-
-             if(animal.get(randomNo) instanceof Herbivorus && animal.get (randomFile) instanceof Herbivorus )
+                 }
+                
+                else(animal.get(randomNo) instanceof Herbivorus && animal.get (randomFile) instanceof Herbivorus )
              {
                 System.out.println("DONOT FIGHT");
 
                }
-           
-        d.pyramidPattern3();
+              
+        else(animal.get(randomNo) instanceof Herbivorus && animal.get(randomFile) instanceof Carnivorous)
+        {
+        	//this.
+        }
+        else (animal.get(randomNo) instanceof Carnivorous && animal.get(randomFile) instance of Herbivorus)
+        {
+        	//this.
+        }
  
  }
 
- public void finalWinner(ArrayList<Animal> animal)
- {
-  
-        System.out.println("Final WINNER");
-
-            for(int i=0;i<animal.size();i++)
-            {
-            randomFile=random.nextInt(animal.size());
-            System.out.println("RANDOM ITEM INDEX "+randomFile);
-
-            while(randomNo==randomFile)
-            {
-                randomFile=random.nextInt(animal.size());
-            }
-
-            System.out.println("RANDOM ITEM IN After"+randomFile);
-
-            if(animal.get(randomNo).getAnimalLife() == true && animal.get(randomFile).getAnimalLife() == true)
-            {
-                if(animal.get(randomNo) instanceof Carnivorous || animal.get(randomFile) instanceof Carnivorous)
-                {
-                    System.out.println("\n PLAYER1     "+animal.get(randomNo).getAnimalName());
-                    System.out.println("\n  STRENGTH   "+animal.get(randomNo).getAnimalStrength());
-                    System.out.println("    ");
-                    System.out.println("\n PLAYER2  "+animal.get(randomFile).getAnimalName());
-                    System.out.println("\n STRENGTH    "+animal.get(randomFile).getAnimalStrength());    
-
-                    if(animal.get(randomNo).getAnimalStrength()>animal.get(randomFile).getAnimalStrength())
-                    {
-                        System.out.println("\n      WINNER IS"+animal.get(randomNo).getAnimalName());
-                        randomNo=randomNo;
-                        animal.get(randomFile).setAnimalLife(false);
-                        animal.remove(randomFile);
-                    }
-                    else{
-                        System.out.println("\n      WINNER IS"+animal.get(randomFile).getAnimalName());
-                        randomNo=randomFile;
-                        animal.get(randomFile).setAnimalLife(false);
-                        animal.remove(randomNo);
-                      
-
-                    }   d.pyramidPattern3();
-                }
-
-                if(animal.get(randomNo) instanceof Herbivorus && animal.get(randomFile) instanceof Herbivorus)
-                {
-                    System.out.println("\n PLAYER1     "+animal.get(randomNo).getAnimalName());
-                    System.out.println("\n  STRENGTH   "+animal.get(randomNo).getAnimalStrength());
-                    System.out.println("    ");
-                    System.out.println("\n PLAYER2  "+animal.get(randomFile).getAnimalName());
-                    System.out.println("\n STRENGTH  "+animal.get(randomFile).getAnimalStrength());  
-                    System.out.println("DONT FIGHT");
-                }  
-
-                   d.pyramidPattern3();
-
-            }
-            
-
-
-        }
-          
-         
-    }
+ 
 }
