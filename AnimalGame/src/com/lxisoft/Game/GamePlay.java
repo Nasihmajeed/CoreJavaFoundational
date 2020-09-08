0156f9e278f4c2e1c3aa35aa3b1a0a40e17e9370
faris-backer <faris.backer@lxisoft.com>
2020-09-08 22:12:37 +0530

    http://jira.bytatech.com:8080/browse/IN-10168

--- AnimalGame/src/com/lxisoft/Game/GamePlay.java
+++ AnimalGame/src/com/lxisoft/Game/GamePlay.java
@@ -150,4 +150,36 @@ public void carVsCar(int randomNo,int randomFile, ArrayList<Animal> animal)
     int randomLuck = rand.nextInt(3);
     return randomLuck;
    }
- }
+
+   public boolean checkArea(int randomNo,int randomFile,  ArrayList<Animal> animal)
+  {
+    int animal1X,animal1Y,animal2X,animal2Y;
+
+    animal1X= 10 + (int) (Math.random()*50);
+    animal1Y= 10 + (int) (Math.random()*50);
+    animal2X= 10 + (int) (Math.random()*50);
+    animal2Y= 10 + (int) (Math.random()*50);
+    System.out.println("\t TERRITORY DETAILS ");
+    System.out.println("   **************************");
+    System.out.println("\n"+animal.get(randomNo).getAnimalName() +"=> \t( "+animal1X+", " +animal1Y+")" + "\t Range =  " +animal.get(randomNo).range );
+    System.out.println("\n"+animal.get(randomFile).getAnimalName()+" => \t( "+animal2X+", " +animal2Y+")" + "\t Range =  " +animal.get(randomFile).range);
+
+    int dist = (int) (Math.sqrt(((animal1X - animal2X)*(animal1X - animal2X))+((animal1Y - animal2Y)*(animal1Y - animal2Y))));
+    int rad = ((animal.get(randomNo).getRange())+(animal.get(randomFile).getRange()));
+    if(dist<= rad)
+    {
+
+      System.out.println("\n\t\t=> "+animal.get(randomNo).getAnimalName() + " AND " +  animal.get(randomFile).getAnimalName() + " are in SAME TERRITORY \n");
+      return true;
+    }
+    else
+    {
+      if(((animal.get(randomNo)) instanceof Herbivorus) && ((animal.get(randomFile)) instanceof Herbivorus))
+        System.out.println("\n\t\t=> "+" Both Are Herbivorus => No Fight Happens");
+      System.out.println("\n\t\t=> "+animal.get(randomNo).getAnimalName() + " AND " +  animal.get(randomFile).getAnimalName() + " are in DIFFERENT TERRITORY \n\t\t=> No FIGHT TAKES PLACE");
+      return false;
+
+    }
+
+  }
+}
