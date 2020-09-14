bd01d2f05187ca91627d1b59d17c67009f5b89b4
faris-backer <faris.backer@lxisoft.com>
2020-09-14 19:32:45 +0530

    http://jira.bytatech.com:8080/browse/IN-10384

---
+++ SnakeAndLadder/src/com/Lxisoft/Game/Dice.java
@@ -0,0 +1,14 @@
+package com.lxisoft.Game;
+import java.util.*;
+import java.util.Scanner;
+
+public class Dice
+{
+	Random diceRolling = new Random();
+	int value;
+
+	public void diceConditions()
+	{
+		this.gameStart();
+	} 
+}
