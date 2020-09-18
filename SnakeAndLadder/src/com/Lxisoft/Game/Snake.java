fbdce5761afab69601f875b3988d499ecdff31e1
faris-backer <faris.backer@lxisoft.com>
2020-09-18 18:02:39 +0530

    http://jira.bytatech.com:8080/browse/IN-10523

--- SnakeAndLadder/src/com/Lxisoft/Game/Snake.java
+++ SnakeAndLadder/src/com/Lxisoft/Game/Snake.java
@@ -4,5 +4,29 @@ public class snake
 	public void decrement()
 	{
 		
-	}
+ 	private int head;
+ 	private int tail;
+
+ 	
+  public void setHead(int head)
+  {
+  	this.head=head;
+  }
+
+  public int getHead()
+  {
+  	return head;
+  }
+
+  public void setTail(int tail)
+  {
+  	this.tail=tail;
+  }
+
+  public int getTail()
+  {
+  	return tail;
+  }
+
 }
+	}
