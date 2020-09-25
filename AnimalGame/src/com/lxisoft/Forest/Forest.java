6496b5e3ab06726bde83cc3eeb8609e0a3ae7da5
vishnu-01 <vishnunath.n@lxisoft.com>
2020-09-25 19:59:08 +0530

    http://jira.bytatech.com:8080/browse/IN-10735

--- AnimalGame/src/com/lxisoft/Forest/Forest.java
+++ AnimalGame/src/com/lxisoft/Forest/Forest.java
@@ -6,24 +6,10 @@ public class Forest
 {
 	ArrayList<Animal> animal = new ArrayList<Animal>();
 	Scanner scanner = new Scanner(System.in);
-	Random r = new Random();
-	int random1 = r.nextInt(5);
-	int random2 = r.nextInt(5);
 
 	public void printName()
-	{
-		
+	{		
 		System.out.println("\n WELCOME TO ANIMAL GAME");
-		/*System.out.println("\n press 1 to Start GAME:");
-		c = scanner.nextInt();
-		if(c==1)
-		{
-			setPage();
-		}
-		else
-		{
-			System.out.println("Invalid choice");
-		}*/
 	}
 
 	public void setName()
@@ -31,19 +17,19 @@ public class Forest
 		System.out.println("\nAnimals");
 		animal.add(new Deer());
 		animal.get(0).setName("Deer");
-		animal.get(0).setStrength(40);
+		animal.get(0).setStrength(20);
 
 		animal.add(new Elephant());
 		animal.get(1).setName("Elephant");
-		animal.get(1).setStrength(70);
+		animal.get(1).setStrength(60);
 		
 		animal.add(new Fox());
 		animal.get(2).setName("Fox");
-		animal.get(2).setStrength(50);
+		animal.get(2).setStrength(40);
 		
 		animal.add(new Lion());
 		animal.get(3).setName("Lion");
-		animal.get(3).setStrength(90);
+		animal.get(3).setStrength(100);
         
         animal.add(new Panther());
 		animal.get(4).setName("Panther");
@@ -63,27 +49,38 @@ public class Forest
 		c = scanner.nextInt();
 		if(c==1)
 		{	
-        System.out.println("\nAnimal 1: "+animal.get(random1).getName()+" "+"Strength: "+animal.get(random1).getStrength());
-        System.out.println("Animal 2: "+animal.get(random2).getName()+" "+"Strength: "+animal.get(random2).getStrength());
-		if(animal.get(random1).getStrength() >= animal.get(random2).getStrength())
-		{
-			a = animal.get(random1).getStrength();
-            b = animal.get(random1).getName();
-            System.out.println("\nWinner:"+b);
-	    }
-	    else
-	    {
-		  	y = animal.get(random2).getStrength();
-            z = animal.get(random2).getName();
-            System.out.println("\nWinner:"+z);
-	    }
+		    for (int i=0;i<animal.size();i++)
+		    {
+		        Random r = new Random();
+	            int random1 = r.nextInt(5);
+	            int random2 = r.nextInt(5);
+	            if(animal.get(random1).getName() == animal.get(random2).getName())
+	            {
+	            	random2 = r.nextInt();
+	            }
+	            else
+	            {
+                    System.out.println("\nAnimal 1: "+animal.get(random1).getName()+"\n"+"Strength: "+animal.get(random1).getStrength());
+                    System.out.println("\nAnimal 2: "+animal.get(random2).getName()+"\n"+"Strength: "+animal.get(random2).getStrength());
+		            if(animal.get(random1).getStrength() >= animal.get(random2).getStrength())
+		            {
+			            a = animal.get(random1).getStrength();
+                        b = animal.get(random1).getName();
+                        System.out.println("\nWinner:"+b);
+	                }
+	                else
+	                {
+		  	            y = animal.get(random2).getStrength();
+                        z = animal.get(random2).getName();
+                        System.out.println("\nWinner:"+z);
+	                }
+	            }
+	        }
 	    System.out.println("\nGame Over");
         }
 	    else
 		{
 			System.out.println("Invalid choice");
 		}
-		   
 	}
-
 }
