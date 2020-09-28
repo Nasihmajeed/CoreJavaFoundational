71f63ff86e97978153b343c0c45556ca6491a068
arun-k-paul <arun.k.paul@lxisoft.com>
2020-09-28 19:26:51 +0530

    http://jira.bytatech.com:8080/browse/IN-10890

--- SnakeAndLadder/src/com/LxiSoft/Game/Board.java
+++ SnakeAndLadder/src/com/LxiSoft/Game/Board.java
@@ -1,117 +1,21 @@
-package com.LxiSoft.Game;
-import java.util.*;
-import java.util.ArrayList;
-import java.util.Scanner;
 public class Board
  {
-  
- /* ArrayList<Board>cells =new ArrayList<Board>(100);
- 	Dice diceRoll = new Dice();
 
-   ArrayList<Snake>snakes = new ArrayList<Snake>();
-   ArrayList<Ladder>ladders = new ArrayList<Ladder>();*/
- Scanner scnr=new Scanner(System.in);
- /* public void startGame()
-  {
-  		
+     final BoardElements[] be = new BoardElements[100];
 
-	   String sGame = "y"; 
-        System.out.println("Do you want to play? Y or N     >  "); 
-        sGame = scnr.nextLine ();
-        System.out.println ("\n\n\n\n\n\n");
-        
-       if (sGame.equals ("y") || sGame.equals ("Y"))
-        {
-            diceRoll.diceConditions(); 
-        }
-        System.out.println ("\n\n\t\t\t\t\t\t");
-    
-   }*/
-  
-
-/*  private void iterateBoard()
-{
-  
- for(int i=0;i<cells.size(101);i++)
-  {
-    
-   
-   }
-   
-}
-
-  public void setSnakePositions()
-  {
-    snakes.get(0).setHead(39);
-    snakes.get(0).setTail(3);
-    snakes.get(1).setHead(35);
-    snakes.get(1).setTail(5);
-    snakes.get(2).setHead(50);
-    snakes.get(2).setTail(34);
-    snakes.get(3).setHead(89);
-    snakes.get(3).setTail(68);
-    snakes.get(4).setHead(97);
-    snakes.get(4).setTail(86);
-    snakes.get(5).setHead(99);
-    snakes.get(5).setTail(26);
- }
-  public void setLadderPositions()
-  {
-    ladders.get(0).setStart(2);
-    ladders.get(0).setEnd(23);
-    ladders.get(1).setStart(7);
-    ladders.get(1).setEnd(29);
-    ladders.get(2).setStart(22);
-    ladders.get(2).setEnd(41);
-    ladders.get(3).setStart(28);
-    ladders.get(3).setEnd(77);
-    ladders.get(4).setStart(30);
-    ladders.get(4).setEnd(32);
-    ladders.get(5).setStart(70);
-    ladders.get(5).setEnd(90);
-    ladders.get(6).setStart(87);
-    ladders.get(6).setEnd(93);
-  }
-*/
-
-  BoardElements[] be = new BoardElements[100];
-
-  public void createSnakes();
+  public void makeSnakes()
   {
     be[39]= new BoardElements();
-    be[39].setSnakeHead(SnakeHead);
+    int a=39;
+   be[39].setSnakeHead(a);
+
     be[3]=new BoardElements();
-    be[3].setSnakeTail(snakeTail);
+   int a1=3; 
+   be[3].setSnakeTail(a1);
+   be[23]=new BoardElements();
+    be[23].setLadderEnd(23);
 
-    be[35]= new BoardElements();
-    be[35].setSnakeHead(snakeHead);
-    be[5]=new BoardElements();
-    be[5].setSnakeTail(snakeTail);
-
-   be[50]= new BoardElements();
-    be[50].setSnakeHead(snakeHead);
-    be[34]=new BoardElements();
-    be[34].setSnakeTail(snakeTail);
-
-   be[89]= new BoardElements();
-    be[89].setSnakeHead(snakeHead);
-    be[68]=new BoardElements();
-    be[68].setSnakeTail(snakeTail);
-
-   be[99]= new BoardElements();
-    be[99].setSnakeHead(snakeHead);
-    be[26]=new BoardElements();
-    be[26].setSnakeTail(snakeTail);
-
-  } 
-  public void createLadder();
-  {
-    be[2]= new BoardElements();
-    be[2].setLadderStart(ladderStart);
-    be[23]=new BoardElements();
-    be[23].setLadderEnd(ladderEnd);
-
-    be[7]= new BoardElements();
+   be[7]= new BoardElements();
     be[7].setLadderStart(ladderStart);
     be[29]=new BoardElements();
     be[29].setLadderEnd(ladderEnd);
@@ -133,10 +37,24 @@ public class Board
     be[87].setLadderStart(ladderStart);
     be[93]=new BoardElements();
     be[93].setLadderEnd(ladderEnd);
+  } 
+  public void createLadder()
+  {
+    be[2]= new BoardElements();
+    int a=2;
+     be[2].setLadderStart(a);
 
+     be[23]= new BoardElements();
+     int a1=23;
+     be[23].setLadderEnd(a1);
   }
+ 
   public void printSnakeAndLadder()
   {
-    
-  }
+    System.out.println("\n The Board Details are ");
+     System.out.println("SNakes Head Positions Are- "+be[39].getSnakeHead());
+    System.out.println("SNake Tail Positionsare:-"+be[3].getSnakeTail());
+    System.out.println("Ladder begin:-"+be[2].getLadderStart());
+    System.out.println("Ladder End: "+be[23].getLadderEnd());
+}
 }
