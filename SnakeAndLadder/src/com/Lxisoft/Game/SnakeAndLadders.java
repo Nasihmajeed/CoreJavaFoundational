23864d86c2c8e3c97a52d1ae1a93899a9e480567
faris-backer <faris.backer@lxisoft.com>
2020-09-28 19:48:58 +0530

    http://jira.bytatech.com:8080/browse/IN-10823

--- SnakeAndLadder/src/com/Lxisoft/Game/SnakeAndLadders.java
+++ SnakeAndLadder/src/com/Lxisoft/Game/SnakeAndLadders.java
@@ -3,40 +3,17 @@ import java.io.*;
 import java.util.*;
 import java.util.Random;
 
-public class SnakeAndLadders
+public class SnakesAndLadders
 {
-	public String snakes;
-	public void snakes()
+	int snakesArray[] = new int [6]{12,36,58,72,92,90};
+	int laddersArray[] = new int [6]{10,15,56,67,46,70};
+
+	public void snakesAndladders()
 	{
-		int snakesArray= new int[] {20,36,70,45,98,90,54};
+		if
+		{
+			
+		}
 	}
-	public void laddrs()
-	{
-		int laddersArray = new int[] {10, 21, 40, 53, 78, 60};
-	}
-	private int head;
- 	private int tail;
 
- 	
-  public void setHead(int head)
-  {
-  	this.head=head;
-  }
-
-  public int getHead()
-  {
-  	return head;
-  }
-
-  public void setTail(int tail)
-  {
-  	this.tail=tail;
-  }
-
-  public int getTail()
-  {
-  	return tail;
-  }
-
-}
 }
