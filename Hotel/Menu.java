8562844c5df79447ad258d5ade7ce8d9d10fe0d9
Karthik <karthik@pop-os.localdomain>
2020-09-30 18:39:04 +0530

    http://jira.bytatech.com:8080/browse/IN-10841

--- Hotel/Menu.java
+++ Hotel/Menu.java
@@ -1,13 +1,33 @@
 import java.util.Scanner;
 public class Menu
 {
-public void quantity(int price)
+ArrayList<item> itm=new ArrayList<item>();
+
+public void addMenu()
 {
-int Total,qty;
-System.out.println("\nEnter the quantity");
-Scanner scanner = new Scanner(System.in);
-qty=scanner.nextInt();
-Total=price*qty;
-System.out.println("\nThe total is"+Total);
+itm.get(0).setName("Pizza");
+itm.get(0).setRate("250");
+itm.get(1).setName("Burger");
+itm.get(1).setRate("450");
+itm.get(2).setName("Pancake");
+itm.get(2).setRate("150");
+itm.get(3).setName("Cakes");
+itm.get(3).setRate("250");
+itm.get(4).setName("Soda");
+itm.get(4).setRate("50");
+itm.get(5).setName("Wine");
+itm.get(5).setRate("500");
+}
+
+Public void print2()
+{
+for(i=0;i<itm.size();i++)
+{
+System.out.println(i+itm.get(i).getName()+"\t"+itm.get(i).getRate());
 }
 }
+Public void order()
+{
+Scanner scn=new Scanner();
+int n=nextln.Int();
+
---
+++ Hotel/item.java
@@ -0,0 +1,19 @@
+public class Item
+{
+public void setName(String a)
+{
+name=a;
+}
+public void getName()
+{
+return name;
+}
+public void setRate(int n)
+{
+price=n;
+}
+public void getRate()
+{
+return price;
+}
+}
