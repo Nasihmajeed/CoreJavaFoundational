32fe1a16bd3509f6569479786566ef161bf4885c
arun-k-paul <arun.k.paul@lxisoft.com>
2020-10-07 17:32:28 +0530

    http://jira.bytatech.com:8080/browse/IN-11093

---
+++ SnakeAndLadder/src/com/LxiSoft/Game/Ladder.java
@@ -0,0 +1,37 @@
+package com.LxiSoft.Game;
+import java.util.*;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
+
+public class Ladder 
+ {
+ 		private int start,end;
+
+ 	public void behaviour() 
+ 	{		
+		System.out.println("I am put up your position");
+	}
+
+	public void setStart(int start)
+  {
+  	this.start=start;
+  }
+
+  public int getStart()
+  {
+  	return start;
+  }
+
+  public void setEnd(int end)
+  {
+  	this.end=end;
+  }
+
+  public int getEnd()
+  {
+  	return end;
+  }
+
+ 
+}
