3877dfdf2c561e026107e2f5e62a8fe92f108a1f
Jithin Pulikkal <73100765+Jithin875@users.noreply.github.com>
2020-12-11 18:01:39 +0530

    Add files via upload

---
+++ SchoolProject/schoolproject/src/com/LxiSoft/SchoolDetails/Subject.java
@@ -0,0 +1,26 @@
+package com.LxiSoft.SchoolDetails;
+import java.util.*;
+
+public class Subject
+{
+	String subject;
+	int mark;
+	
+	public void addMarkDetails()
+	{
+		Scanner s = new Scanner(System.in);
+
+		System.out.println("Subject :");
+		this.subject = s.nextLine();
+		System.out.println("Marks 	:");
+		this.mark = s.nextInt();
+		
+	}
+	
+	public void printMarkDetails()
+	{
+		System.out.println("Subject \t: "+this.subject);
+		System.out.println("Mark    \t: "+this.mark);
+		
+	}
+}
