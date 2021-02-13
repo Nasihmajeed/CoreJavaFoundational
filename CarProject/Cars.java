88b7b1709d6e363b34d3640c9529f8f7898fd764
@ROHITH-77 <rohith.r@lxisoft.com>
2021-02-13 17:18:06 +0530

    http://jira.bytatech.com:8080/browse/IN-13694

--- CarProject/Cars.java
+++ CarProject/Cars.java
@@ -27,7 +27,7 @@ Driver driver3=new Driver();
 public void print1()
 {
 System.out.println("***** Car Detail 1 *****");
-System.out.println("_______________________");
+System.out.println("-------------------------");
 System.out.println("Brand of the car is : "+model1.name1);
 System.out.println("Name of the car is : "+car1Name);
 System.out.println("Price of the car is : "+price1);
@@ -36,14 +36,14 @@ System.out.println("Age of Driver :"+driver1.age1);
 System.out.println("No.of doors in the car :"+door1.door_no);
 System.out.println("Type of the door :"+door1.door_type);
 System.out.println("No.of tyres : "+tyre1.ty_no);
-System.out.println("Company of the tyre : "+tyre1.ty_name1);
-System.out.println("_______________________");
+System.out.println("Company of the tyre : "+tyre1.ty_name1);f
+System.out.println("-------------------------");
 System.out.println("\n");
 }
 public void print2()
 {
 System.out.println("***** Car Detail 2 *****");
-System.out.println("_______________________");
+System.out.println("-------------------------");
 System.out.println("Brand of the car is : "+model2.name2);
 System.out.println("Name of the car is : "+car2Name);
 System.out.println("Price of the car is : "+price2);
@@ -53,13 +53,13 @@ System.out.println("No.of doors in the car : "+door2.door_no);
 System.out.println("Type of the door : "+door2.door_type);
 System.out.println("No.of tyres : "+tyre2.ty_no);
 System.out.println("Company of the tyre : "+tyre2.ty_name2);
-System.out.println("_______________________");
+System.out.println("-------------------------");
 System.out.println("\n");
 }
 public void print3()
 {
 System.out.println("***** Car Detail 3 *****");
-System.out.println("_______________________");
+System.out.println("-------------------------");
 System.out.println("Brand of the car is : "+model3.name3);
 System.out.println("Name of the car is : "+car3Name);
 System.out.println("Price of the car is : "+price3);
@@ -69,7 +69,7 @@ System.out.println("No.of doors in the car : "+door3.door_no);
 System.out.println("Type of the door : "+door3.door_type);
 System.out.println("No.of tyres : "+tyre3.ty_no);
 System.out.println("Company of the tyre : "+tyre3.ty_name3);
-System.out.println("_______________________");
+System.out.println("-------------------------");
 System.out.println("\n");
 }
 }
