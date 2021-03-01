e781194f01636e46fff33991dacaf0264ab5e816
@ROHITH-77 <rohith.r@lxisoft.com>
2021-03-01 17:53:21 +0530

    http://jira.bytatech.com:8080/browse/IN-13949

---
+++ Hotel/Food.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public interface Food{
+	
+	public void setName(String food);//{
+		//this.foodName = food;
+	//}
+	public String getName();//{
+		//return foodName;
+	//}
+	//public void setType(String type){
+	//	this.foodType = type;
+	//}
+	//public String getType(){
+	//	return foodType;
+	//}
+	public void setPrize(int prize);//{
+	//	this.prize = prize;
+	//}
+	public int getPrize();//{
+		//return prize;
+	//}
+}
