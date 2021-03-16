fa0933275dd20a96b853b78f06101bde80307b88
Saikrishnaprasad7 <saikrishnaprasad.p.v@lxisoft.com>
2021-03-16 22:13:09 +0530

    sample

---
+++ CarProject/Beer.java
@@ -0,0 +1,29 @@
+	public class Beer{
+		
+		public static void main(String[] args){
+	int beerNum=10;
+    String word="bottles";
+
+		while(beerNum>0)
+		{	
+		if(beerNum>0)
+		{
+			System.out.println(beerNum+"  "+word+" "+"of beer on the wall");
+			beerNum=beerNum-1;
+		}
+		if(beerNum==1)
+		{
+			System.out.println(beerNum+"  "+word+" "+"of beer on the wall");
+			System.out.println(beerNum+"  "+word+" "+"of beer");
+			System.out.println("Take one down");
+			System.out.println("Pass it around");
+		}
+		else
+		{
+			System.out.println("No more bottles of beer on the wall");
+		}
+	}	
+     }
+	}
+
+
