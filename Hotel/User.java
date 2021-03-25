099f489fb5f2e3ed5407f0a056304f9161349b7c
amalraveendran7 <amal.c.r@lxisoft.com>
2021-03-25 18:16:50 +0530

    http://jira.bytatech.com:8080/browse/IN-14419

---
+++ Hotel/User.java
@@ -0,0 +1,14 @@
+class User
+{
+	String dishes;
+	float price,total;
+	
+	User(String dishes,float price)
+	{
+	this.dishes=dishes;
+	this.price=price;
+	System.out.println(dishes+"    "+price);
+	}
+	public void 
+	
+}
