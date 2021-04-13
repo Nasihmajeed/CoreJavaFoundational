381e33b9322998530ace92a3eba43d4e475f7c41
nishad96 <nishad.k@lxisoft.com>
2021-04-13 12:39:04 +0530

    http://jira.bytatech.com:8080/browse/IN-14800

--- AnimalGame/Animal.java
+++ AnimalGame/Animal.java
@@ -1,9 +1,9 @@
 import java.util.*;
 class Animal{
-	String name;
+	String name,type;
 	int strength;
-	ArrayList<Animal> herbAnimal=new ArrayList<Animal>();
-	ArrayList<Animal> carnAnimal=new ArrayList<Animal>();
+	ArrayList<Animal> animal=new ArrayList<Animal>();
+	//ArrayList<Animal> carnAnimal=new ArrayList<Animal>();
 public void setName(String name){
 	this.name=name;
 }
@@ -16,42 +16,58 @@ public void setStrength(int strength){
 public int getStrength(){
 	return strength;
 }
-public void addAnimal(ArrayList<Animal> herbAnimal,ArrayList<Animal> carnAnimal){
-	for(int i=0;i<5;i++){
-		herbAnimal.add(new Animal());
+public void setType(String type){
+	this.type=type;
+}
+public String getType(){
+	return type;
+}
+public void addAnimal(ArrayList<Animal> animal){
+	for(int i=0;i<10;i++){
+		animal.add(new Animal());
 	}
-	herbAnimal.get(0).setName("DEER");
-	herbAnimal.get(0).setStrength(40);
+	animal.get(0).setName("DEER");
+	animal.get(0).setStrength(40);
+	animal.get(0).setType("HERBIVORES");
 	//animal.add(new Animal());
-	herbAnimal.get(1).setName("BUFFALLO");
-	herbAnimal.get(1).setStrength(50);
+	animal.get(1).setName("BUFFALLO");
+	animal.get(1).setStrength(50);
+	animal.get(1).setType("HERBIVORES");
 	//animal.add(new Animal());
-	herbAnimal.get(2).setName("GIRAFFE");
-	herbAnimal.get(2).setStrength(45);
+	animal.get(2).setName("GIRAFFE");
+	animal.get(2).setStrength(45);
+	animal.get(2).setType("HERBIVORES");
 	//animal.add(new Animal());
-	herbAnimal.get(3).setName("ELEPHANT");
-	herbAnimal.get(3).setStrength(90);
+	animal.get(3).setName("ELEPHANT");
+	animal.get(3).setStrength(90);
+	animal.get(3).setType("HERBIVORES");
 	
-	herbAnimal.get(4).setName("COW");
-	herbAnimal.get(4).setStrength(38);
+	animal.get(4).setName("COW");
+	animal.get(4).setStrength(38);
+	animal.get(4).setType("HERBIVORES");
 	
-	for(int i=0;i<5;i++){
+	/* for(int i=0;i<5;i++){
 		carnAnimal.add(new Animal());
-	}
-	carnAnimal.get(0).setName("TIGER");
-	carnAnimal.get(0).setStrength(84);
+	} */
+	animal.get(5).setName("TIGER");
+	animal.get(5).setStrength(84);
+	animal.get(5).setType("CARNIVORES");
 	
-	carnAnimal.get(1).setName("LION");
-	carnAnimal.get(1).setStrength(87);
+	animal.get(6).setName("LION");
+	animal.get(6).setStrength(87);
+	animal.get(6).setType("CARNIVORES");
 	
-	carnAnimal.get(2).setName("CHEETAH");
-	carnAnimal.get(2).setStrength(60);
+	animal.get(7).setName("CHEETAH");
+	animal.get(7).setStrength(60);
+	animal.get(7).setType("CARNIVORES");
 	
-	carnAnimal.get(3).setName("FOX");
-	carnAnimal.get(3).setStrength(79);
+	animal.get(8).setName("FOX");
+	animal.get(8).setStrength(79);
+	animal.get(8).setType("CARNIVORES");
 	
-	carnAnimal.get(4).setName("PANTHER");
-	carnAnimal.get(4).setStrength(82);
+	animal.get(9).setName("PANTHER");
+	animal.get(9).setStrength(82);
+	animal.get(9).setType("CARNIVORES");
 	
 	/* carnAnimal.get(5).setName("LEOPARD");
 	carnAnimal.get(5).setStrength(83); */
--- AnimalGame/Forest.java
+++ AnimalGame/Forest.java
@@ -1,11 +1,11 @@
 import java.util.*;
 class Forest{
-		ArrayList<Animal> herbAnimal=new ArrayList<Animal>();
-	ArrayList<Animal> carnAnimal=new ArrayList<Animal>();
+		//ArrayList<Animal> herbAnimal=new ArrayList<Animal>();
+	ArrayList<Animal> animal=new ArrayList<Animal>();
 	ArrayList<String> deadAnimal=new ArrayList<String>();
 	Animal a=new Animal();
 	public int randNum(){
-		int random=(int)(Math.random()*carnAnimal.size());
+		int random=(int)(Math.random()*animal.size());
                 return random;
 	}
 	/* 			
@@ -17,25 +17,29 @@ class Forest{
 		System.out.println("|**********************************|");
 		System.out.println("|******WELCOME TO THE JUNGLE*******|");
 		System.out.println("|**********************************|");
-		a.addAnimal(herbAnimal,carnAnimal);
+		a.addAnimal(animal);
 				try {
 			Thread.sleep(1500);                 //1500 milliseconds is one second.
 		} catch(InterruptedException ex) {
 			Thread.currentThread().interrupt();
 		}
-		fight(herbAnimal,carnAnimal);
+		fight(animal);
 	}
-	public void fight(ArrayList<Animal> herbAnimal,ArrayList<Animal> carnAnimal){
+	public void fight(ArrayList<Animal> animal){
 		System.out.println("\n \n ANIMALS IN THE JUNGLE\n");
 		System.out.printf("%-20s %10s\n", "HERBIVORES ANIMAL","STRENGTH");
 		System.out.printf("----------------------------------------------");
-			for(int i=0;i<carnAnimal.size();i++){
-				System.out.printf("\n%-15s %10s\n", herbAnimal.get(i).name,herbAnimal.get(i).strength);
+			for(int i=0;i<animal.size();i++){
+				if(animal.get(i).type.equals("HERBIVORES")){
+				System.out.printf("\n%-15s %10s\n", animal.get(i).name,animal.get(i).strength);
+				}
 			}
 			System.out.printf("\n\n%-20s %10s\n", "CARNIVORES ANIMAL","STRENGTH");
 		System.out.printf("----------------------------------------------");
-			for(int i=0;i<carnAnimal.size();i++){
-				System.out.printf("\n%-15s %10s\n", carnAnimal.get(i).name,carnAnimal.get(i).strength);
+			for(int i=0;i<animal.size();i++){
+				if(animal.get(i).type.equals("CARNIVORES")){
+				System.out.printf("\n%-15s %10s\n", animal.get(i).name,animal.get(i).strength);
+				}
 			}
 		do{
 			
@@ -49,64 +53,67 @@ class Forest{
 				} catch(InterruptedException ex) {
 					Thread.currentThread().interrupt();
 				} */
-				System.out.println("\n ANIMALS IN THE RING "+carnAnimal.get(randm1).name +" AND "+ carnAnimal.get(randm2).name);
-				if(carnAnimal.get(randm1).strength>carnAnimal.get(randm2).strength){
+				System.out.println("\n ANIMALS IN THE RING "+animal.get(randm1).name +" AND "+ animal.get(randm2).name);
+				if(animal.get(randm1).type.equals("HERBIVORES")&&animal.get(randm2).type.equals("HERBIVORES")){
+					System.out.println("HERBIVORES ANIMALS WON'T FIGHT");
+				}
+				 else if(animal.get(randm1).strength>animal.get(randm2).strength){
 					/* 				try {
 					Thread.sleep(2000);                 //1500 milliseconds is one second.
 				} catch(InterruptedException ex) {
 					Thread.currentThread().interrupt();
 				} */
-				System.out.println("\n ANIMAL "+carnAnimal.get(randm1).name+" WINS");
-				carnAnimal.get(randm2).strength=carnAnimal.get(randm2).strength/2;
+				System.out.println("\n ANIMAL "+animal.get(randm1).name+" WINS");
+				animal.get(randm2).strength=animal.get(randm2).strength/2;
 						/* 			try {
 						Thread.sleep(1500);                 //1500 milliseconds is one second.
 					} catch(InterruptedException ex) {
 						Thread.currentThread().interrupt();
 					} */
-				System.out.println("Strength of "+carnAnimal.get(randm2).name +" is "+carnAnimal.get(randm2).strength);
-					if(carnAnimal.get(randm2).strength<=10){
+				System.out.println("Strength of "+animal.get(randm2).name +" is "+animal.get(randm2).strength);
+					if(animal.get(randm2).strength<=10){
 							/* 				try {
 						Thread.sleep(3000);                 //1500 milliseconds is one second.
 					} catch(InterruptedException ex) {
 						Thread.currentThread().interrupt();
 					} */
 						
-						System.out.println("ANIMAL "+carnAnimal.get(randm2).name+" IS DEAD");
+						System.out.println("ANIMAL "+animal.get(randm2).name+" IS DEAD");
 						//deadAnimal.add(new String());
-						deadAnimal.add(carnAnimal.get(randm2).name);
-						carnAnimal.remove(randm2);
+						deadAnimal.add(animal.get(randm2).name);
+						animal.remove(randm2);
 					}
 				}
-				else if(carnAnimal.get(randm1).strength<carnAnimal.get(randm2).strength){
+				else if(animal.get(randm1).strength<animal.get(randm2).strength){
 								/* 	try {
 					Thread.sleep(2000);                 //1500 milliseconds is one second.
 				} catch(InterruptedException ex) {
 					Thread.currentThread().interrupt();
 				} */
-					System.out.println("\n ANIMAL "+carnAnimal.get(randm2).name+" WINS");
-					carnAnimal.get(randm1).strength=carnAnimal.get(randm1).strength/2;
+					System.out.println("\n ANIMAL "+animal.get(randm2).name+" WINS");
+					animal.get(randm1).strength=animal.get(randm1).strength/2;
 									/* 		try {
 							Thread.sleep(1500);                 //1500 milliseconds is one second.
 						} catch(InterruptedException ex) {
 							Thread.currentThread().interrupt();
 						} */
-					System.out.println("Strength of "+carnAnimal.get(randm1).name +" is "+carnAnimal.get(randm1).strength);
-					if(carnAnimal.get(randm1).strength<=10){
+					System.out.println("Strength of "+animal.get(randm1).name +" is "+animal.get(randm1).strength);
+					if(animal.get(randm1).strength<=10){
 								/* 		try {
 						Thread.sleep(3000);                 //1500 milliseconds is one second.
 					} catch(InterruptedException ex) {
 						Thread.currentThread().interrupt();
 					} */
-											System.out.println("ANIMAL "+carnAnimal.get(randm1).name+" IS DEAD");
-											deadAnimal.add(carnAnimal.get(randm1).name);
-							carnAnimal.remove(randm1);
+											System.out.println("ANIMAL "+animal.get(randm1).name+" IS DEAD");
+											deadAnimal.add(animal.get(randm1).name);
+							animal.remove(randm1);
 					}
 				}
 
 			}
 			//else
 				//System.out.println("SAME ANIMALS......CANNOT FIGHT");
-		}while(carnAnimal.size()!=1);
+		}while(animal.size()!=1);
 					try {
 				Thread.sleep(5000);                 //1500 milliseconds is one second.
 			} catch(InterruptedException ex) {
@@ -116,7 +123,7 @@ class Forest{
 			System.out.println("DEAD ANIMAL : "+deadAnimal.get(i));
 			}
 		System.out.println("\n\n|***************************************************|");
-		System.out.println("|******************WINNER IS "+carnAnimal.get(0).name+"******************|");
+		System.out.println("|******************WINNER IS "+animal.get(0).name+"******************|");
 		System.out.println("|***************************************************|");
 	}
 	//public void fight(randm1,randm2,ArrayList<Animal> animal){
