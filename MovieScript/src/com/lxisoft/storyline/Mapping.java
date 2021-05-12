1181c778bffd55766283a876cd1a1c905ce0084b
Amalraveendran <36656127+Amalraveendran@users.noreply.github.com>
2021-05-12 17:42:14 +0530

    http://jira.bytatech.com:8080/browse/IN-15431

---
+++ MovieScript/src/com/lxisoft/storyline/Mapping.java
@@ -0,0 +1,11 @@
+package com.lxisoft.storyline;
+import com.lxisoft.script.*;
+import java.io.*;
+import java.util.*;
+public class Mapping
+{
+
+
+Map<String,String> mapFromFile=new HashMap();
+
+}
