02593b7b7cedc59ac4eb8ab797d7f47d043e29be
mahesan711 <mahesan.a.u@lxisoft.com>
2021-05-22 12:34:02 +0530

    http://jira.bytatech.com:8080/browse/IN-15610

--- CarProject/Tyre.java
+++ CarProject/Tyre.java
@@ -1,12 +1,18 @@
 import java.util.Scanner;
 public class Tyre {
 Scanner input=new Scanner(System.in);
-String tyreType;
+String rearTyreLeft,rearTyreRight,frontTyreLeft,frontTyreRight;
 public void inputDetails(){
-  System.out.println("Enter Tyre Type");
-  tyreType=input.nextLine();
+  System.out.println("Enter Rear Tyre Left Type");
+  rearTyreLeft=input.nextLine();
+  System.out.println("Enter Rear Tyre Right Type");
+  rearTyreRight=input.nextLine();
+  System.out.println("Enter front Tyre left Type");
+  frontTyreLeft=input.nextLine();
+  System.out.println("Enter front Tyre right Type");
+  frontTyreRight=input.nextLine();
 }
 public void printDetails(){
-  System.out.println("4 Tyre Type of "+tyreType+"is fixed to car \n Tyre is Rolling");
+  System.out.println("Rear  Tyre left Type  is "+rearTyreLeft+"\nRear Tyre right Type is "+rearTyreRight+"\nFront Tyre Left type is "+frontTyreLeft+"\nFront Tyre Right type is "+frontTyreRight+"\n Tyres are Rolling");
   }
 }
