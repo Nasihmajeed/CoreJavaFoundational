2efa57ae68c7bd9f1a7c3f2da07dd00c59788d0f
Saikrishnaprasad7 <saikrishnaprasad.p.v@lxisoft.com>
2021-06-02 17:40:34 +0530

    http://jira.bytatech.com:8080/browse/IN-15823

--- MovieScript/src/com/lxisoft/scripts/Script.java
+++ MovieScript/src/com/lxisoft/scripts/Script.java
@@ -1,159 +1,243 @@
-package com.lxisoft.scripts;
-
-import java.io.*;
-import java.util.*;
-
-import java.nio.charset.StandardCharsets;
-import java.util.stream.Collectors;
-
-import com.lxisoft.cast.*;
-
-
-public class Script
-{
+	case 1 :  //romantic
+	                      System.out.println("-----------------------------------Romantic Script--------------------------------------------");
 	
-final String romance="E:\\MovieScriptProject\\src\\com\\lxisoft\\scripts\\RomanticScr.txt";
-final String emotional= "E:\\MovieScriptProject\\src\\com\\lxisoft\\scripts\\EmotionalScr.txt";
-final String comedy= "E:\\MovieScriptProject\\src\\com\\lxisoft\\scripts\\ComedyScr.txt";
-
-Actors act=new Actors();
-
-
-int n;    //line number
-String line;
-
-public void selectScript(int genre)
-{
-	switch(genre)
-	{
-
-
-
-		
-		RandomAccessFile raf = new RandomAccessFile("file.txt","r"); 
-		
-    //above 'r' means open in read only mode
-    ArrayList<Integer> arrayList = new ArrayList<Integer>();
+	                            
+					     try (BufferedReader romanticReader=new BufferedReader(new FileReader(new File(romance)))) {
+                                                     for (n=0; n<5; n++)
+													 {
+														  line = romanticReader.readLine();
+                                                System.out.println("Dialogue : "+line);									
+													 }  
+						 }
+						catch(IOException e)
+					     {
+						 System.out.println("Something is wrong........File Reading Failed !!!");
+						 System.out.println(e);
+					     }
+				
+                                                                   try{
+					                                                     Thread.sleep(2000);
+				                                                        }
+				                                                  catch(InterruptedException e)
+				                                                        {
+					                                                     Thread.currentThread().interrupt();
+				                                                        } 
+	                     
+						 System.out.println("\r\n");
+						 System.out.println("\r\n");
+						 System.out.println("\r\n");
+						 System.out.println("\r\n");
+						 System.out.println("\r\n");
+						 
+						 
+				System.out.println("-----------------------------------Emotional Script--------------------------------------------");
+											
+											try (BufferedReader emotionalReader=new BufferedReader(new FileReader(new File(emotional)))) {
+                                                     for (n=0; n<2; n++)
+													 {
+														  line = emotionalReader.readLine();
+                                                System.out.println("Dialogue : "+line);									
+													 }  
+						 }
+						catch(IOException e)
+					     {
+						 System.out.println("Something is wrong........File Reading Failed !!!");
+						 System.out.println(e);
+					     }
+				
+                                                                   try{
+					                                                     Thread.sleep(2000);
+				                                                        }
+				                                                  catch(InterruptedException e)
+				                                                        {
+					                                                     Thread.currentThread().interrupt();
+				                                                        } 							
+															System.out.println("\r\n");
+						
+    					 
+						 System.out.println("\r\n");
+						 System.out.println("\r\n");
+						 System.out.println("\r\n");
+						 System.out.println("\r\n");			
+				
+				System.out.println("-----------------------------------Comedy Script--------------------------------------------");
+											
+											try (BufferedReader comedyReader=new BufferedReader(new FileReader(new File(comedy)))) {
+                                                     for (n=0; n<3; n++)
+													 {
+														  line = comedyReader.readLine();
+                                                System.out.println("Dialogue : "+line);									
+													 }  
+						 }
+						catch(IOException e)
+					     {
+						 System.out.println("Something is wrong........File Reading Failed !!!");
+						 System.out.println(e);
+					     }
+				 							
+																		
+									break;									
+																		
+	case 2 : 
+	              //emotional
+				  
+                   System.out.println("-----------------------------------Romantic Script--------------------------------------------");
 	
-    String cur_line = "";
+	                            
+					     try (BufferedReader romanticReader=new BufferedReader(new FileReader(new File(romance)))) {
+                                                     for (n=0; n<1; n++)
+													 {
+														  line = romanticReader.readLine();
+                                                System.out.println("Dialogue : "+line);									
+													 }  
+						 }
+						catch(IOException e)
+					     {
+						 System.out.println("Something is wrong........File Reading Failed !!!");
+						 System.out.println(e);
+					     }
+				
+                                                                   try{
+					                                                     Thread.sleep(2000);
+				                                                        }
+				                                                  catch(InterruptedException e)
+				                                                        {
+					                                                     Thread.currentThread().interrupt();
+				                                                        } 
+	                     
+						 System.out.println("\r\n");
+						 System.out.println("\r\n");
+						 System.out.println("\r\n");
+						 System.out.println("\r\n");
+						 System.out.println("\r\n");
+						 
+				System.out.println("-----------------------------------Emotional Script--------------------------------------------");
+											
+											try (BufferedReader emotionalReader=new BufferedReader(new FileReader(new File(emotional)))) {
+                                                     for (n=0; n<10; n++)
+													 {
+														  line = emotionalReader.readLine();
+                                                System.out.println("Dialogue : "+line);									
+													 }  
+						 }
+						catch(IOException e)
+					     {
+						 System.out.println("Something is wrong........File Reading Failed !!!");
+						 System.out.println(e);
+					     }
+				
+                                                                   try{
+					                                                     Thread.sleep(2000);
+				                                                        }
+				                                                  catch(InterruptedException e)
+				                                                        {
+					                                                     Thread.currentThread().interrupt();
+				                                                        } 							
+															
+                         System.out.println("\r\n");
+						 System.out.println("\r\n");
+						 System.out.println("\r\n");
+						 System.out.println("\r\n");
+						 System.out.println("\r\n");
+															
+				System.out.println("-----------------------------------Comedy Script--------------------------------------------");
+											
+											try (BufferedReader comedyReader=new BufferedReader(new FileReader(new File(comedy)))) {
+                                                     for (n=0; n<2; n++)
+													 {
+														  line = comedyReader.readLine();
+                                                System.out.println("Dialogue : "+line);									
+													 }  
+						 }
+						catch(IOException e)
+					     {
+						 System.out.println("Something is wrong........File Reading Failed !!!");
+						 System.out.println(e);
+					     }
+
+				  break;
+				   
+				   
+	case 3 :
+	                //comedy
+	               
+				   System.out.println("-----------------------------------Romantic Script--------------------------------------------");
 	
-    while((cur_line=raf.readLine())!=null)
-    {
-    arrayList.add(raf.getFilePointer());
-    }
-	
-    //Print the 32 line
-    //Seeks the file to the particular location from where our '32' line starts
-    raf.seek(raf.seek(arrayList.get(8)));
-	
-    System.out.println(raf.readLine());
-	
-    raf.close();
+	                            
+					     try (BufferedReader romanticReader=new BufferedReader(new FileReader(new File(romance)))) {
+                                                     for (n=0; n<2; n++)
+													 {
+														  line = romanticReader.readLine();
+                                                System.out.println("Dialogue : "+line);									
+													 }  
+						 }
+						catch(IOException e)
+					     {
+						 System.out.println("Something is wrong........File Reading Failed !!!");
+						 System.out.println(e);
+					     }
+				
+                                                                   try{
+					                                                     Thread.sleep(2000);
+				                                                        }
+				                                                  catch(InterruptedException e)
+				                                                        {
+					                                                     Thread.currentThread().interrupt();
+				                                                        } 
+	                     
+						 System.out.println("\r\n");
+						 System.out.println("\r\n");
+						 System.out.println("\r\n");
+						 System.out.println("\r\n");
+						 System.out.println("\r\n");
+						 
+				System.out.println("-----------------------------------Emotional Script--------------------------------------------");
+											
+											try (BufferedReader emotionalReader=new BufferedReader(new FileReader(new File(emotional)))) {
+                                                     for (n=0; n<1; n++)
+													 {
+														  line = emotionalReader.readLine();
+                                                System.out.println("Dialogue : "+line);									
+													 }  
+						 }
+						catch(IOException e)
+					     {
+						 System.out.println("Something is wrong........File Reading Failed !!!");
+						 System.out.println(e);
+					     }
+				
+                                                                   try{
+					                                                     Thread.sleep(2000);
+				                                                        }
+				                                                  catch(InterruptedException e)
+				                                                        {
+					                                                     Thread.currentThread().interrupt();
+				                                                        } 							
+												
+                         System.out.println("\r\n");
+						 System.out.println("\r\n");
+						 System.out.println("\r\n");
+						 System.out.println("\r\n");
+						 System.out.println("\r\n");
+
+ 
+ 
+												
+				System.out.println("-----------------------------------Comedy Script--------------------------------------------");
+											
+											try (BufferedReader comedyReader=new BufferedReader(new FileReader(new File(comedy)))) {
+                                                     for (n=0; n<15; n++)
+													 {
+														  line = comedyReader.readLine();
+                                                System.out.println("Dialogue : "+line);									
+													 }  
+						 }
+						catch(IOException e)
+					     {
+						 System.out.println("Something is wrong........File Reading Failed !!!");
+						 System.out.println(e);
+					     }
+				   
+				   break;
 	
 	}
-}
-		
-}
-
-       //Method-1
-
-try (BufferedReader reader = Files.newBufferedReader(
-        Paths.get("file.txt"), StandardCharsets.UTF_8)) {
-    List<String> line = reader.lines()
-                              .skip(31)
-                              .limit(1)
-                              .collect(Collectors.toList());
-
-    line.stream().forEach(System.out::println);
-}
-
-
-     // Method-2
-
-RandomAccessFile raf = new RandomAccessFile("myFile.txt","r"); 
-    //above 'r' means open in read only mode
-    ArrayList<Integer> arrayList = new ArrayList<Integer>();
-    String cur_line = "";
-    while((cur_line=raf.readLine())!=null)
-    {
-    arrayList.add(raf.getFilePointer());
-    }
-    //Print the 32 line
-    //Seeks the file to the particular location from where our '32' line starts
-    raf.seek(raf.seek(arrayList.get(31));
-    System.out.println(raf.readLine());
-    raf.close();
-	
-	
-	
-	//To read a specific line
-	
-	import java.nio.file.Files;
-
-import java.nio.file.Paths;
-
-public class FileWork 
-{
-
-    public static void main(String[] args) throws IOException {
-
-        String line = Files.readAllLines(Paths.get("D:/abc.txt")).get(1);
-
-        System.out.println(line);  
-    }
-
-}
-
-
-
-//you can use the skip() function to skip the lines from begining.
-
-public static void readFile(String filePath, long lineNum) {
-    List<String> list = new ArrayList<>();
-    long totalLines, startLine = 0;
-
-    try (Stream<String> lines = Files.lines(Paths.get(filePath))) {
-        totalLines = Files.lines(Paths.get(filePath)).count();
-        startLine = totalLines - lineNum;
-        // Stream<String> line32 = lines.skip(((startLine)+1));
-
-        list = lines.skip(startLine).collect(Collectors.toList());
-        // lines.forEach(list::add);
-    } catch (IOException e1) {
-        // TODO Auto-generated catch block
-        e1.printStackTrace();
-    }
-
-    list.forEach(System.out::println);
-
-}
-
-
-// Looping through a HashMap and Print keys and values
-for (String i : capitalCities.keySet()) {
-  System.out.println("key: " + i + " value: " + capitalCities.get(i));
-}
-
-
-
-//Writing to a File
-
-
-import java.io.FileWriter;   // Import the FileWriter class
-import java.io.IOException;  // Import the IOException class to handle errors
-
-public class WriteToFile {
-  public static void main(String[] args) {
-    try {
-      FileWriter myWriter = new FileWriter("filename.txt");
-      myWriter.write("Files in Java might be tricky, but it is fun enough!");
-      myWriter.close();
-      System.out.println("Successfully wrote to the file.");
-    } catch (IOException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
-    }
-  }
-}
-
-}
