e4ac755bf43b443cde3cd89e300df239c2fe61d3
mahesan711 <mahesan.a.u@lxisoft.com>
2021-06-05 21:42:21 +0530

    http://jira.bytatech.com:8080/browse/IN-15894

--- CarProject/Car.java
+++ CarProject/Car.java
@@ -7,14 +7,13 @@ Scanner input=new Scanner(System.in);
 
 Engine engine;
 Driver driver;
-//Tyre[] tyres=new Tyre[4];
+Tyre[] tyres=new Tyre[4];
 
 
 
-Tyre tyres0;
-Tyre tyres1;
-Tyre tyres2;
-Tyre tyres3;
+//Tyre rearTyreRight;
+//Tyre frontTyreLeft;
+//Tyre frontTyreRight;
 public void inputDetails(){
 
   System.out.println("Enter Car's name");
@@ -22,23 +21,20 @@ name=input.nextLine();
 System.out.println("Enter car's color");
 color=input.nextLine();
 this.engine.inputDetails();
-this.driver.inputDetails();
 //tyre= new Tyre[4];
+for(int num=0;num<=3;num++){
+this.tyres[num].inputDetails();
 }
-public void addTyre(){
-this.tyres0.inputDetails();
-this.tyres1.inputDetails();
-this.tyres2.inputDetails();
-this.tyres3.inputDetails();
-}
-
+this.driver.inputDetails();
+ }
+ 
 public void printDetails(){
   System.out.println("The Car's name is "+name+"\n Car's color is "+color);
  this.engine.printDetails();
-this.tyres0.printDetails();
-this.tyres2.printDetails();
-this.tyres2.printDetails();
-this.tyres3.printDetails();
+ for(int num=0;num<=3;num++){
+  this.tyres[num].printDetails();
+ // this.tyres[num].addTyre();
+  }
   
 this.driver.printDetails();
 }
--- CarProject/TDD.java
+++ CarProject/TDD.java
@@ -4,61 +4,47 @@ public class Tdd {
 
   //Taking input from Cars 1
    Car car1=new Car();
-   //Array 
-  // Tyre[] tyre=new Tyre[8];
-   
-   //objects for car1
+      //objects for car1
    
    Engine engine1=new Engine();
    Driver driver1=new Driver();
-   Tyre tyre0=new Tyre();
-   Tyre tyre1=new Tyre();
-   Tyre tyre2=new Tyre();
-   Tyre tyre3=new Tyre();
 //assigning for methods of car 1
 
-car1.engine=engine1;
-car1.driver=driver1;
-car1.tyres0=tyre0;
-car1.tyres1=tyre1;
-car1.tyres2=tyre2;
-car1.tyres3=tyre3;
+   car1.engine=engine1;
+  car1.driver=driver1;
+  
+for(int num=0;num<=3;num++){
+Tyre tyre=new Tyre();
+car1.tyres[num]=tyre;
+}
 //input details
-
 car1.inputDetails();
-//method to add tyre
- car1.addTyre();
+
+
    // input for car 2
-   
 Car car2=new Car();
 //objects for car 2
-   Engine engine2=new Engine();
+  Engine engine2=new Engine();
    Driver driver2=new Driver();
-   Tyre tyre4=new Tyre();
-   Tyre tyre5=new Tyre();
-   Tyre tyre6=new Tyre();
-   Tyre tyre7=new Tyre();
+
 
 //assigng for method for car 2
-
  car2.engine=engine2;
 car2.driver=driver2;
-car2.tyres0=tyre4;
-car2.tyres1=tyre5;
-car2.tyres2=tyre6;
-car2.tyres3=tyre7;
-
+ for(int num=0;num<=3;num++){
+ Tyre tyre=new Tyre();
+ car2.tyres[num]=tyre;
+}
  
  //input deatils of car2
   
   car2.inputDetails();
-  // method to add tyre
-  car2.addTyre();
   
-  //comand to print about both cars*/
+  
+  //comand to print about both cars
  
     car1.printDetails();
-    car2.printDetails();
+ car2.printDetails();
   
   }
 
