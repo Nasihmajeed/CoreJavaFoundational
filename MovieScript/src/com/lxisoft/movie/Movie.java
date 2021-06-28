a968ac351555e45773e5b1a5d381bb979ce604e5
nishad96 <nishad.k@lxisoft.com>
2021-06-28 17:53:28 +0530

    http://jira.bytatech.com:8080/browse/IN-16407

--- MovieScript/src/com/lxisoft/movie/Movie.java
+++ MovieScript/src/com/lxisoft/movie/Movie.java
@@ -25,31 +25,9 @@ try{
   }
   }
 
-  public void printPlot(){
-    System.out.println("|**************************************************^^*************************************************************************************************************|");
-    System.out.println("|Unnikrishnan  owns an old bus, received as compensation for his father                                                                   |");
-    System.out.println("|The plot takes a big turn when a girl named Basanthi enters the bus as a nomad,                                                             |");
-    System.out.println("|but she is actually  Gayathri, the daughter of an influential and politically powerful minister R. K. Santhanam in Puducherry.           |");
-    System.out.println("|The police traced her and takes her back to Santhanam's custody. Santhanam is making arrangements for her marriage with someone else.    |");
-    System.out.println("|Meanwhile, Unni realizes that he could not live without Gayathri. He and Sundareshan secretly enter her house.                         |");
-    System.out.println("|Unni and his friends plans to take Gayathri, but Santhanam find them inside the almirah. After a fight, everything ends well.           |");
-    System.out.println("|***************************************************************************************************************************************************|");
-  }
-  public void printDialogue(){
-    System.out.println("\n\n***************************************************************************************************");
-    System.out.println("*********************************SCENE1*********************************************");
-    System.out.println("************************************************************************************");
-    System.out.println("Sundhareshan enters the  scene humming a song, finds Basanthi siiting infront of the bus\n\n");
 
-  }
 
-  public void printCast(){
-    System.out.println("|****************************************|");
-    System.out.println("|Dileep as Unnikrishnan                  |");
-    System.out.println("|Harisree Ashokan as Sundhareshan        |");
-    System.out.println("|Nithya Das as Basanthi,Gayathri         |");
-    System.out.println("|Cochin Haneefa as Veerappan Kurupp      |");
-    System.out.println("|**************************************|");
-  }
+
+
 
   }
