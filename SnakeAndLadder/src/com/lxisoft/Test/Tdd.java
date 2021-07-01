0a1f86c3e1d3dc8512bc1358205acb33ee41e5c3
@ROHITH-77 <rohith.r@lxisoft.com>
2021-07-01 11:51:44 +0530

    git add Rohith@DESKTOP-D1QOM3S MINGW64 /f/Intership 2/Core Java Foundational/CoreJavaFoundational (rohith) $ git add -A warning: LF will be replaced by CRLF in SnakeAndLadder/src/com/lxisoft/Test/Tdd.java. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in SnakeAndLadder/src/com/lxisoft/game/Game.java. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in SnakeAndLadder/src/com/lxisoft/Game/Players. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in SnakeAndLadder/src/com/lxisoft/Game/Players.java. The file will have its original line endings in your working directory
    Rohith@DESKTOP-D1QOM3S MINGW64 /f/Intership 2/Core Java Foundational/CoreJavaFoundational (rohith)
    $ git commit -m

--- SnakeAndLadder/src/com/lxisoft/Test/Tdd.java
+++ SnakeAndLadder/src/com/lxisoft/Test/Tdd.java
@@ -1,10 +1,14 @@
-package com.LxiSoft.Test;
-import com.LxiSoft.Game.*;
+package com.lxisoft.Test;
+import com.lxisoft.Game.Game;
 public class Tdd
 {
-public static void main(String args[])
+	public static void main (String[] args)
 	{
-		PlayBoard playBoard=new PlayBoard();
-		playBoard.createBoard();
+	  Game game = new Game();
+	  game.details();
+	  game.playersDetails();
+	  game.board();
+	  game.planHowToPlayTheGame();
+	  game.finalWinner();
 	}
 }
