038a9996b3567d15ffd02d2b0378c6a2421d834a
Saikrishna <sai.krishnaprasad07@gmail.com>
2021-07-29 17:06:28 +0530

    http://34.87.37.122:8080/browse/IN-17074

--- MovieScript/src/com/lxisoft/scripts/Script.java
+++ MovieScript/src/com/lxisoft/scripts/Script.java
@@ -8,46 +8,19 @@ import java.io.*;
 public class Script
 {
 
-  private String scriptWriterName;
-
-  public void setScriptWriterName(String scriptWriterName)
-  {
-     this.scriptWriterName=scriptWriterName;
-  }
-  
-
-  public String getScriptWriterName()
-  {
-  return scriptWriterName;  	
-  }
-
-
+public void writeScript(int genre , HashMap<Integer,ArrayList<Actor>> actors)
+  {          
+      String line;
 
  List<String> romanticDialogues=new ArrayList<String>();
  List<String> emotionalDialogues=new ArrayList<String>();
  List<String> comedyDialogues=new ArrayList<String>();
  List<String> actionDialogues=new ArrayList<String>();
 
-
-
-public void writeScript(int genre , HashMap<Integer,ArrayList<Actor>> actors)
-  {          
-
-String line;
-
 String romance=new String("com\\lxisoft\\resources\\RomanticScr.CSV");
 String comedy=new String("com\\lxisoft\\resources\\EmotionalScr.CSV");
 String emotional=new String("com\\lxisoft\\resources\\ComedyScr.CSV");
 String action=new String("com\\lxisoft\\resources\\ActionScr.CSV");
-
-
-                    try{
-                        Thread.sleep(2000);
-                       }
-                catch(InterruptedException ie)
-                       {
-                        Thread.currentThread().interrupt();
-                       }
 
 
     try{
