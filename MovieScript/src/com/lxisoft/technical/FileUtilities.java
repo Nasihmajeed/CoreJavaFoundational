6373fff3c2e491f559f75bdab3720859132e8f68
Amalraveendran <36656127+Amalraveendran@users.noreply.github.com>
2021-08-03 17:31:07 +0530

    http://34.87.37.122:8080/browse/IN-17192

--- MovieScript/src/com/lxisoft/technical/FileUtilities.java
+++ MovieScript/src/com/lxisoft/technical/FileUtilities.java
@@ -53,47 +53,7 @@ return dialogue;
 
 }
 
-public fetchMinorContents()
-{
-	file=new File(scriptPath+(genere.toString()+extension));
-				
-				try{
-							scriptReader=new BufferedReader(new FileReader(file));
-								String line=null;
 
-									while((line=scriptReader.readLine())!=null  )
-									{
-										String []parts=line.split(":");
-										String scene=parts[0].trim();
-									
-										StringBuilder dialogue=new StringBuilder();
-										dialogue.append(line);
-					
-							
-									}
-					
-							
-					}
-					catch(IOException e)
-						{
-						e.printStackTrace();
-						}
-					finally
-					{
-
-						try
-						{
-							scriptReader.close();
-			
-						}
-						catch(IOException e)
-						{
-
-						}
-					}
-
-return dialogue;
-}
 
 
 
