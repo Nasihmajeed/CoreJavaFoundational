71a6ce13765d4efbc19db3ff67140af6af503d5b
NishadK96 <nishadgopalakrishnan0@gmail.com>
2021-08-10 16:50:39 +0530

    http://34.87.37.122:8080/browse/IN-17301

--- MovieScript/src/com/lxisoft/director/Director.java
+++ MovieScript/src/com/lxisoft/director/Director.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Director{
   String directorName;
-  Map<String,ArrayList<Actor>> castingActors==new HashMap<String,ArrayList<Actor>>();
+
 
 
   Scanner sc= new Scanner(System.in);
@@ -13,33 +13,26 @@ public class Director{
     directorName=sc.next();
     for(Actor a : actors){
         if(a instanceof Hero){
-          castingActors.put("hero",actors);
+          System.out.println("HEROES\n");
+          System.out.println(a.getActorName()+" "+a.getCharacterName());
+        }
+        if(a instanceof Heroine){
+          System.out.println("HEROINES\n");
+          System.out.println(a.getActorName()+" "+a.getCharacterName());
+        }
+        if(a instanceof Villain){
+          System.out.println("VILLAINES\n");
+          System.out.println(a.getActorName()+" "+a.getCharacterName());
+        }
+        if(a instanceof Comedian){
+          System.out.println("COMEDIANS\n");
+          System.out.println(a.getActorName()+" "+a.getCharacterName());
         }
 
-            else if(a instanceof Heroine){
-              castingActors.put("heroine",actors);
-            }
-            else if(a instanceof Comedian){
-              castingActors.put("comedian",actors);
-            }
-            else if(a instanceof Villain){
-                castingActors.put("villain",actors);
-              }
+
 
 
 
 }
-System.out.println("HEROES\n");
-System.out.println(castingActors.get("hero").get(0).getActorName()+" IN AND AS "+castingActors.get("hero").get(0).getCharacterName());
-
-System.out.println("HEROINES\n");
-System.out.println(castingActors.get("heroine").get(1).getActorName()+" IN AND AS "+castingActors.get("heroine").get(1).getCharacterName());
-
-System.out.println("COMEDIAN\n");
-System.out.println(castingActors.get("comedian").get(2).getActorName()+" IN AND AS "+castingActors.get("comedian").get(2).getCharacterName());
-
-System.out.println("VILLAIN\n ");
-System.out.println(castingActors.get("villain").get(3).getActorName()+" IN AND AS "+castingActors.get("villain").get(3).getCharacterName());
-
 }
 }
