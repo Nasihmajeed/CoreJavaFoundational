58faa7497a21f9bf23526f708db6623b30f57760
NishadK96 <nishadgopalakrishnan0@gmail.com>
2021-08-19 17:29:41 +0530

    http://34.87.37.122:8080/browse/IN-17582

--- MovieScript/src/com/lxisoft/director/Director.java
+++ MovieScript/src/com/lxisoft/director/Director.java
@@ -4,35 +4,67 @@ import java.util.*;
 
 public class Director{
   String directorName;
+    ArrayList<Actor>actors= new ArrayList<Actor>();
 
 
 
   Scanner sc= new Scanner(System.in);
-  public void castActors(ArrayList<Actor> actors){
+  public void castActors(){
     System.out.println("ENTER DIRECTOR NAME ");
     directorName=sc.next();
-    for(Actor a : actors){
-        if(a instanceof Hero){
-          System.out.println("\nHEROES");
-          System.out.println(a.getActorName()+"IN AND AS "+a.getCharacterName());
-        }
-        if(a instanceof Heroine){
-          System.out.println("\nHEROINES");
-          System.out.println(a.getActorName()+"IN AND AS "+a.getCharacterName());
-        }
-        if(a instanceof Villain){
-          System.out.println("\nVILLAINES");
-          System.out.println(a.getActorName()+"IN AND AS "+a.getCharacterName());
-        }
-        if(a instanceof Comedian){
-          System.out.println("\nCOMEDIANS");
-          System.out.println(a.getActorName()+"IN AND AS "+a.getCharacterName());
-        }
+    System.out.println("ENTER NUMBER OF HEROES");
+  int numOfHeroes=sc.nextInt();//  a.setNumOfActors(sc.nextInt());
+    for(int i=0;i<numOfHeroes;i++){
+      System.out.println("ENTER THE NAME OF HERO ");
+      String actorName=sc.next();
+
+
+      System.out.println("ENTER THE CHARACTER NAME : ");
+      String characterName=sc.next();
+      actors.add(new Hero(actorName,characterName));
+    }
+
+    System.out.println("ENTER NUMBER OF HEROINES");
+    int numOfHeroines=sc.nextInt();
+    for(int i=0;i<numOfHeroines;i++){
+      System.out.println("ENTER THE NAME OF HEROINE ");
+      String actorName=sc.next();
+
+
+
+      System.out.println("ENTER THE CHARACTER NAME OF : ");
+      String characterName=sc.next();
+        actors.add(new Heroine(actorName,characterName));
+      }
+
+
+    System.out.println("ENTER NUMBER OF COMEDIANS");
+    int numOfComedians=sc.nextInt();
+    for(int i=0;i<numOfComedians;i++){
+      System.out.println("ENTER THE NAME OF COMEDIAN ");
+      String actorName=sc.next();
+
+      System.out.println("ENTER THE CHARACTER NAME OF : ");
+      String characterName=sc.next();
+      actors.add(new Comedian(actorName,characterName));
+    }
+
+    System.out.println("ENTER NUMBER OF VILLAINS");
+    int numOfVillains=sc.nextInt();
+    for(int i=0;i<numOfVillains;i++){
+      System.out.println("ENTER THE NAME OF VILLAIN ");
+      String actorName=sc.next();
+
+
+
+      System.out.println("ENTER THE CHARACTER NAME : ");
+      String characterName=sc.next();
+      actors.add(new Villain(actorName,characterName));
+    }
 
 
 
 
 
-}
 }
 }
--- MovieScript/src/com/lxisoft/movie/Movie.java
+++ MovieScript/src/com/lxisoft/movie/Movie.java
@@ -17,7 +17,7 @@ public class Movie{
   private String movieName;
   Director director=new Director();
   ScriptWriter writer=new ScriptWriter();
-    ArrayList<Actor>actors= new ArrayList<Actor>();
+
   public void setMovieName(String movieName){
     this.movieName=movieName;
   }
@@ -29,55 +29,6 @@ public class Movie{
   public  void startMovie() {
     System.out.println("ENTER MOVIE NAME ");
     setMovieName(sc.next());
-    System.out.println("ENTER NUMBER OF HEROES");
-  int numOfHeroes=sc.nextInt();//  a.setNumOfActors(sc.nextInt());
-    for(int i=0;i<numOfHeroes;i++){
-      System.out.println("ENTER THE NAME OF HERO ");
-      String actorName=sc.next();
-
-
-      System.out.println("ENTER THE CHARACTER NAME : ");
-      String characterName=sc.next();
-      actors.add(new Hero(actorName,characterName));
-    }
-
-    System.out.println("ENTER NUMBER OF HEROINES");
-    int numOfHeroines=sc.nextInt();
-    for(int i=0;i<numOfHeroines;i++){
-      System.out.println("ENTER THE NAME OF HEROINE ");
-      String actorName=sc.next();
-
-
-
-      System.out.println("ENTER THE CHARACTER NAME OF : ");
-      String characterName=sc.next();
-        actors.add(new Heroine(actorName,characterName));
-      }
-
-
-    System.out.println("ENTER NUMBER OF COMEDIANS");
-    int numOfComedians=sc.nextInt();
-    for(int i=0;i<numOfComedians;i++){
-      System.out.println("ENTER THE NAME OF COMEDIAN ");
-      String actorName=sc.next();
-
-      System.out.println("ENTER THE CHARACTER NAME OF : ");
-      String characterName=sc.next();
-      actors.add(new Comedian(actorName,characterName));
-    }
-
-    System.out.println("ENTER NUMBER OF VILLAINS");
-    int numOfVillains=sc.nextInt();
-    for(int i=0;i<numOfVillains;i++){
-      System.out.println("ENTER THE NAME OF VILLAIN ");
-      String actorName=sc.next();
-
-
-
-      System.out.println("ENTER THE CHARACTER NAME : ");
-      String characterName=sc.next();
-      actors.add(new Villain(actorName,characterName));
-    }
 
 
  /*  for(Map.Entry m : cast.entrySet()){
@@ -87,8 +38,8 @@ public class Movie{
 
         }
       }*/
-  
-    director.castActors(actors);
+
+    director.castActors();
 try{
     writer.writeScript();
   }
