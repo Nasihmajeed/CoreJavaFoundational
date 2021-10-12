62970ba76303507763c7dee70b7034e710d42906
@ROHITH-77 <rohith.r@lxisoft.com>
2021-10-12 12:24:11 +0530

    http://34.87.37.122:8080/browse/IN-18587

--- SnakeAndLadder/src/com/lxisoft/Test/Tdd.java
+++ SnakeAndLadder/src/com/lxisoft/Test/Tdd.java
@@ -1,9 +1,10 @@
-package com.lxisoft.Test;
-import com.lxisoft.SnakeAndLadder.Game;
-public class TDD{
-	public static void main(String[] args) {
-		Game game = new Game();
-		game.startGame();
-		//game.winner();
+package com.lxisoft.test;
+import com.lxisoft.game.*;
+public class Tdd
+{
+public static void main(String args[])
+	{
+		PlayBoard playBoard=new PlayBoard();
+		playBoard.createBoard();
 	}
 }
