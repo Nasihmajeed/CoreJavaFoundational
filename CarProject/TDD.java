0f8640cee96c31931948cc4fb5e9f694a2b4a2c7
[SAHADH] <[sahadh.k.s@lxisoft.com]>
2021-11-10 17:25:11 +0530

    http://34.87.37.122:8080/browse/IN-18983

--- CarProject/TDD.java
+++ CarProject/TDD.java
@@ -1,101 +1,61 @@
 import java.util.Scanner;
-class Car
+public class TDD
 {
-       String Brand;
+       public static void main(String[] ar)
+       {
+              String Brand,Model,RegNo,Color ;
 
-       String Model;
-
-       String RegNo;
-
-       String Color;
-
-       Scanner data = new Scanner(System.in);
-       
-       public  void PrintDetails(){
-              System.out.println("Mention the Brand of your car" );
-
-              Brand = data.nextLine();
-
-              System.out.println("Which model is you want ?");
-
-              Model = data.nextLine();
-
-              System.out.println("please Enter your registration number: ");
-
-              RegNo = data.nextLine();
-
-              System.out.println("which color varient is preffering!");
-
-              Color = data.nextLine();
-
-             
-       System.out.println("CAR BRAND IS :" +Brand);
-
-       System.out.println("CAR MODEL IS :" +Model);
-
-       System.out.println("CAR REGISTERED AS :" +RegNo);
-
-       System.out.println("COLOR OF THE CAR IS :" +Color);
-
-       }
-}
-
-
-  
-class Tyre
-{
-	
-String tyreType;
-
-String Brand;
-
-float Radius;
-
-String t1,t2,t3,t4;
-
-public void typeOfTyre()
-
-{
-	System.out.println("CAR TYRE IS : "+tyreType);
-
-       System.out.println("BRAND OF TYRE IS : "+Brand);
-
-       System.out.println("RADIUS OF TYRE IS : "+Radius);
-
-       System.out.println("THE CAR HAVE FOUR TYRE'S : " + t1 + " ,"+t2+","+t3+", "+t4 );
-}
-
-}
-	
-
-public class TDD{
-      
-       public static void main(String[] ar){
+              Scanner data = new Scanner(System.in);
               
-              System.out.println("WELCOME TO THE WORLD OF CARS");
-
-              Car car1 = new Car();
-
-              car1.PrintDetails();
-
+              System.out.println("WELCOME TO THE WORLD OF CARS");      
+              
+              System.out.println("Mention the Brand of your car" );
+              
+              Brand = data.nextLine();
+              
+              System.out.println("Which model is you want ?");
+              
+              Model = data.nextLine();
+              
+              System.out.println("please Enter your registration number: ");
+              
+              RegNo = data.nextLine();
+              
+              System.out.println("which color varient is preffering!");
+              
+              Color = data.nextLine();
+              
+              Car c1 = new Car();
+              
+              c1.Brand=Brand;
+              
+              
+              c1.Model=Model;
+              
+              c1.RegNo=RegNo;
+              
+              c1.Color=Color;
+              
               Tyre t=new Tyre();
-
+              
               t.tyreType="Flat";
-
+              
               t.Brand="MRF";
-
+              
               t.Radius=3.75f;
-
+              
               t.t1="FRONT_LEFT_TYRE";
-
+              
               t.t2="FRONT_RIGHT_TYRE";
-
+              
               t.t3="BACK_RIGHT_TYRE";
-
+              
               t.t4="BACK_LEFT_TYRE";
-
-              t.typeOfTyre();
+              
+              c1.PrintDetails();
+              
+              t.PrintDetails();
        }
-    
        
 }
+
---
+++ CarProject/Tyre.java
@@ -0,0 +1,26 @@
+public class Car
+{
+      
+              String Brand;
+             
+              String Model;
+             
+              String RegNo;
+             
+              String Color;
+
+       public void PrintDetails()
+       {
+              
+              System.out.println("CAR BRAND IS : " +this.Brand);
+              
+              System.out.println("CAR MODEL IS : " +this.Model);
+              
+              System.out.println("CAR REGISTERED AS : " +this.RegNo);
+              
+              System.out.println("COLOR OF THE CAR IS : " +this.Color);
+              
+             
+      
+       }
+}
