019bd3a7c3a84090f1c13863f218b9a866f4d169
GokulKizhuveettil <gokul.k@lxisoft.com>
2021-11-12 19:21:35 +0530

    http://34.87.37.122:8080/browse/IN-19036

---
+++ BasicPrograms/pattern2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class Pattern2
+{
+
+public static void main(String[] args)
+
+{
+
+int i, j;
+ 
+ for (i =5; i >0; i--) 
+{
+   
+System.out.println("\n");
+    
+for (j = 1; j <= i; j++) 
+{
+     
+System.out.print("*");
+    
+}
+  }
+}
+}
