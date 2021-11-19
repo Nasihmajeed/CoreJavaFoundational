bb522ba3b567ee0595a146605697e81f1cd1e733
kasujith <sujith.k.a@lxisoft.com>
2021-11-19 18:10:20 +0530

    http://34.87.37.122:8080/browse/IN-19200

---
+++ CarProject/Car.java
@@ -0,0 +1,22 @@
+public class Car {
+String name;
+String model;
+String RegNo;
+String colour;
+public void inputDetails() {
+
+this.name= "Maruthi";
+this.model= "Maruthi 800";
+this.RegNo= "Kl-09-AB-9003";
+this.colour= "Red";
+}
+void printDetails()
+{
+System.out.println("CAR NAME:" +name);
+System.out.println("CAR MODEL:" +model);
+System.out.println("CAR REGISTER NUMBER:" +RegNo);
+System.out.println("CAR COLOUR:" +colour);
+
+
+}
+}
---
+++ CarProject/TDD.java
@@ -0,0 +1,9 @@
+public class TDD {
+public static void main(String[] args) {
+for(int i=0;i<3;i++) {
+Car C = new Car();
+C.inputDetails();
+C.printDetails();
+}
+}
+}
