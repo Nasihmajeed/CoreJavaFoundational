4009dfc80e02855b5e749173d08c35afbba1819d
mubashir.k <mubashir.k@lxisoft.com>
2021-11-19 18:24:12 +0300

    http://34.87.37.122:8080/browse/IN-19232

---
+++ CarProject/Car.java
@@ -0,0 +1,36 @@
+
+public class Car  {
+
+String model;
+String brand;
+String registerno;
+String colour;
+
+
+
+public void printDetails()   {
+
+System.out.println("Car Model:" + this.model);
+System.out.println("Car Brand:" + this.brand);
+System.out.println("Car Register number:" + this.registerno);
+System.out.println("Car colour:" + this.colour);
+System.out.println("\n");
+
+
+
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+                      
