978501f43283768fb6aa03ddff946e1765b1a676
kasujith <sujith.k.a@lxisoft.com>
2021-11-22 13:42:18 +0530

    http://34.87.37.122:8080/browse/IN-19200

--- CarProject/TDD.java
+++ CarProject/TDD.java
@@ -2,8 +2,19 @@ public class TDD {
 public static void main(String[] args) {
 for(int i=0;i<3;i++) {
 Car C = new Car();
+
+Tyre T1 = new Tyre();
+C.t1=T1;
+Tyre T2 = new Tyre();
+C.t2=T2;
+Tyre T3 = new Tyre();
+C.t3=T3;
+Tyre T4 = new Tyre();
+C.t4=T4;
+
 C.inputDetails();
 C.printDetails();
+
 }
 }
 }
