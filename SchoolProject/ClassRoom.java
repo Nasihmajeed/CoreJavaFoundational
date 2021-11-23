e987a3f94fa84fbe64139f710aa23058bc28e462
[SAHADH] <[sahadh.k.s@lxisoft.com]>
2021-11-23 17:51:58 +0530

    http://34.87.37.122:8080/projects/IN/issues/IN-19076?filter=allopenissues

--- SchoolProject/ClassRoom.java
+++ SchoolProject/ClassRoom.java
@@ -1,60 +1,24 @@
-public class ClassRoom {
-       String classes;
+public class ClassRoom  {
+	
+	String name;
+	int seats = 35;
+       int number;
        String div;
-       int numOfSeats;
-       Teacher[] ts = new  Teacher[2];
-       Student[] ss = new Student[6];
-       public void inputDetails1() {
-              this.classes = "10 TH";
-              
-              this.div = "A";
-	 this.numOfSeats = 60;
-              
-              this.ts[0].inputDetails1();
-             this.ss[0].inputDetailsS1();
-              this.ss[1].inputDetailsS1();
-              this.ss[2].inputDetailsS1();
-              
+       //Teacher[] t = new Teacher[3];
+	ClassRoom(String n,int i,String d){
+           name = n;
+           number = i;
+           div = d;
        }
-       public void inputDetails2() {
-            
-              this.classes = "10 TH";
-              this.div = "B";
-              this.numOfSeats = 60;
-              this.ts[1].inputDetails2();
-              this.ss[3].inputDetailsS1();
-              this.ss[4].inputDetailsS1();
-              this.ss[5].inputDetailsS1();
 
-       }
-       public void printDetails1() {
-              System.out.println();
-              System.out.println("~~~~~~CLASS ~~~~~~~~~");
-              System.out.println();
-              System.out.println("THE DETAILS OF "+classes+" "+div);
-              System.out.println("TOTAL NUMBER OF SEATS ARE: " +numOfSeats);
-             this.ts[0].printDetails1();
-              System.out.println();
-              this.ss[0].printDetailsS1();
-              System.out.println();
-              this.ss[1].printDetailsS2();
-              System.out.println();
-              this.ss[2].printDetailsS3();
-        
-       }
-       public void printDetails2() {
-              System.out.println();
-              System.out.println("~~~~~~CLASS ~~~~~~~~~");
-              System.out.println();
-              System.out.println("THE DETAILS OF "+classes+" "+div);
-              System.out.println("TOTAL NUMBER OF SEATS ARE: " +numOfSeats);
-              this.ts[1].printDetails2();
-              System.out.println();
-              this.ss[3].printDetailsS4();
-              System.out.println();
-              this.ss[4].printDetailsS5();
-              System.out.println();
-              this.ss[5].printDetailsS6();
-       }
-   
-}
+
+	public void printDetails() {
+			System.out.println(name +" "+ number +" "+ div + "\n");
+			
+		System.out.println("NUMBER OF SEATS : " + seats + "\n");
+		
+             // for (int i = 0; i < t.length; i++) {
+               //      this.t[i].printDetails();
+            //  }
+		}
+	}
--- SchoolProject/TDD.java
+++ SchoolProject/TDD.java
@@ -1,42 +1,21 @@
 public class TDD {
+
        public static void main(String[] args) {
-              System.out.println("---------- WELCOME TO MHSS ---------");
-              System.out.println();
-        School s = new School();
+		School s = new School();
+              
+		for (int i = 0; i < 3; i++) {
+			ClassRoom c = new ClassRoom("CLASSROOM",i+1,"A");
+			s.c[i] = c;
+              
+              
+                    // Teacher t = new Teacher("TEACHER","SUBJECT","phoneNumber");
+                   //  s.c[i].t[i]=t;
+              
+              }
+		
+		s.inputDetails();
+		s.printDetails();
 
-        ClassRoom c1 = new ClassRoom();
-        s.classes[0] = c1;
+	}
 
-        Teacher t1 = new Teacher();
-        c1.ts[0] = t1;
-
-        Student s1 = new Student();
-        
-        c1.ss[0] = s1;
-       
-        Student s2 = new Student();
-        
-        c1.ss[1] = s2;
-
-        Student s3 = new Student();
-        c1.ss[2] = s3;
-
-        ClassRoom c2 = new ClassRoom();
-        s.classes[1] = c2;
-
-        Teacher t2 = new Teacher();
-        c2.ts[1] = t2;
-
-        Student s4 = new Student();
-        c2.ss[3] = s4;
-
-        Student s5 = new Student();
-        c2.ss[4] = s5;
-
-        Student s6 = new Student();
-        c2.ss[5] = s6;
-
-        s.inputDetails();
-        s.printDetails();      
-       }
 }
