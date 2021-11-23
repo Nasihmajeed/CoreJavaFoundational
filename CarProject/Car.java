25bc9d13d6fd16ed8580f607c547506ed33b50dc
mubashir.k <mubashir.k@lxisoft.com>
2021-11-23 18:07:56 +0300

    http://34.87.37.122:8080/browse/IN-19232

--- CarProject/Car.java
+++ CarProject/Car.java
@@ -1,11 +1,11 @@
-
 public class Car  {
 
 String model;
 String brand;
 String registerno;
 String colour;
-
+String price;
+Tyre t1,t2,t3,t4;
 
 
 public void printDetails()   {
@@ -14,23 +14,35 @@ System.out.println("Car Model:" + this.model);
 System.out.println("Car Brand:" + this.brand);
 System.out.println("Car Register number:" + this.registerno);
 System.out.println("Car colour:" + this.colour);
+System.out.println("Car price:" + this.price);
+System.out.println("\n");
 System.out.println("\n");
 
+ t1.brand = "appollo";
+t1.position = "Front Right";
+t1.radius = "5";
 
+t2.brand = "appollo";
+t2.position = "Front Left";
+t2.radius = "5";
+
+t3.brand = "appollo";
+t3.position = "Back Right";
+t3.radius = "5";
+
+t4.brand = "appollo";
+t4.position = "Back Left";
+t4.radius = "Back Right"; 
 
 }
 
+public Tyre tyreDetails()  {
 
+t1.printDetails();
+t2.printDetails();
+t3.printDetails();
+t4.printDetails();
+
+  
 }
-
-
-
-
-
-
-
-
-
-
-
-                      
+}
---
+++ CarProject/Tyre.java
@@ -0,0 +1,12 @@
+public class Tyre {
+
+String brand ;
+String position;
+String radius;
+
+
+public void printDetails()  {
+System.out.println("TYRE");
+System.out.println(brand + position + radius);
+}
+}
