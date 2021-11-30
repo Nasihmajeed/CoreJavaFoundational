faee2519d39503d8383c3cc13650a30eb2d3dc12
kasujith <sujith.k.a@lxisoft.com>
2021-11-30 18:34:23 +0530

    http://34.87.37.122:8080/browse/IN-19374

---
+++ MovieScript/com/lxisoft/Cast/Hero.java
@@ -0,0 +1,21 @@
+public class Hero  {
+  private String name;
+  private String characterName;
+   
+   public String getName() {
+    	return name;
+
+}
+  public void setName(String name) {
+     this.name=name;
+}
+
+public String getcharacterName() {
+    	return characterName;
+}
+  public void setCharacterName(String characterName) {
+     this.characterName=characterName;
+}
+   }
+  
+ 
---
+++ MovieScript/com/lxisoft/Cast/TDD.java
@@ -0,0 +1,12 @@
+public class TDD  {
+ 	public static void main(String[] args) {
+	Hero hero = new Hero();
+        hero.setName("Dileep");
+    	hero.setCharacterName("Madhavan"); 
+System.out.println(hero);
+} 
+} 
+
+	
+	
+	
