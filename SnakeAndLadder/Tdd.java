e4108959db8396fe76a695fba0e34d199f4fc19b
ShabnaSirajudeen <shabna.sirajudeen@lxisoft.com>
2021-12-01 17:55:22 +0530

    http://34.87.37.122:8080/browse/IN-19363

--- SnakeAndLadder/Tdd.java
+++ SnakeAndLadder/Tdd.java
@@ -1,7 +1,23 @@
 public class Tdd {
 
 	public static void main(String[] args) {
-
+		System.out.println("WELCOME TO SNAKES AND LADDERS");
+		System.out.println("==============================");
+		System.out.println("Board");
+		System.out.println("-------");
+		System.out.println("*Number of cells: ");
+		System.out.println("*Number of snakes: ");
+		System.out.println("*Number of ladders: \n");
+		System.out.println("Players");
+		System.out.println("-------");
+		System.out.println("*Player1");
+		System.out.println("*Player2\n");
+		System.out.println("Dice");
+		System.out.println("-------");
+		System.out.println("*Number of faces:6");
+		System.out.println("*Value to start the game:1\n");
+		System.out.println("Let's start the game...\n");
+		System.out.println("Player1 rolling the die...");
 	}
 
 }
