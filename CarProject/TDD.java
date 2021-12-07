29057d6b6a74827bf9066967b8ada0670c7695ca
mubashir.k <mubashir.k@lxisoft.com>
2021-12-07 18:09:56 +0300

    http://34.87.37.122:8080/browse/IN-19232

--- CarProject/TDD.java
+++ CarProject/TDD.java
@@ -6,20 +6,25 @@ public static void main(String [] args)  {
 
 Scanner sc = new Scanner(System.in);
 
-
+for(int i = 1; i <=3; i++)  {
 
 Car c = new Car();
-
+System.out.println("CAR ="+i);
 
 
 
 Tyre tyre = new Tyre();
-c.tyre = tyre;
-
+c.T1= tyre;
+Tyre tyre2 = new Tyre();
+c.T2= tyre2;
+Tyre tyre3 = new Tyre();
+c.T3= tyre3;
+Tyre tyre4 = new Tyre();
+c.T4= tyre4;
 
 c.printDetails();
 
-
+}
 }
 
 }
