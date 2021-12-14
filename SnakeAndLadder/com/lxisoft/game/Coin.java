de440433edebaa082a9f8961dc0473df81fbe975
ShabnaSirajudeen <shabna.sirajudeen@lxisoft.com>
2021-12-14 17:34:19 +0530

    http://34.87.37.122:8080/browse/IN-19363

---
+++ SnakeAndLadder/com/lxisoft/game/Coin.java
@@ -0,0 +1,19 @@
+package com.lxisoft.game;
+
+public class Coin {
+	private int position;
+	private String color;
+	public int getPosition() {
+		return position;
+	}
+	public void setPosition(int position) {
+		this.position = position;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+}
