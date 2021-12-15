c35d3491c01456f81d4210e31846911a82b91517
kasujith <sujith.k.a@lxisoft.com>
2021-12-15 18:01:47 +0530

    http://34.87.37.122:8080/browse/IN-19374

--- MovieScript/com/lxisoft/moviescript/Movie.java
+++ MovieScript/com/lxisoft/moviescript/Movie.java
@@ -22,7 +22,7 @@ public class Movie {
    public void printDetails (){
 
       
-    System.out.println("\t\t          -- Movie Details --");
+    System.out.println("\t\t          -- Movie Details -- ");
 
 
        System.out.println("\t\t+----------------------------------+");
@@ -55,8 +55,42 @@ public class Movie {
         heroine.characterName ="\t\tCharacter Name:Hazel Graze Lancaster";
         heroine.showDetails();
 
+        System.out.println("\t\t+----------------------------------+");
+
+        Comedian comedian1 = new Comedian();
+        comedian1.name = "\t\tComedian Name:  Jennifer Aniston ";
+        comedian1.characterName = "\t\tCharacter Name: Rachel Green";
+        comedian1.showDetails();
+
+        System.out.println("\t\t+----------------------------------+");
+
+        Comedian comedian2 = new Comedian();
+        comedian2.name = "\t\tComedian Name: Courteney Cox ";
+        comedian2.characterName = "\t\tCharacter Name: Monica ";
+        comedian2.showDetails();
+
+        System.out.println("\t\t+----------------------------------+");
+
+        Comedian comedian3 = new Comedian();
+        comedian3.name = "\t\tComedian Name: Matthew Perry ";
+        comedian3.characterName = "\t\tCharacter Name: Chandler ";
+        comedian3.showDetails();
+
+        System.out.println("\t\t+----------------------------------+");
+
+        Comedian comedian4 = new Comedian();
+        comedian4.name = "\t\tComedian Name: Matt LeBlanc ";
+        comedian4.characterName = "\t\tCharacter Name: Jeoy ";
+        comedian4.showDetails();
+
+        System.out.println("\t\t+----------------------------------+");
+
+        Comedian comedian5 = new Comedian();
+        comedian5.name = "\t\tComedian Name: Lisa Kudrow ";
+        comedian5.characterName = "\t\tCharacter Name: Phoebe ";
+        comedian5.showDetails();
+
     
-		
         Script script = new Script();
         script.inputDetails();
         script.printDetails();
