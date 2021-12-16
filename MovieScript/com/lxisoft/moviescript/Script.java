d531198040cd9f534edee210c4bedf331f283e84
kasujith <sujith.k.a@lxisoft.com>
2021-12-16 19:15:41 +0530

    http://34.87.37.122:8080/browse/IN-19374

--- MovieScript/com/lxisoft/moviescript/Script.java
+++ MovieScript/com/lxisoft/moviescript/Script.java
@@ -1,9 +1,10 @@
 package com.lxisoft.moviescript;
 
-public class Script {
+  public class Script {
     String scene1;
     String scene2;
     String scene3;
+
     public void inputDetails() {
         this.scene1=" 1";
         this.scene2=" 2";
@@ -11,157 +12,79 @@ public class Script {
 }
     public void printDetails() {
 
-        System.out.println("\n");
+        System.out.println("\n");   
 
         System.out.println("\t\t      SCENE " +scene1);
+
         System.out.println("\t\t+----------------+" +"\n");
 
-        try{ 
-            Thread.sleep(1500);
-            }
-      catch(InterruptedException e)
-           {
-           Thread.currentThread().interrupt();
-           }
-
         System.out.println("\t\t  ( OUTSIDE THE CHURCH )  "+"\n");
-        try{ 
-            Thread.sleep(1000);
-            }
-      catch(InterruptedException e)
-           {
-           Thread.currentThread().interrupt();
-           }
+       
         System.out.println("\t ** Hazel waits for her Mom’s car to appear ** " +"\n");
 
         System.out.println("\t** Hazel turns to find the Beautiful Boy, Augustus, standing right next to her. **" +"\n");
-
-        try{ 
-            Thread.sleep(1000);
-            }
-      catch(InterruptedException e)
-           {
-           Thread.currentThread().interrupt();
-           }
 
         System.out.println("\tGUS: I thought we were in a church basement but apparently we were literally in the  heart of Jesus." +"\n");
 
         System.out.println("\t** Hazel smiles. **" +"\n");
 
-        try{ 
-            Thread.sleep(1000);
-            }
-      catch(InterruptedException e)
-           {
-           Thread.currentThread().interrupt();
-           }
-
         System.out.println("\tHAZEL: Someone should probably tell him, dont you think? Jesus? Seems kinda dangerous  keeping all these kids with cancer in  your heart." +"\n");
 
         System.out.println("\t** Gus laughs. **" +"\n");
 
-        try{ 
-            Thread.sleep(1000);
-            }
-      catch(InterruptedException e)
-           {
-           Thread.currentThread().interrupt();
-           }
-
         System.out.println("\tGUS: What's your name?" +"\n");
 
         System.out.println("\tHAZEL: Hazel." +"\n");
-        try{ 
-            Thread.sleep(1000);
-            }
-      catch(InterruptedException e)
-           {
-           Thread.currentThread().interrupt();
-           }
+        
         System.out.println("\tGUS: No your full name..." +"\n");
-
-        try{ 
-            Thread.sleep(1000);
-            }
-      catch(InterruptedException e)
-           {
-           Thread.currentThread().interrupt();
-           }
 
         System.out.println("\t** Hazel confused ** " +"\n");
 
         System.out.println("\tHAZEL: Hazel Grace Lancaster." +"\n");
 
-        try{ 
-            Thread.sleep(1000);
-            }
-      catch(InterruptedException e)
-           {
-           Thread.currentThread().interrupt();
-           }
-
         System.out.println("\t** Gus nods to himself, smiles. Still fixated on her. ** " +"\n");
 
         System.out.println("\t** Hazel Continued ** " +"\n");
-
-        try{ 
-            Thread.sleep(1000);
-            }
-      catch(InterruptedException e)
-           {
-           Thread.currentThread().interrupt();
-           }
 
         System.out.println("\tHAZEL: What?" +"\n");
 
         System.out.println("\tGUS: I did not say anything." +"\n");
 
-        try{ 
-            Thread.sleep(1000);
-            }
-      catch(InterruptedException e)
-           {
-           Thread.currentThread().interrupt();
-           }
-
         System.out.println("\tHAZEL: Why are you looking at me like that?" +"\n"); 
 
         System.out.println("\tGUS: Because you are beautiful." +"\n");
-
-        try{ 
-            Thread.sleep(713);
-            }
-      catch(InterruptedException e)
-           {
-           Thread.currentThread().interrupt();
-           }
 
         System.out.println("\t** Hazel is taken aback. No one’s ever said that to her before. ** " +"\n");
 
         System.out.println("\t** Gus Continued ** " +"\n");
 
-        try{ 
-            Thread.sleep(1000);
-            }
-      catch(InterruptedException e)
-           {
-           Thread.currentThread().interrupt();
-           }
-
         System.out.println("\tGUS: I enjoy looking at beautiful people and I  decided a while back not to deny myself  the simpler pleasures of existence.  Particularly given that, as you soastutely pointed out, we are all gonna die pretty soon. " +"\n");
 
         System.out.println("\tHAZEL: Iam not beaut --" +"\n");
 
-        try{ 
-            Thread.sleep(1000);
-            }
-      catch(InterruptedException e)
-           {
-           Thread.currentThread().interrupt();
-           }
+        System.out.println("\n");
 
-    
+        System.out.println("\t\t      SCENE " +scene2);
+
+        System.out.println("\t\t+----------------+" +"\n");
+
+        System.out.println("\t[Scene: Monica and Rachel's, late at night Monica is still examining her bill as Rachel emerges from her room.]");
+
+        System.out.println("\tRachel: Oh, Monica. You are not still going over that thing.");
+
+        System.out.println("\tMonica: This woman's living my life.");
+
+        System.out.println("\tRachel: What?");
+
+        System.out.println("\tMonica: She's living my life, and she's doing it better than me! Look at this, look. She buys tickets for plays that I wanna see. She, she buys clothes from stores that I'm intimidated by the sales people. She spent three hundred dollars on art supplies.");
         
+        System.out.println("\tRachel: You're not an artist.");
+   
+        System.out.println("\tMonica: Yeah, well I might be if I had the supplies! I mean, I could do all this stuff. Only I don't.");
+
+        System.out.println("\tRachel: Oh, Monica, c'mon, you do cool things.");
+
+        System.out.println("\tMonica: Oh really? Okay, let's compare, shall we.");
     }
     }  
 
