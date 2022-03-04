5ca72c9baf3f8148cd79787732b23db17d70ddcb
sreejith.s <sreejith.s@lxisoft.com>
2022-03-04 19:17:23 +0530

    http://34.87.37.122:8080/browse/IN-20801

--- CarProject/Tyre.java
+++ CarProject/Tyre.java
@@ -4,12 +4,11 @@ public class Tyre {
     String position;
     String radius;
     
-    
+   
     public void printDetails() {
- System.out.println("-------------------------Tyre Details-------------------------");
+    System.out.println("-------------------------Tyre Details-------------------------");
     System.out.println("TYRE BRAND\t*\tTYRE POSITION\t*\tTYRE RADIUS");
     System.out.println(brand+"\t\t*\t" +position+"\t*\t" +radius);
     
-
     }
     }
