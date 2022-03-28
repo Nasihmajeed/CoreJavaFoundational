bce3da2df382af094ecc07f8d4823dbaa17026b5
kasujith <sujith.k.a@lxisoft.com>
2022-03-28 12:48:50 +0530

    File_Reader

--- MovieScript/com/lxisoft/moviescript/Scriptwriter.java
+++ MovieScript/com/lxisoft/moviescript/Scriptwriter.java
@@ -1,5 +1,8 @@
 package com.lxisoft.moviescript;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.*;
 
    public class Scriptwriter {
@@ -19,184 +22,24 @@ import java.util.*;
       
        public void setupDialogue () {
 
-      
-        
+        try {
 
-         dialogue.add(new Dialogue("\t       ( OUTSIDE THE CHURCH )  "+"\n"));
-       
-         dialogue.add(new Dialogue("\t ** Hazel waits for her Mom’s car to appear ** " +"\n"));
+          Scanner scanner = new Scanner(new FileReader("resources/Dialogues.csv"));
 
-         dialogue.add(new Dialogue("\t** Hazel turns to find the Beautiful Boy, Augustus, standing right next to her. **" +"\n"));
 
-         dialogue.add(new Dialogue("\t : I thought we were in a church basement but apparently we were literally in the  heart of Jesus." +"\n"));
+          while(scanner.hasNext()){
 
-         dialogue.add(new Dialogue("\t** Hazel smiles. **" +"\n"));
+            dialogue.add(new Dialogue(scanner.nextLine()));
+          }
 
-         dialogue.add(new Dialogue("\t : Someone should probably tell him, dont you think? Jesus? Seems kinda dangerous  keeping all these kids with cancer in  your heart." +"\n"));
+        }
 
-         dialogue.add(new Dialogue("\t** Gus laughs. **" +"\n"));
+        catch (FileNotFoundException e) {
 
-         dialogue.add(new Dialogue("\t : What's your name?" +"\n"));
+          e.printStackTrace();
+        }
 
-         dialogue.add(new Dialogue("\t : Hazel." +"\n"));
-        
-         dialogue.add(new Dialogue("\t : No your full name..." +"\n"));
-
-         dialogue.add(new Dialogue("\t** Hazel confused ** " +"\n"));
-
-         dialogue.add(new Dialogue("\t : Hazel Grace Lancaster." +"\n"));
-
-         dialogue.add(new Dialogue("\t** Gus nods to himself, smiles. Still fixated on her. ** " +"\n"));
-
-         dialogue.add(new Dialogue("\t** Hazel Continued ** " +"\n"));
-
-         dialogue.add(new Dialogue("\t : What?" +"\n"));
-
-         dialogue.add(new Dialogue("\t : I did not say anything." +"\n"));
-
-         dialogue.add(new Dialogue("\t : Why are you looking at me like that?" +"\n")); 
-
-         dialogue.add(new Dialogue("\t : Because you are beautiful." +"\n"));
-
-         dialogue.add(new Dialogue("\t** Hazel is taken aback. No one’s ever said that to her before. ** " +"\n"));
-
-         dialogue.add(new Dialogue("\t** Gus Continued ** " +"\n"));
-
-         dialogue.add(new Dialogue("\t : I enjoy looking at beautiful people and I  decided a while back not to deny myself  the simpler pleasures of existence. " ));
-         dialogue.add(new Dialogue("\t   Particularly given that, as you soastutely pointed out, we are all gonna die pretty soon."+"\n"));
-
-         dialogue.add(new Dialogue("\t : Iam not beaut --" +"\n"));
-
-       
-       
-       
-       
-        
-        dialogue.add(new Dialogue("\t[**Scene: Monica and Rachel's, late at night Monica is still examining her bill as Rachel emerges from her room.**]" +"\n"));
-
-        dialogue.add(new Dialogue("\t: Oh, Monica. You are not still going over that thing."+"\n"));
-
-        dialogue.add(new Dialogue("\t : This woman's living my life."+"\n"));
-
-        dialogue.add(new Dialogue("\t : What?"+"\n"));
-
-        dialogue.add(new Dialogue("\t : She's living my life, and she's doing it better than me! Look at this, look. She buys tickets for plays that I wanna see."));
-        dialogue.add(new Dialogue("\t        She, she buys clothes from stores that I'm intimidated by the sales people. She spent three hundred dollars on art supplies."+"\n"));
-        
-        dialogue.add(new Dialogue("\t : You're not an artist."+"\n"));
-   
-        dialogue.add(new Dialogue("\t : Yeah, well I might be if I had the supplies! I mean, I could do all this stuff. Only I don't."+"\n"));
-
-        dialogue.add(new Dialogue("\t : Oh, Monica, c'mon, you do cool things."+"\n"));
-
-        dialogue.add(new Dialogue("\t : Oh really? Okay, let's compare, shall we."+"\n"));
-
-        dialogue.add(new Dialogue("\t : (Yawning) Oh, it's so late for 'Shall we'..."+"\n"));
-
-        dialogue.add(new Dialogue("\t : Do I go horseback riding in the park? Do I take classes at the New School?"+"\n"));
-
-        dialogue.add(new Dialogue("\t : (Yawning) Nooo..."+"\n"));
-        
-        dialogue.add(new Dialogue("\t : This is so unfair! She's got everything I want, and she doesn't have my mother."+"\n"));
-
-        dialogue.add(new Dialogue("\t[**Scene: Central Perk, Joey and Chandler are discussing stage names.**]"+"\n"));
-
-        dialogue.add(new Dialogue("\t : How about Joey... Pepponi?"+"\n"));
-
-        dialogue.add(new Dialogue("\t : No, still too ethnic. My agent thinks I should have a name that's more neutral."+"\n"));
-
-        dialogue.add(new Dialogue("\t : Joey... Switzerland?"+"\n"));
-
-        dialogue.add(new Dialogue("\t(*The waitress brings their coffee.*)"+"\n"));
-
-        dialogue.add(new Dialogue("\t : Plus, y'know, I think it should be Joe. Y'know, Joey makes me sound like I'm, I dunno, this big. (Waitress looks at him funny) Which I'm not."+"\n"));
-
-        dialogue.add(new Dialogue("\t : Joe...Joe...Joe...Stalin?"+"\n"));
-
-        dialogue.add(new Dialogue("\t : Stalin...Stalin...do I know that name? It sounds familiar."+"\n"));
-
-        dialogue.add(new Dialogue("\t : Well, it does not ring a bell with me..."+"\n"));
-
-        dialogue.add(new Dialogue("\t : (Writes it down) Joe Stalin. Y'know, that's pretty good."+"\n"));
-
-        dialogue.add(new Dialogue("\t : Might wanna try Joseph."+"\n"));
-
-        dialogue.add(new Dialogue("\t(*Joey visibly thinks 'Of course!' and writes it down.*)"+"\n"));
-
-        dialogue.add(new Dialogue("\t : Joseph Stalin. I think you'd remember that!"+"\n"));
-
-        dialogue.add(new Dialogue("\t : Oh yes! Bye Bye Birdie, starring Joseph Stalin. Joseph Stalin is the Fiddler on the Roof."+"\n"));
-        
-        
-
-
-    
-        
-
-        dialogue.add(new Dialogue("\t  (HERENGRACHT CANAL - DAY 121)  " + "\n"));
-        
-        dialogue.add(new Dialogue("\t (*They stroll across a footbridge. Hazel notices he’s struggling with something.*) " + "\n"));
-
-        dialogue.add(new Dialogue("\t : Augustus?" + "\n"));
-
-        dialogue.add(new Dialogue("\t *Gus takes a cigarette out, sticks it between his lips.* " + "\n"));
-
-        dialogue.add(new Dialogue("\t HAZEL (CONT’D) " + "\n"));
-
-        dialogue.add(new Dialogue("\t : What is it? " + "\n"));
-
-        dialogue.add(new Dialogue("\t : There’s something I have to tell you...They walk in silence a few beats." + "\n" ));
-
-        dialogue.add(new Dialogue("\t GUS (CONT’D)" + "\n"));
-
-        dialogue.add(new Dialogue("\t : Just before you went into the hospital...There was this... I felt this... ache in my hip." + "\n"));
-
-        dialogue.add(new Dialogue("\t (*Hazel grabs onto his arm, a lump already forming.Gus takes the cigarette out of his mouth, clenches his teeth  tightly, trying not to cry.*) " + "\n"));
-
-        dialogue.add(new Dialogue("\t GUS (CONT’D) " + "\n"));
-
-        dialogue.add(new Dialogue("\t : It lit up like a Christmas tree, Hazel.The lining of my chest, my liver... everywhere " + "\n"));
-
-        dialogue.add(new Dialogue("\t (*Hazel loses it in that moment, hugging him for dear life, her  head in his lap.*) " + "\n"));
-
-        dialogue.add(new Dialogue("\t : I’m so sorry, Augustus. I’m so so sorry -- " + "\n"));
-
-        dialogue.add(new Dialogue("\t : I’m sorry too -- " + "\n"));
-
-        dialogue.add(new Dialogue("\t : It’s so unfair -- " + "\n"));
-
-        dialogue.add(new Dialogue("\t : I should have told you --" + "\n"));
-
-        dialogue.add(new Dialogue("\t : It’s so fucking unfair!  " + "\n"));
-
-        dialogue.add(new Dialogue("\t **A beat. Gus still trying not to cry.** " + "\n"));
-
-        dialogue.add(new Dialogue("\t : Apparently the world is... not a wish granting factory. " + "\n"));
-
-        dialogue.add(new Dialogue("\t GUS (CONT’D) " + "\n"));
-
-        dialogue.add(new Dialogue("\t : Don’t you worry about me, Hazel Grace. I’ll find a way to hang around and annoy you for a long time." + "\n"));
-
-        dialogue.add(new Dialogue("\t **She hugs him, perhaps a little too tightly. He winces.** " + "\n"));
-
-        dialogue.add(new Dialogue("\t : Does it hurt? " + "\n"));
-
-        dialogue.add(new Dialogue("\t : It’s ok. " + "\n"));
-        dialogue.add(new Dialogue("\t                : I’m ok." + "\n"));
-
-        dialogue.add(new Dialogue("\t : Ok. " + "\n"));
-
-        dialogue.add(new Dialogue("\t **Hazel takes a moment to look at him, touches his cheek.** " + "\n"));
-
-        dialogue.add(new Dialogue("\t : What? " + "\n" ));
-
-        dialogue.add(new Dialogue("\t : I’m just... I’m very fond of you. He grabs her hand and holds it. " + "\n"));
-
-        dialogue.add(new Dialogue("\t : I don’t suppose you can forget about it, treat me like I’m not dying. " + "\n"));
-
-        dialogue.add(new Dialogue("\t : I don’t think you’re dying, Augustus. You’ve just got a touch of cancer Gus nods. Squeezes her hand. " + "\n"));
-
-    
+         
       }
       
       public Script writeScript(List <Actor> actors) {
