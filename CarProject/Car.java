65a3372552e89031e32992ae89ff47fb716befae
Kiran <kiran.c.k@lxisoft.com>
2022-05-25 15:11:43 +0530

    car

---
+++ CarProject/Car.java
@@ -0,0 +1,20 @@
+public class Car
+{
+ String type;
+    String model;
+    String color;
+    int tyre;
+
+    
+    Car(String type, String model, String color) {
+        this.type = type;
+        this.model = model;
+        this.color = color;
+}
+public void car()
+{
+System.out.println("type is:"+type);
+System.out.println("model is:"+model);
+System.out.println("color is:"+color);
+}
+}
---
+++ CarProject/Tdd.java
@@ -0,0 +1,8 @@
+public class Tdd {
+public static void main(String[] args) {
+Car car = new Car("Ford", "Focus", "red");
+Car car1 = new Car("Toyota", "Auris", "blue");
+car.car();
+car1.car();
+	}
+}
