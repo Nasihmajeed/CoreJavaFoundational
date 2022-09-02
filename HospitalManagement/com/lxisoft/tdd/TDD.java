8ab863a31199accf92b71883761be2b6efa474ba
Santhosh Rajan <ajanthas1@gmail.com>
2022-09-02 13:09:37 +0530

    Adding new tasks

--- HospitalManagement/com/lxisoft/tdd/TDD.java
+++ HospitalManagement/com/lxisoft/tdd/TDD.java
@@ -18,13 +18,17 @@ public static void main (String [] args)
 //	New Department & Hospital Creation
 
 Department departmentna=new Department();
+Department ndepartment=new Department();
 Hospital sHospital = new Hospital();
+Hospital nHospital = new Hospital();
 
-//Patient Details
+//Patient Details 
 
 List<Patient> patients = new ArrayList<Patient>();
 Ward ward = new Ward();
+Ward nward = new Ward();
 ward.setPatients(patients);
+nward.setPatients(patients);
 
 for (int i=0; i < 10; i++){
 	Patient patient = new Patient();
@@ -32,24 +36,40 @@ for (int i=0; i < 10; i++){
 	patient.setPatientAge(21 + (i+1));
 	patients.add(patient);
 	}
-
+	
 // Ward Details
 
-ward.setWardNumber("Ward Number: 0005");
+ward.setWardNumber("Ward Number: 0001");
 ward.setTotalCapacity("Ward Total Capacity : 2500");
+
+// NewWard Details
+
+nward.setWardNumber("New Ward Number: 0001");
+nward.setTotalCapacity("New Ward Total Capacity : 5000");
  
 // Pinting
 
 sHospital.setDepartments(departmentna);
 
-sHospital.setHospitalName("HospitalName: Palakkad Hospital");
+nHospital.setDepartments(ndepartment);
+
+sHospital.setHospitalName("HospitalName: Aadvik Hospital");
 sHospital.setHospitalPlace("HospitalPlace: Palakkad Kerala");
+
+nHospital.setHospitalName("HospitalName: Aadvik Hospital");
+nHospital.setHospitalPlace("HospitalPlace: Mannarkkad Kerala");
  
 departmentna.setWards(ward);
-departmentna.setDepartmentName("Department1: AadvikBlock1");
-departmentna.setDepartmentBuilding("Department Building: AadvikBuilding1");
-departmentna.setDepartmentFloornumber("Department Floor Number: FirstFloor");		
+departmentna.setDepartmentName("Department1: Aadvik Block");
+departmentna.setDepartmentBuilding("Department Building: Aadvik Building");
+departmentna.setDepartmentFloornumber("Department Floor Number: Aadvik Building - FirstFloor");		
 sHospital.print();
+
+ndepartment.setWards(nward);
+ndepartment.setDepartmentName("NewDepartment1: New Aadvik Block");
+ndepartment.setDepartmentBuilding("NewDepartment Building: New Aadvik Building");
+ndepartment.setDepartmentFloornumber("NewDepartment Floor Number: New Aadvik Building - FirstFloor");		
+nHospital.print();
 
 }
 }
