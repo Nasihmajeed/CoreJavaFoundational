809637fe8ba5b7abf8a877045dc6e6d55ef87111
althaf <althafothaloor@gmail.com>
2022-09-05 09:27:42 +0400

    final code

--- CarProject/Tdd.java
+++ CarProject/Tdd.java
@@ -13,11 +13,13 @@ car.manufact="India";
  audio1.brand="JBL";
  audio1.noOfSpeaker=4;
  car.audio1=audio1;
- Tyre[] tyres=new Tyre[4]; 
+ Tyre[] tyres=new Tyre[4];
+ 
  tyres[0] = new Tyre("MRF","zapper",15,"front right");
  tyres[1] = new Tyre("BRIDGESTONE","ULTRAGRIP",15,"front left");
  tyres[2] = new Tyre("CEAT","GRIPPER",25,"rear right");
  tyres[3] = new Tyre("MICHELLiIN","NYLOGRIP",25,"rear left");
+ car.tyres=tyres;
  
 car.startCar();
 car.playMusic();
