46bc824f98c956047028ef9f343e9ab706ffc6ef
Kiran <kiran.c.k@lxisoft.com>
2022-10-26 07:59:13 +0530

    Location demo1

---
+++ AnimalGame/com/lxisoft/factors/Location.java
@@ -0,0 +1,28 @@
+package com.lxisoft.factors;
+
+public class Location {
+
+    private int locationXaxis;
+    private int locationYaxis;
+
+
+    public int getLocationXaxis() {
+
+        return locationXaxis;
+    }
+
+    public void setLocationXaxis(int x) {
+
+        this.locationXaxis = x;
+    }
+
+    public int getLocationYaxis() {
+
+        return locationYaxis;
+    }
+
+    public void setLocationYaxis(int y) {
+
+        this.locationYaxis = y;
+    }
+}
