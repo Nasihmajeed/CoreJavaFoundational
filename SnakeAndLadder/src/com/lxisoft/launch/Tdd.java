0d18255e4ad2ab6dd24144b47df9d35e3f4fd059
YasirHussain-Ts <yasirhussain.t.s@lxisoft.com>
2022-11-14 16:39:39 +0530

    Snake And Ladder Game 3.0

--- SnakeAndLadder/src/com/lxisoft/launch/Tdd.java
+++ SnakeAndLadder/src/com/lxisoft/launch/Tdd.java
@@ -6,7 +6,15 @@ public class Tdd
     public static void main(String [] args)    
     {                                                     
         Game snakeNLadderGame = new Game();                             
-        snakeNLadderGame.startGame();                      
+        snakeNLadderGame.startGame(); 
+        
+        //make locationCell into spaces for Coins, Ladders, Snakes.
+        //make efficient Methods() for the code.
+        //make code too short!
+        //starting location and ending location is must!
+        //player one have only one coin!
+        //playerOne Winning routes(Scores,snakebites,ladder points,etc...).
+        //make a well outstanding Connection!                              
     }
 }
 
